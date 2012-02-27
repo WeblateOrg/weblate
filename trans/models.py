a6e27df223fc4b7311052da49f1dfdfaadeb0a69
Michal Čihař <mcihar@suse.cz>
2012-02-27 10:13:42 +0100

    Create model for project

--- trans/models.py
+++ trans/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length = 100)
+    slug = models.SlugField(db_index = True)
+    web = models.URLField()
+    mail = models.EmailField()
+    instructions = models.URLField()
+    gitrepo = models.CharField(max_length = 200)
