37beb201d7bd7e58810cdf449d0b48868301a032
Michal Čihař <mcihar@suse.cz>
2012-02-27 09:49:20 +0100

    Start of Django app

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'weblate.views.home', name='home'),
+    # url(r'^weblate/', include('weblate.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
