57b99bb330b38fc16d42a18bc9846d6ccb5d60b2
Michal Čihař <mcihar@suse.cz>
2012-02-27 16:25:17 +0100

    Add some languages used in phpMyAdmin

--- lang/management/commands/setuplang.py
+++ lang/management/commands/setuplang.py
@@ -4,6 +4,10 @@ from translate.lang import data
 
 EXTRALANGS = [
     ('ur', 'Urdu', 2, '(n != 1)'),
+    ('uz@latin', 'Uzbek (latin)', 1, '0'),
+    ('uz', 'Uzbek', 1, '0'),
+    ('sr@latin', 'Serbian (latin)', 3, '(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)'),
+    ('be@latin', 'Belarusian (latin)', 3, '(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)'),
 ]
 
 class Command(BaseCommand):
@@ -12,14 +16,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for code, props in data.languages.items():
             lang, created = Language.objects.get_or_create(
-                code = code,
-                name = props[0],
-                nplurals = props[1],
-                pluralequation = props[2])
+                code = code)
+            lang.name = props[0]
+            lang.nplurals = props[1]
+            lang.pluralequation = props[2]
+            lang.save()
         for props in EXTRALANGS:
             lang, created = Language.objects.get_or_create(
-                code = props[0],
-                name = props[1],
-                nplurals = props[2],
-                pluralequation = props[3])
+                code = props[0])
+            lang.name = props[1]
+            lang.nplurals = props[2]
+            lang.pluralequation = props[3]
+            lang.save()
 
