6efb5905f789f814c6de570d26a168193bc6198d
Michal Čihař <mcihar@suse.cz>
2012-02-27 10:46:12 +0100

    Model for translation

--- trans/models.py
+++ trans/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from lang.models import Language
 
 class Project(models.Model):
     name = models.CharField(max_length = 100)
@@ -15,3 +14,10 @@ class SubProject(models.Model):
     project = models.ForeignKey(Project)
     repo = models.CharField(max_length = 200)
     branch = models.CharField(max_length = 50)
+
+class Translation(models.Model):
+    subproject = models.ForeignKey(SubProject)
+    language = models.ForeignKey(Language)
+    translated = models.FloatField()
+    revision = models.CharField(max_length = 40)
+    filename = models.CharField(max_length = 200)
