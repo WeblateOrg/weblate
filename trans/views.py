baa6cf036f3942081e9b8548d50893f2ff1d5d0f
Michal Čihař <mcihar@suse.cz>
2012-02-27 11:58:55 +0100

    Fix rendering

--- trans/views.py
+++ trans/views.py
@@ -6,7 +6,7 @@ from trans.models import Project, SubProject, Translation, Unit
 def show_project(request, project):
     obj = get_object_or_404(Project, slug = project)
 
-    return render_to_response('project.html', RequestContext({
+    return render_to_response('project.html', RequestContext(request, {
         'object': obj,
         'title': '%s @ Weblate' % obj.name,
     }))
@@ -14,7 +14,7 @@ def show_project(request, project):
 def show_subproject(request, project, subproject):
     obj = get_object_or_404(SubProject, slug = subproject, project__slug = project)
 
-    return render_to_response('subproject.html', RequestContext({
+    return render_to_response('subproject.html', RequestContext(request, {
         'object': obj,
         'title': '%s/%s @ Weblate' % (obj.name, obj.project.name),
     }))
@@ -22,7 +22,7 @@ def show_subproject(request, project, subproject):
 def show_translation(request, project, subproject, lang):
     obj = get_object_or_404(Language, language__code = lang, subproject__slug = subproject, subproject__project__slug = project)
 
-    return render_to_response('translation.html', RequestContext({
+    return render_to_response('translation.html', RequestContext(request, {
         'object': obj,
         'title': '%s %s/%s @ Weblate' % (obj.language.name, obj.subproject.name, obj.subproject.project.name),
     }))
