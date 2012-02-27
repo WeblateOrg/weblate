67c7fb29e1714f2b6a693410b05b02ecb0e1b788
Michal Čihař <mcihar@suse.cz>
2012-02-27 11:21:43 +0100

    Set time zone

--- settings.py
+++ settings.py
@@ -39,7 +39,7 @@ else:
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Prague'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
