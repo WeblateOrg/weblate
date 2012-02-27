84c5996893f8d6a81bc7da3c12e5cd920e31c860
Michal Čihař <mcihar@suse.cz>
2012-02-27 10:14:32 +0100

    Install both applications

--- settings.py
+++ settings.py
@@ -117,6 +117,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'trans',
+    'lang',
 )
 
 # A sample logging configuration. The only tangible logging
