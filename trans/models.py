1c13f50c61575b131d6c2cdd2f173b1e1ad051f3
Michal Čihař <mcihar@suse.cz>
2012-02-27 10:39:01 +0100

    Add subproject

--- trans/models.py
+++ trans/models.py
@@ -8,4 +8,11 @@ class Project(models.Model):
     web = models.URLField()
     mail = models.EmailField()
     instructions = models.URLField()
-    gitrepo = models.CharField(max_length = 200)
+
+class SubProject(models.Model):
+    name = models.CharField(max_length = 100)
+    slug = models.SlugField(db_index = True)
+    project = models.ForeignKey(Project)
+    repo = models.CharField(max_length = 200)
+    branch = models.CharField(max_length = 50)
+    propagate = models.ForeignKey('self')
