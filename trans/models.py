69af54174d76f1a4a3c4a29f90c2c8269ef23225
Michal Čihař <mcihar@suse.cz>
2012-02-28 15:28:56 +0100

    Fix import condition

--- trans/models.py
+++ trans/models.py
@@ -212,7 +212,7 @@ class Translation(models.Model):
         # Load po file
         store = self.get_store()
         for pos, unit in enumerate(store.units):
-            if unit.istranslatable():
+            if not unit.istranslatable():
                 continue
             newunit = Unit.objects.update_from_unit(self, unit, pos)
             try:
