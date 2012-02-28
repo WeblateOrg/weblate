3936780671fa6710bc3e9f5c6952f9d3c9daadf8
Michal Čihař <mcihar@suse.cz>
2012-02-28 10:27:29 +0100

    Ignore po headers

--- trans/models.py
+++ trans/models.py
@@ -189,6 +189,8 @@ class Translation(models.Model):
         # Load po file
         store = factory.getobject(os.path.join(self.subproject.get_path(), self.filename))
         for unit in store.units:
+            if unit.isheader():
+                continue
             newunit = Unit.objects.update_from_unit(self, unit)
             try:
                 oldunits.remove(newunit.id)
