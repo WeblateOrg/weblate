a5098513bc2d4c0cafe4c55a7548c9a97de36aed
Michal Čihař <mcihar@suse.cz>
2012-03-01 11:23:56 +0100

    Highlight whitespaces and keep newlines in strings

--- trans/templatetags/weblate.py
+++ trans/templatetags/weblate.py
@@ -1,12 +1,23 @@
 from django.template.defaultfilters import stringfilter
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
 from django import template
+import re
 
 register = template.Library()
 
+
+def fmt_whitespace(value):
+    value = re.sub(r'(  +| $|^ )', '<span class="hlspace">\\1</span>', value)
+    return value
+
 @register.filter
 @stringfilter
-def fmttranslation(s):
-    s = escape(s)
-    return mark_safe(s)
+def fmttranslation(value):
+    value = escape(force_unicode(value))
+    value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
+    paras = re.split('\n', value)
+    paras = [fmt_whitespace(p) for p in paras]
+    value = '<br />'.join(paras)
+    return mark_safe(value)
