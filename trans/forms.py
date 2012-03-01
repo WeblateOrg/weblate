3c384e17c06bacac68befcd1aaa94b2773b7336a
Michal Čihař <mcihar@suse.cz>
2012-03-01 13:31:23 +0100

    Started renderer for plural editor

--- trans/forms.py
+++ trans/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.utils.translation import ugettext_lazy, ugettext as _
 
+class PluralTextarea(forms.Textarea):
+    '''
+    Text area extension which possibly handles plurals.
+    '''
+    def render(self, name, value, attrs=None):
+        ret = super(PluralTextarea, self).render(name, value, attrs)
+        return ret
+
 class TranslationForm(forms.Form):
     checksum = forms.CharField(widget = forms.HiddenInput)
-    target = forms.CharField(widget = forms.Textarea, required = False)
+    target = forms.CharField(widget = PluralTextarea, required = False)
     fuzzy = forms.BooleanField(label = ugettext_lazy('Fuzzy'), required = False)
 
