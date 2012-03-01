9bf892c4607f1a8150ae66c36c5c4d83c0cf0f2a
Michal Čihař <mcihar@suse.cz>
2012-03-01 11:36:06 +0100

    Styling for accept/delete buttons

--- media/js/loader.js
+++ media/js/loader.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.button').button();
     $('ul.menu li').button();
     $('ul.breadcums').buttonset();
+    $('.sug-accept').button({text: false, icons: { primary: "ui-icon-check" }});
+    $('.sug-delete').button({text: false, icons: { primary: "ui-icon-close" }});
 });
