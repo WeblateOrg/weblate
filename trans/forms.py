e658c65b19e9b9d30f7102bf577c1e79cc44adab
Michal Čihař <mcihar@suse.cz>
2012-03-01 13:35:31 +0100

    Render plurals

--- trans/forms.py
+++ trans/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 from django.utils.translation import ugettext_lazy, ugettext as _
+from django.utils.safestring import mark_safe
 
 class PluralTextarea(forms.Textarea):
     '''
     Text area extension which possibly handles plurals.
     '''
     def render(self, name, value, attrs=None):
-        ret = super(PluralTextarea, self).render(name, value, attrs)
-        return ret
+        if type(value) != list:
+            return super(PluralTextarea, self).render(name, value, attrs)
+        ret = []
+        print value
+        for idx, val in enumerate(value):
+            if idx > 0:
+                fieldname = '%s_%d' % (name, idx)
+            else:
+                fieldname = name
+            ret.append(super(PluralTextarea, self).render(fieldname, val, attrs))
+        return mark_safe('<br />'.join(ret))
 
 class TranslationForm(forms.Form):
     checksum = forms.CharField(widget = forms.HiddenInput)
