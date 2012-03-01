fe463a69072a6ebf68451fb9ab618701bd5facce
Michal Čihař <mcihar@suse.cz>
2012-03-01 10:03:01 +0100

    Add method for accepting suggestion

--- trans/models.py
+++ trans/models.py
@@ -427,3 +427,14 @@ class Suggestion(models.Model):
     user = models.ForeignKey(User, null = True, blank = True)
     project = models.ForeignKey(Project)
     language = models.ForeignKey(Language)
+
+    def accept(self, request):
+        allunits = Unit.objects.filter(
+            checksum = self.checksum,
+            translation__subproject__project = self.project,
+            translation__language = self.language
+        )
+        for unit in allunits:
+            unit.target = self.target
+            unit.fuzzy = False
+            unit.save_backend(request, False)
