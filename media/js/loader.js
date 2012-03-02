1e6aed9a103a7639716f0a100e20488d6be9ab41
Michal Čihař <mcihar@suse.cz>
2012-03-02 17:35:48 +0100

    Make menu buttons work as buttons

--- media/js/loader.js
+++ media/js/loader.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.button').button();
-    $('ul.menu li').button();
+    $('ul.menu li a').button();
     $('ul.breadcums').buttonset();
     $('.sug-accept').button({text: false, icons: { primary: "ui-icon-check" }});
     $('.sug-delete').button({text: false, icons: { primary: "ui-icon-close" }});
