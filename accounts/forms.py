67c2342488d7bde2fcc4fa4f4a265efb3cb23f5e
Michal Čihař <mcihar@suse.cz>
2012-03-02 16:38:41 +0100

    Editing of user info

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 
 from accounts.models import Profile
+from django.contrib.auth.models import User
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from accounts.forms import ProfileForm
+from accounts.forms import ProfileForm, UserForm
 
 def profile(request):
 
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
+        form = ProfileForm(request.POST, instance = request.user.get_profile())
+        userform = UserForm(request.POST, instance = request.user)
+        if form.is_valid() and userform.is_valid():
+            form.save()
+            userform.save()
     else:
         form = ProfileForm(instance = request.user.get_profile())
+        userform = UserForm(instance = request.user)
 
-    return render_to_response('profile.html', RequestContext(request, {'form': form}))
+    return render_to_response('profile.html', RequestContext(request, {
+        'form': form,
+        'userform': userform,
+        }))
