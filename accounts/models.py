d62a39d7ec0a4b700f1de8c11322159f8043711b
Michal Čihař <mcihar@suse.cz>
2012-03-02 14:45:01 +0100

    Cleanup imports

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.db.models.signals import post_save, pre_save
+from django.contrib.auth.signals import user_logged_in
+from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
 from lang.models import Language
