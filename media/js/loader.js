51378cf32df9b7a6ff8a870a2756ec17d3d7d03f
Michal Čihař <mcihar@suse.cz>
2012-03-02 17:05:07 +0100

    Complete navigation (add first/end buttons)

--- media/js/loader.js
+++ media/js/loader.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
     $('.sug-accept').button({text: false, icons: { primary: "ui-icon-check" }});
     $('.sug-delete').button({text: false, icons: { primary: "ui-icon-close" }});
     $('.navi').buttonset();
+    $('.button-first').button({text: false, icons: { primary: "ui-icon-seek-first" }});
     $('.button-next').button({text: false, icons: { primary: "ui-icon-seek-next" }});
     $('.button-prev').button({text: false, icons: { primary: "ui-icon-seek-prev" }});
+    $('.button-end').button({text: false, icons: { primary: "ui-icon-seek-end" }});
     $('#id_target').change(function f() {$('#id_fuzzy').attr('checked', false);}).focus();
     $('#copy-text').button({text: false, icons: { primary: "ui-icon-arrowthick-1-s" }}).click(function f() {
         $.get("/js/get/" + $('#id_checksum').attr('value') + '/', function(data) {
