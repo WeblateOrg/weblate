4e490930644e93844efbe7682d4a539eefc2c51b
Michal Čihař <mcihar@suse.cz>
2012-03-02 14:52:36 +0100

    Use LocaleMiddleware

--- settings.py
+++ settings.py
@@ -99,6 +99,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
