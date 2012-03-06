d6ad1efb76f1b6e0ed842e70d3506d18b037281e
Michal Čihař <mcihar@suse.cz>
2012-03-06 15:53:53 +0100

    Use own definition of urls

--- urls.py
+++ urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.simple import direct_to_template
+
+from registration.views import activate
+from registration.views import register
 
 from accounts.forms import RegistrationForm
 
@@ -22,11 +27,43 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Auth
-    url(r'^accounts/register/$', 'registration.views.register', {
+    url(r'^accounts/register/$', register, {
             'form_class': RegistrationForm,
             'extra_context': {'title': _('User registration')}},
             name='weblate_register'),
-    url(r'^accounts/', include('registration.urls')),
+    url(r'^accounts/register/complete/$',
+        direct_to_template,
+        {'template': 'registration/registration_complete.html'},
+        name='registration_complete'),
+    url(r'^accounts/activate/(?P<activation_key>\w+)/$',
+        activate,
+        name='registration_activate'),
+    url(r'^accounts/login/$',
+        auth_views.login,
+        {'template_name': 'registration/login.html'},
+        name='auth_login'),
+    url(r'^accounts/logout/$',
+        auth_views.logout,
+        {'template_name': 'registration/logout.html'},
+        name='auth_logout'),
+    url(r'^accounts/password/change/$',
+        auth_views.password_change,
+        name='auth_password_change'),
+    url(r'^accounts/password/change/done/$',
+        auth_views.password_change_done,
+        name='auth_password_change_done'),
+    url(r'^accounts/password/reset/$',
+        auth_views.password_reset,
+        name='auth_password_reset'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        name='auth_password_reset_confirm'),
+    url(r'^accounts/password/reset/complete/$',
+        auth_views.password_reset_complete,
+        name='auth_password_reset_complete'),
+    url(r'^accounts/password/reset/done/$',
+        auth_views.password_reset_done,
+        name='auth_password_reset_done'),
     url(r'^accounts/profile/', 'accounts.views.profile'),
 
     url(r'^contact/', 'accounts.views.contact'),
