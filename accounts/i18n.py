84a48977585f06464052c1f9a75cbdec1d0c2c06
Michal Čihař <mcihar@suse.cz>
2012-03-06 15:15:58 +0100

    Add file to translate messages from django-registration

---
+++ accounts/i18n.py
@@ -0,0 +1,8 @@
+'''
+Fake file to translate messages from django-registration.
+'''
+
+def fake():
+    _(u'This username is already taken. Please choose another.')
+    _(u'You must type the same password each time')
+    _(u'This email address is already in use. Please supply a different email address.')
