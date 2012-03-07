275d1f05bc3f79ca7a4335ce0ea03b1a4b024553
Michal Čihař <mcihar@suse.cz>
2012-03-07 16:22:18 +0100

    Show plural identifiers for source as well

--- trans/templatetags/weblate.py
+++ trans/templatetags/weblate.py
@@ -7,6 +7,7 @@ from django.conf import settings
 import re
 
 from trans.util import split_plural
+from lang.models import Language
 
 register = template.Library()
 
@@ -17,15 +18,18 @@ def fmt_whitespace(value):
 
 @register.filter
 @stringfilter
-def fmttranslation(value):
+def fmttranslation(value, language=None):
+    if language is None:
+        language = Language.objects.get(code = 'en')
     plurals = split_plural(value)
     parts = []
-    for value in plurals:
+    for idx, value in enumerate(plurals):
         value = escape(force_unicode(value))
         value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
         paras = re.split('\n', value)
         paras = [fmt_whitespace(p) for p in paras]
-        value = '<br />'.join(paras)
+        value = '<span class="plural">%s</span><br />' % language.get_plural_label(idx)
+        value += '<br />'.join(paras)
         parts.append(value)
     value = '<hr />'.join(parts)
     return mark_safe(value)
