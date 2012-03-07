968182ca88ca4a3a490489c26366548ebc997608
Michal Čihař <mcihar@suse.cz>
2012-03-07 13:26:16 +0100

    Remove not used import

--- trans/forms.py
+++ trans/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _, gettext
+from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_unicode
 
