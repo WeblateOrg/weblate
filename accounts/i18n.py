b7adbea131fab1bb1e564fed4ae70cd22c8da8f1
Michal Čihař <mcihar@suse.cz>
2012-03-12 11:54:51 +0100

    Add some more messages we want to translate

--- accounts/i18n.py
+++ accounts/i18n.py
@@ -1,8 +1,15 @@
 '''
-Fake file to translate messages from django-registration.
+Fake file to translate messages from django-registration and django.contrib.auth.
 '''
 
 def fake():
     _(u'This username is already taken. Please choose another.')
     _(u'You must type the same password each time')
     _(u'This email address is already in use. Please supply a different email address.')
+    _("Old password")
+    _("New password")
+    _("New password confirmation")
+    _("The two password fields didn't match.")
+    _("E-mail")
+    _("Username")
+    _("Password")
