03916dfe82bd6f03ac9ccad98207e6870bdafe12
Michal Čihař <mcihar@suse.cz>
2012-03-21 14:03:20 +0100

    Simplify printing of array

--- html/config.js
+++ html/config.js
@@ -2,15 +2,11 @@
 var APERTIUM_API_KEY = '{{ apertium_api_key }}';
 {% endif %}
 {% if apertium_langs %}
-var APERTIUM_LANGS = [
-{% for lang in apertium_langs %}
-    '{{ lang }}',
-{% endfor %}
-];
+var APERTIUM_LANGS = ['{{ apertium_langs|join:"','" }}'];
 {% endif %}
 {% if microsoft_api_key %}
 var MICROSOFT_API_KEY = '{{ microsoft_api_key }}';
 {% endif %}
 {% if microsoft_langs %}
-var MICROSOFT_LANGS = [{% for lang in microsoft_langs %}'{{ lang }}',{% endfor %}];
+var MICROSOFT_LANGS = ['{{ microsoft_langs|join:"','" }}'];
 {% endif %}
