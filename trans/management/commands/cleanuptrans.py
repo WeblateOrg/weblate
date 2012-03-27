e1cfbc0d3dcaaf39cd6f4e542d3c948645056cb0
Michal Čihař <mcihar@suse.cz>
2012-03-27 15:32:32 +0200

    Cleanup per language

--- trans/management/commands/cleanuptrans.py
+++ trans/management/commands/cleanuptrans.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from trans.models import Suggestion, Check, Unit
+from lang.models import Language
 from optparse import make_option
 
 class Command(BaseCommand):
     help = 'clenups orphaned checks and suggestions'
 
     def handle(self, *args, **options):
-        units = Unit.objects.values('checksum').distinct()
-        Check.objects.exclude(checksum__in = units).delete()
-        Suggestion.objects.exclude(checksum__in = units).delete()
+        for lang in Language.objects.all():
+            units = Unit.objects.filter(translation__language = lang).values('checksum').distinct()
+            Check.objects.filter(language = lang).exclude(checksum__in = units).delete()
+            Suggestion.objects.filter(language = lang).exclude(checksum__in = units).delete()
