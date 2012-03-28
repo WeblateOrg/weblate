1ce923d3352c0759547ce422836045a5af61f1d7
Michal Čihař <mcihar@suse.cz>
2012-03-28 13:49:31 +0200

    Method for commiting pending changes

--- trans/models.py
+++ trans/models.py
@@ -69,7 +69,7 @@ class Project(models.Model):
             return 0
         return round(translations['translated__sum'] * 100.0 / translations['total__sum'], 1)
 
-    def git_needs_commit(self, gitrepo = None):
+    def git_needs_commit(self):
         '''
         Checks whether there are some not commited changes.
         '''
@@ -77,6 +77,13 @@ class Project(models.Model):
             if s.git_needs_commit():
                 return True
         return False
+
+    def check_commit_needed(self):
+        '''
+        Commits any pending changes.
+        '''
+        for s in self.subproject_set.all():
+            s.check_commit_needed()
 
 class SubProject(models.Model):
     name = models.CharField(max_length = 100, help_text = _('Name to display'))
