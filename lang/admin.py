5a6384c32e414085f318e13c40f1c3c93fdcb905
Michal Čihař <mcihar@suse.cz>
2012-03-30 15:27:11 +0200

    Show plural form in admin interface

--- lang/admin.py
+++ lang/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from lang.models import Language
 
 class LanguageAdmin(admin.ModelAdmin):
-    list_display = ['name', 'code']
+    list_display = ['name', 'code', 'get_plural_form']
     search_fields = ['name', 'code']
 
 admin.site.register(Language, LanguageAdmin)
