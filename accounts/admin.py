f7e218a1edeef3564eb0c81236ea321a7955e635
Michal Čihař <mcihar@suse.cz>
2012-03-30 15:29:05 +0200

    Profile filtering

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from accounts.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ['user__username']
+    list_display = ['user', 'language', 'suggested', 'translated']
+    search_fields = ['user__username', 'user__email']
+    list_filter = ['language']
 
 admin.site.register(Profile, ProfileAdmin)
 
