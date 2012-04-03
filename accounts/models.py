d6b2c1a861c87faeabf009dd613254def692e43a
Michal Čihař <mcihar@suse.cz>
2012-04-03 13:11:19 +0200

    No need to mark safe

--- accounts/models.py
+++ accounts/models.py
@@ -47,7 +47,7 @@ def set_lang(sender, **kwargs):
         if newprofile:
             profile.save
             messages.add_message(request, messages.INFO,
-                mark_safe(gettext('Your profile has been migrated, you might want to adjust preferences.')))
+                gettext('Your profile has been migrated, you might want to adjust preferences.'))
     lang_code = user.get_profile().language
     request.session['django_language'] = lang_code
 
