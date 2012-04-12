3d9968930f3094ec49eabcc190c5c785b6a34e71
Michal Čihař <mcihar@suse.cz>
2012-04-12 08:53:11 +0200

    Revert "Use httpdomain extension"
    This reverts commit 875ef404c4621a15ebb56c8c01510b04f89bd1ee.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.httpdomain']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
