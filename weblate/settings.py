78f685bc47567a349baaae90e1157f09050e7804
Michal Čihař <mcihar@suse.cz>
2012-04-16 15:33:11 +0200

    Update settings.py for Django 1.4

--- weblate/settings.py
+++ weblate/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2',  'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'weblate',                      # Or path to database file if using sqlite3.
         'USER': 'weblate',                      # Not used with sqlite3.
         'PASSWORD': 'weblate',                  # Not used with sqlite3.
@@ -62,8 +62,11 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -171,12 +174,17 @@ else:
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'formatters': {
         'syslog': {
             'format': 'weblate[%(process)d]: %(levelname)s %(message)s'
@@ -191,6 +199,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console':{
