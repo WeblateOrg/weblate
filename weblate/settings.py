8b021cee8c54d7ad61a5dbcee601de46e2b6ac46
Michal Čihař <mcihar@suse.cz>
2012-04-17 10:31:40 +0200

    Prevent using timezone aware dates

--- weblate/settings.py
+++ weblate/settings.py
@@ -66,7 +66,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
