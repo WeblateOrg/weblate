d0bff06a8b4f1def4a586269a9816d7622adacc9
Michal Čihař <michal@cihar.com>
2012-07-11 22:13:53 +0200

    Add "fake" localization entries

--- weblate/accounts/i18n.py
+++ weblate/accounts/i18n.py
@@ -28,4 +28,8 @@ def fake():
     _("Failed translation")
     _("The request for machine translation has failed.")
     _("Error details:")
+    _('Confirm reseting repository')
+    _('Reseting the repository will throw away all local changes!')
+    _('Ok')
+    _('Cancel')
 
