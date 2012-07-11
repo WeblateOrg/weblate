7b0d7258c17aa0e047fbd88bbe482b6e5e4967c8
Michal Čihař <michal@cihar.com>
2012-07-11 22:11:33 +0200

    Reset of git repository needs to be confirmed (issue #57).

--- weblate/media/js/loader.js
+++ weblate/media/js/loader.js
@@ -235,6 +235,29 @@ $(function() {
             $('.buttons').buttonset();
             $('.buttons .disabled').button('disable');
             $('.details-accordion').accordion({collapsible: true, active: -1});
+            $('.confirm-reset').click(function() {
+
+                $('<div title="' + gettext('Confirm reseting repository') + '"><p>' + gettext('Reseting the repository will throw away all local changes!') + '</p></div>').dialog({
+                    modal: true,
+                    autoOpen: true,
+                    buttons: [
+                        {
+                            text: gettext("Ok"),
+                            click: function() {
+                                window.location = $('.confirm-reset').attr('href');
+                                $(this).dialog("close");
+                            },
+                        },
+                        {
+                            text: gettext("Cancel"),
+                            click: function() {
+                                $(this).dialog("close");
+                            }
+                        }
+                    ]
+                });
+                return false;
+            });
         },
         show: function (e, ui) {
             var $panel = $(ui.panel);
