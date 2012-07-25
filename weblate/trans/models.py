e0afedb45f43bacf48f57a0c9a299956b4438910
Michal Čihař <mcihar@suse.cz>
2012-07-25 16:10:59 +0200

    Propagate even if there is no local change (might be elsewhere)

--- weblate/trans/models.py
+++ weblate/trans/models.py
@@ -1624,6 +1624,7 @@ class Unit(models.Model):
 
         # Return if there was no change
         if not saved:
+            self.propagate(request)
             return
 
         # Update translated flag
