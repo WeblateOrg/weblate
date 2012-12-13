3798d7da5782dee73ddc4463a55e80443d842d41
Michal Čihař <mcihar@suse.cz>
2012-12-13 09:30:34 +0100

    Add more tests for SameCheck

--- weblate/trans/tests/test_checks.py
+++ weblate/trans/tests/test_checks.py
@@ -14,13 +14,42 @@ class Language(object):
         self.code = code
 
 
-class CheckTest(TestCase):
-    def test_same(self):
-        check = SameCheck()
-        self.assertFalse(check.check_single(
+class SameCheckTest(TestCase):
+    def setUp(self):
+        self.check = SameCheck()
+
+    def test_not_same(self):
+        self.assertFalse(self.check.check_single(
             'source',
             'translation',
             '',
+            Language('cs'),
+            None
+        ))
+
+    def test_same(self):
+        self.assertTrue(self.check.check_single(
+            'source',
+            'source',
+            '',
+            Language('cs'),
+            None
+        ))
+
+    def test_same_english(self):
+        self.assertFalse(self.check.check_single(
+            'source',
+            'source',
+            '',
+            Language('en'),
+            None
+        ))
+
+    def test_same_format(self):
+        self.assertFalse(self.check.check_single(
+            '%(source)s',
+            '%(source)s',
+            'python-format',
             Language('cs'),
             None
         ))
