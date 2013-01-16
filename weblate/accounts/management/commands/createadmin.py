3b5b084e61102198768172c47f6c4440cfd679bd
Michal Čihař <mcihar@suse.cz>
2013-01-16 09:46:53 +0100

    Coding style

--- weblate/accounts/management/commands/createadmin.py
+++ weblate/accounts/management/commands/createadmin.py
@@ -21,6 +21,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
+
 class Command(BaseCommand):
     help = 'setups admin user with admin password (INSECURE!)'
 
