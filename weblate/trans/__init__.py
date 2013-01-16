0c97d84502e414044223a2d39547afe7d22bc62d
Michal Čihař <mcihar@suse.cz>
2013-01-16 09:45:50 +0100

    Use decorator for attaching signal

--- weblate/trans/__init__.py
+++ weblate/trans/__init__.py
@@ -19,7 +19,10 @@
 #
 
 from south.signals import post_migrate
+from django.dispatch import receiver
 
+
+@receiver(post_migrate)
 def create_permissions_compat(app, **kwargs):
     '''
     Creates permissions like syncdb would if we were not using South
@@ -29,5 +32,3 @@ def create_permissions_compat(app, **kwargs):
     from django.db.models import get_app
     from django.contrib.auth.management import create_permissions
     create_permissions(get_app(app), (), 0)
-
-post_migrate.connect(create_permissions_compat)
