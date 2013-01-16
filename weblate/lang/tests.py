84da807835e8a1462a70ab31661b1bc58b997dbf
Michal Čihař <mcihar@suse.cz>
2013-01-16 09:51:43 +0100

    Coding style improvements

--- weblate/lang/tests.py
+++ weblate/lang/tests.py
@@ -18,12 +18,16 @@ class LanguagesTest(TestCase):
         ('ar_AA', 'ar', 'rtl'),
         ('ar_XX', 'ar_XX', 'rtl'),
     )
+
     def test_auto_create(self):
         """
         Tests that auto create correctly handles languages
         """
         for original, expected, direction in self.TEST_LANGUAGES:
-            self.assertEqual(Language.objects.auto_get_or_create(original).code, expected)
+            self.assertEqual(
+                Language.objects.auto_get_or_create(original).code,
+                expected
+            )
 
     def test_rtl(self):
         '''
@@ -36,7 +40,7 @@ class LanguagesTest(TestCase):
             self.assertIn(expected, lang.get_html())
 
     def test_plurals(self):
-        lang = Language.objects.get(code = 'cs')
+        lang = Language.objects.get(code='cs')
         self.assertEqual(
             lang.get_plural_form(),
             'nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;'
