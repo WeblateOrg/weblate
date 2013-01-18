0eaf64d60ccd45ebdd95c24c6cb89a937eaa852a
Michal Čihař <mcihar@suse.cz>
2013-01-18 16:20:21 +0100

    Factor out message editing

--- weblate/trans/tests/test_views.py
+++ weblate/trans/tests/test_views.py
@@ -143,23 +143,29 @@ class EditTest(ViewTestCase):
     '''
     Tests for manipulating translation.
     '''
-    def test_edit(self):
+    def edit_unit(self, source, target):
         translation = self.subproject.translation_set.get(language_code='cs')
-        unit = translation.unit_set.get(source='Hello, world!\n')
+        unit = translation.unit_set.get(source=source)
         translate_url = reverse('translate', kwargs={
             'project': self.subproject.project.slug,
             'subproject': self.subproject.slug,
             'lang': 'cs',
         })
-        response = self.client.post(
+        return self.client.post(
             translate_url,
             {
                 'checksum': unit.checksum,
-                'target': 'Nazdar svete!\n',
+                'target': target,
                 'type': 'all',
                 'dir': 'forward',
                 'pos': '1',
             }
+        )
+
+    def test_edit(self):
+        response = self.edit_unit(
+            'Hello, world!\n',
+            'Nazdar svete!\n'
         )
         # We should get to second message
         self.assertRedirects(response, translate_url + '?type=all&pos=1')
@@ -168,22 +174,9 @@ class EditTest(ViewTestCase):
         self.assertEqual(len(unit.checks()), 0)
 
     def test_edit_check(self):
-        translation = self.subproject.translation_set.get(language_code='cs')
-        unit = translation.unit_set.get(source='Hello, world!\n')
-        translate_url = reverse('translate', kwargs={
-            'project': self.subproject.project.slug,
-            'subproject': self.subproject.slug,
-            'lang': 'cs',
-        })
-        response = self.client.post(
-            translate_url,
-            {
-                'checksum': unit.checksum,
-                'target': 'Nazdar svete!',
-                'type': 'all',
-                'dir': 'forward',
-                'pos': '1',
-            }
+        response = self.edit_unit(
+            'Hello, world!\n',
+            'Nazdar svete!'
         )
         # We should stay on current message
         self.assertRedirects(response, translate_url + '?type=all&pos=1&dir=stay')
