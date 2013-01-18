e10384b929f698f9d8d5638c9901afeabd33b912
Michal Čihař <mcihar@suse.cz>
2013-01-18 14:36:40 +0100

    Count translations, whether project was actually loaded

--- weblate/trans/tests/test_models.py
+++ weblate/trans/tests/test_models.py
@@ -101,18 +101,22 @@ class SubProjectTest(RepoTestCase):
     def test_create(self):
         project = self.create_subproject()
         self.assertTrue(os.path.exists(project.get_path()))
+        self.assertEqual(project.translation_set.count(), 2)
 
     def test_create_iphone(self):
         project = self.create_iphone()
         self.assertTrue(os.path.exists(project.get_path()))
+        self.assertEqual(project.translation_set.count(), 1)
 
     def test_create_java(self):
         project = self.create_java()
         self.assertTrue(os.path.exists(project.get_path()))
+        self.assertEqual(project.translation_set.count(), 1)
 
     def test_create_xliff(self):
         project = self.create_xliff()
         self.assertTrue(os.path.exists(project.get_path()))
+        self.assertEqual(project.translation_set.count(), 1)
 
     def test_link(self):
         project = self.create_iphone()
