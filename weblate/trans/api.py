add46b32eda620e45f41429ae4aa8c051c15d193
Frederik Freso S. Olesen <freso.dk@gmail.com>
2013-02-14 00:42:04 +0100

    Use git_service_hook() for GitHub.

--- weblate/trans/api.py
+++ weblate/trans/api.py
@@ -132,18 +132,10 @@ def bitbucket_hook_helper(data):
 
 
 @csrf_exempt
-def github_hook(request):
+def github_hook_helper(data):
     '''
     API to handle commit hooks from Github.
     '''
-    if not appsettings.ENABLE_HOOKS:
-        return HttpResponseNotAllowed([])
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-    try:
-        data = json.loads(request.POST['payload'])
-    except (ValueError, KeyError):
-        return HttpResponseBadRequest('could not parse json!')
     try:
         repo = 'git://github.com/%s/%s.git' % (
             data['repository']['owner']['name'],
@@ -152,17 +144,14 @@ def github_hook(request):
         branch = data['ref'].split('/')[-1]
     except KeyError:
         return HttpResponseBadRequest('could not parse json!')
-    logger.info(
-        'received GitHub notification on repository %s, branch %s',
-        repo,
-        branch
-    )
-    for obj in SubProject.objects.filter(repo=repo, branch=branch):
-        logger.info('GitHub notification will update %s', obj)
-        thread = threading.Thread(target=obj.do_update)
-        thread.start()
 
-    return HttpResponse('update triggered')
+    return_data = {
+        'service_long_name': 'GitHub',
+        'repo': repo,
+        'branch': branch,
+    }
+
+    return return_data
 
 
 def dt_handler(obj):
