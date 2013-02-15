c5a1d814b313cd70be2c1fefab3bed37a587b019
Michal Čihař <mcihar@suse.cz>
2013-02-15 10:57:25 +0100

    Consistent codingstyle

--- weblate/accounts/views.py
+++ weblate/accounts/views.py
@@ -38,17 +38,22 @@ from weblate.accounts.forms import (
 
 
 def mail_admins_sender(subject, message, sender, fail_silently=False,
-        connection=None, html_message=None):
-    """Sends a message to the admins, as defined by the ADMINS setting."""
+            connection=None, html_message=None):
+    '''
+    Sends a message to the admins, as defined by the ADMINS setting.
+    '''
     if not settings.ADMINS:
         return
+
     mail = EmailMultiAlternatives(
         u'%s%s' % (settings.EMAIL_SUBJECT_PREFIX, subject),
         message, sender, [a[1] for a in settings.ADMINS],
         connection=connection
     )
+
     if html_message:
         mail.attach_alternative(html_message, 'text/html')
+
     mail.send(fail_silently=fail_silently)
 
 
