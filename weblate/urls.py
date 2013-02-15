17bc0a647be298a31423baddcd5947bc8bfda936
Michal Čihař <mcihar@suse.cz>
2013-02-15 09:12:32 +0100

    Fix syntax error (issue #208)

--- weblate/urls.py
+++ weblate/urls.py
@@ -182,8 +182,8 @@ urlpatterns = patterns('',
 
     url(r'^hooks/update/(?P<project>[^/]*)/(?P<subproject>[^/]*)/$', 'weblate.trans.api.update_subproject'),
     url(r'^hooks/update/(?P<project>[^/]*)/$', 'weblate.trans.api.update_project'),
-    url(r'^hooks/github/$', 'weblate.trans.api.git_service_hook', {service: 'github'}),
-    url(r'^hooks/bitbucket/$', 'weblate.trans.api.git_service_hook', {service: 'bitbucket'}),
+    url(r'^hooks/github/$', 'weblate.trans.api.git_service_hook', {'service': 'github'}),
+    url(r'^hooks/bitbucket/$', 'weblate.trans.api.git_service_hook', {'service': 'bitbucket'}),
 
     url(r'^exports/stats/(?P<project>[^/]*)/(?P<subproject>[^/]*)/$', 'weblate.trans.api.export_stats', name='export-stats'),
 
