465511ee69aa9c0d2bb852e42959878cd750ac42
Michal Čihař <mcihar@suse.cz>
2013-02-21 11:37:02 +0100

    Drop compatibility import

--- weblate/trans/checks/__init__.py
+++ weblate/trans/checks/__init__.py
@@ -18,34 +18,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Compatibility imports
-from weblate.trans.checks.same import SameCheck
-from weblate.trans.checks.chars import BeginNewlineCheck
-from weblate.trans.checks.chars import EndNewlineCheck
-from weblate.trans.checks.chars import BeginSpaceCheck
-from weblate.trans.checks.chars import EndSpaceCheck
-from weblate.trans.checks.chars import EndStopCheck
-from weblate.trans.checks.chars import EndColonCheck
-from weblate.trans.checks.chars import EndQuestionCheck
-from weblate.trans.checks.chars import EndExclamationCheck
-from weblate.trans.checks.chars import EndEllipsisCheck
-from weblate.trans.checks.format import PythonFormatCheck
-from weblate.trans.checks.format import PHPFormatCheck
-from weblate.trans.checks.format import CFormatCheck
-from weblate.trans.checks.consistency import PluralsCheck
-from weblate.trans.checks.consistency import ConsistencyCheck
-from weblate.trans.checks.consistency import DirectionCheck
-from weblate.trans.checks.chars import NewlineCountingCheck
-from weblate.trans.checks.markup import BBCodeCheck
-from weblate.trans.checks.chars import ZeroWidthSpaceCheck
-from weblate.trans.checks.markup import XMLTagsCheck
-from weblate.trans.checks.source import OptionalPluralCheck
-from weblate.trans.checks.source import EllipsisCheck
-
-
-# Initialize checks list
 from weblate.trans import appsettings
 from django.core.exceptions import ImproperlyConfigured
+
+# Initialize checks list
 CHECKS = {}
 for path in appsettings.CHECK_LIST:
     i = path.rfind('.')
