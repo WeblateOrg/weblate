d8ec470beb24108633cce4a643dc470b52b119ac
Michal Čihař <mcihar@suse.cz>
2013-03-25 13:28:40 +0100

    Missing params

--- trans/admin.py
+++ trans/admin.py
@@ -57,7 +57,7 @@ class ProjectAdmin(admin.ModelAdmin):
         Commits pending changes for selected resources.
         '''
         for project in queryset:
-            project.commit_pending()
+            project.commit_pending(request)
         self.message_user(
             request,
             "Flushed changes in %d git repos." % queryset.count()
@@ -102,7 +102,7 @@ class SubProjectAdmin(admin.ModelAdmin):
         Commits pending changes for selected resources.
         '''
         for project in queryset:
-            project.commit_pending()
+            project.commit_pending(request)
         self.message_user(
             request,
             "Flushed changes in %d git repos." % queryset.count()
