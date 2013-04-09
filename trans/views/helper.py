c8b9eca08194f7171a098a8738be29b6f13e4198
Michal Čihař <mcihar@suse.cz>
2013-04-09 15:13:51 +0200

    Drop not used import

--- trans/views/helper.py
+++ trans/views/helper.py
@@ -23,7 +23,6 @@ Helper methods for views.
 
 from trans.models import Project, SubProject, Translation
 from lang.models import Language
-from django.utils.translation import ugettext as _
 from django.shortcuts import get_object_or_404
 import django.utils.translation
 
