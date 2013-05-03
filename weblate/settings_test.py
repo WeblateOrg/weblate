8608b3bbbf0abf2a7c02129b44dab499f18b8f98
Michal Čihař <mcihar@suse.cz>
2013-05-03 14:01:44 +0200

    Silent logging for all tests

--- weblate/settings_test.py
+++ weblate/settings_test.py
@@ -43,3 +43,32 @@ MACHINE_TRANSLATION_SERVICES = (
     'trans.machine.microsoft.MicrosoftTranslation',
     'trans.machine.dummy.DummyTranslation',
 )
+
+# Silent logging setup
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'weblate': {
+            'handlers': [],
+            'level': 'ERROR',
+        }
+    }
+}
