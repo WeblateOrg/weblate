01dacf6c157da819b25333051ad7988c3a72d3ea
Michal Čihař <mcihar@suse.cz>
2013-05-03 13:46:20 +0200

    No verbose logging in Jenkins

--- weblate/settings_jenkins.py
+++ weblate/settings_jenkins.py
@@ -55,3 +55,31 @@ PROJECT_APPS = (
 )
 
 PYLINT_RCFILE = os.path.join(WEB_ROOT, '..', 'pylint.rc')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'weblate': {
+            'handlers': [],
+            'level': 'ERROR',
+        }
+    }
+}
