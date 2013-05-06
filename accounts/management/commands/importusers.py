079e36910a497fe6c5616b1e2c98e4cf0830031d
Michal Čihař <mcihar@suse.cz>
2013-05-06 16:53:12 +0200

    Remove not used imports

--- accounts/management/commands/importusers.py
+++ accounts/management/commands/importusers.py
@@ -19,7 +19,7 @@
 #
 
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import Group, Permission, User
+from django.contrib.auth.models import User
 import json
 
 
