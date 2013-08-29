037a69aa092fe210f0ccf7e2ce81b8d43a638c1f
Michal Čihař <mcihar@suse.cz>
2013-08-29 09:01:50 +0200

    Silent some of pyflakes warnings

--- accounts/migrations/0002_subscriptions.py
+++ accounts/migrations/0002_subscriptions.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
