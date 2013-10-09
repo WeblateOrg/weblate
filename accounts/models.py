08307287b76d731d1c919f2ccc561638900c2e14
Michal Čihař <mcihar@suse.cz>
2013-10-09 14:42:42 +0200

    Remove unused imports

--- accounts/models.py
+++ accounts/models.py
@@ -23,8 +23,7 @@ from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
-from django.utils.translation import ugettext_lazy as _, gettext
-from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import (
     Group, User, Permission, UNUSABLE_PASSWORD
 )
