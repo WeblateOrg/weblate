11e643301921595f99d88c10cbb35ec9aed53aef
Michal Čihař <mcihar@suse.cz>
2013-11-26 12:12:56 +0100

    Do not include full text in tmserver/amagama requests

--- trans/machine/tmserver.py
+++ trans/machine/tmserver.py
@@ -69,7 +69,7 @@ class TMServerTranslation(MachineTranslation):
             self.url,
             urllib.quote(appsettings.SOURCE_LANGUAGE),
             urllib.quote(language),
-            urllib.quote(text.encode('utf-8')),
+            urllib.quote(text[:500].encode('utf-8')),
         )
         response = self.json_req(url)
 
