0834b372acd931692b72edcb8acaac29b5bb7a92
Michal Čihař <mcihar@suse.cz>
2013-11-29 14:33:59 +0100

    Share single config for all database tests

--- weblate/settings_test.py
+++ weblate/settings_test.py
@@ -23,6 +23,20 @@
 #
 
 from weblate.settings_example import *
+import os
+
+if 'TRAVIS_DATABASE' in os.environ:
+    if os.environ['TRAVIS_DATABASE'] == 'mysql':
+        DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+        DATABASES['default']['NAME'] = 'weblate'
+        DATABASES['default']['USER'] = 'root'
+        DATABASES['default']['PASSWORD'] = ''
+    elif os.environ['TRAVIS_DATABASE'] == 'postgresql':
+        DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+        DATABASES['default']['NAME'] = 'weblate'
+        DATABASES['default']['USER'] = 'postgres'
+        DATABASES['default']['PASSWORD'] = ''
+
 
 # Configure admins
 ADMINS = (('Weblate test', 'noreply@weblate.org'), )
