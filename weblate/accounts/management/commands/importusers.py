2dd04d6c2c82945ad708b70da6db637691d0125a
Michal Čihař <mcihar@suse.cz>
2014-01-03 14:39:45 +0100

    Move future import to top

--- weblate/accounts/management/commands/importusers.py
+++ weblate/accounts/management/commands/importusers.py
@@ -18,9 +18,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from __future__ import print_function
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
-from __future__ import print_function
 import json
 
 
