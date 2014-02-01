8a3b123c4001710c12ba57f58d4664b4bd8fae77
Michal Čihař <michal@cihar.com>
2014-02-01 15:55:11 +0100

    Allow to erase commit message
    Closes #414

--- weblate/trans/views/edit.py
+++ weblate/trans/views/edit.py
@@ -321,7 +321,9 @@ def handle_translate(translation, request, user_locked,
         )
     elif not user_locked:
         # Custom commit message
-        if 'commit_message' in request.POST and request.POST['commit_message']:
+        if ('commit_message' in request.POST
+                and request.POST['commit_message']
+                != unit.translation.commit_message):
             # Commit pending changes so that they don't get new message
             unit.translation.commit_pending(request, request.user)
             # Store new commit message
