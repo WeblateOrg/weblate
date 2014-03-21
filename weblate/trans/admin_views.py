e93355e1d95e08a42bdad1e28c023b9d49944557
Michal Čihař <michal@cihar.com>
2014-03-21 11:21:38 +0100

    Fix import
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/admin_views.py
+++ weblate/trans/admin_views.py
@@ -27,7 +27,7 @@ from django.contrib import messages
 from django.conf import settings
 from weblate import settings_example
 from weblate import appsettings
-from weblate.trans.util import HAS_LIBRAVATAR
+from weblate.accounts.avatar import HAS_LIBRAVATAR
 from weblate.accounts.forms import HAS_ICU
 import weblate
 import django
