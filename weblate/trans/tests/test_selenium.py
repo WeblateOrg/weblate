388f80a95d277e7b1d3e83697d37dad2fc35a2cd
Michal Čihař <michal@cihar.com>
2014-08-22 11:08:01 +0200

    Disable Opera testing for now, seems to cause troubles
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/tests/test_selenium.py
+++ weblate/trans/tests/test_selenium.py
@@ -140,10 +140,10 @@ EXTRA_PLATFORMS = {
         'browserName': 'chrome',
         'platform': 'XP',
     },
-    'Opera': {
-        'browserName': 'opera',
-        'platform': 'WIN7',
-    },
+    # 'Opera': {
+    #     'browserName': 'opera',
+    #     'platform': 'WIN7',
+    # },
     'MSIE10': {
         'browserName': 'internet explorer',
         'version': '10',
