7c6922c6d84e7b43642eeca2d51e04212b346cd5
Michal Čihař <michal@cihar.com>
2014-08-27 08:57:59 +0200

    Convert README to rst (for pypi compatibility)
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
