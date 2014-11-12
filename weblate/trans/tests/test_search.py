920ad46fd3c8f69a7198f8b18f8e7922c2079230
Michal Čihař <michal@cihar.com>
2014-11-12 15:17:08 +0100

    Split up tests
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/trans/tests/test_search.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2014 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Tests for search views.
+"""
+
+import re
+from django.core.urlresolvers import reverse
+from weblate.trans.tests.test_views import ViewTestCase
+
+
+class SearchViewTest(ViewTestCase):
+    def setUp(self):
+        super(SearchViewTest, self).setUp()
+        self.translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        self.translate_url = self.translation.get_translate_url()
+
+    def do_search(self, params, expected):
+        '''
+        Helper method for performing search test.
+        '''
+        response = self.client.get(
+            self.translate_url,
+            params,
+        )
+        if expected is None:
+            self.assertRedirects(
+                response,
+                self.translation.get_absolute_url()
+            )
+        else:
+            self.assertContains(
+                response,
+                expected
+            )
+        return response
+
+    def test_all_search(self):
+        '''
+        Searching in all projects.
+        '''
+        response = self.client.get(
+            reverse('search'),
+            {'q': 'hello'}
+        )
+        self.assertContains(
+            response,
+            '<span class="hlmatch">Hello</span>, world'
+        )
+
+    def test_project_search(self):
+        '''
+        Searching within project.
+        '''
+        # Default
+        self.do_search(
+            {'q': 'hello'},
+            'Fulltext search for'
+        )
+        # Fulltext
+        self.do_search(
+            {'q': 'hello', 'search': 'ftx'},
+            'Fulltext search for'
+        )
+        # Substring
+        self.do_search(
+            {'q': 'hello', 'search': 'substring'},
+            'Substring search for'
+        )
+        # Exact string
+        self.do_search(
+            {'q': 'Thank you for using Weblate.', 'search': 'exact'},
+            'Search for exact string'
+        )
+        # Short string
+        self.do_search(
+            {'q': 'x'},
+            'Ensure this value has at least 2 characters (it has 1).'
+        )
+        # Wrong type
+        self.do_search(
+            {'q': 'xxxxx', 'search': 'xxxx'},
+            'Select a valid choice. xxxx is not one of the available choices.'
+        )
+
+    def test_review(self):
+        # Review
+        self.do_search(
+            {'date': '2010-01-10', 'type': 'review'},
+            None
+        )
+        # Review, invalid date
+        self.do_search(
+            {'date': '2010-01-', 'type': 'review'},
+            'Enter a valid date.'
+        )
+
+    def test_search_links(self):
+        response = self.do_search(
+            {'q': 'weblate'},
+            'Fulltext search for'
+        )
+        # Extract search ID
+        search_id = re.findall(r'sid=([0-9a-f-]*)&amp', response.content)[0]
+        # Try access to pages
+        response = self.client.get(
+            self.translate_url,
+            {'sid': search_id, 'offset': 0}
+        )
+        self.assertContains(
+            response,
+            'http://demo.weblate.org/',
+        )
+        response = self.client.get(
+            self.translate_url,
+            {'sid': search_id, 'offset': 1}
+        )
+        self.assertContains(
+            response,
+            'Thank you for using Weblate.',
+        )
+        # Go to end
+        response = self.client.get(
+            self.translate_url,
+            {'sid': search_id, 'offset': 2}
+        )
+        self.assertRedirects(
+            response,
+            self.translation.get_absolute_url()
+        )
+        # Try invalid SID (should be deleted above)
+        response = self.client.get(
+            self.translate_url,
+            {'sid': search_id, 'offset': 1}
+        )
+        self.assertRedirects(
+            response,
+            self.translation.get_absolute_url()
+        )
+
+    def test_seach_checksum(self):
+        unit = self.translation.unit_set.get(
+            source='Try Weblate at <http://demo.weblate.org/>!\n'
+        )
+        response = self.do_search(
+            {'checksum': unit.checksum},
+            '3 / 4'
+        )
+        # Extract search ID
+        search_id = re.findall(r'sid=([0-9a-f-]*)&amp', response.content)[0]
+        # Navigation
+        response = self.do_search(
+            {'sid': search_id, 'offset': 0},
+            '1 / 4'
+        )
+        response = self.do_search(
+            {'sid': search_id, 'offset': 3},
+            '4 / 4'
+        )
+        response = self.do_search(
+            {'sid': search_id, 'offset': 4},
+            None
+        )
+
+    def test_search_type(self):
+        self.do_search(
+            {'type': 'untranslated'},
+            'Untranslated strings'
+        )
+        self.do_search(
+            {'type': 'fuzzy'},
+            None
+        )
+        self.do_search(
+            {'type': 'suggestions'},
+            None
+        )
+        self.do_search(
+            {'type': 'allchecks'},
+            None
+        )
+        self.do_search(
+            {'type': 'plurals'},
+            None
+        )
+        self.do_search(
+            {'type': 'all'},
+            '1 / 4'
+        )
+
+    def test_search_plural(self):
+        response = self.do_search(
+            {'q': 'banana'},
+            'banana'
+        )
+        self.assertContains(response, 'One')
+        self.assertContains(response, 'Few')
+        self.assertContains(response, 'Other')
+        self.assertNotContains(response, 'Plural form ')
+
+    def test_checksum(self):
+        response = self.do_search({'checksum': 'invalid'}, None)
+        self.assertRedirects(
+            response,
+            self.get_translation().get_absolute_url()
+        )
---
+++ weblate/trans/tests/test_suggestions.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2014 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Tests for sugestion views.
+"""
+
+from weblate.trans.models.unitdata import Suggestion
+from weblate.trans.tests.test_views import ViewTestCase
+
+
+class SuggestionsTest(ViewTestCase):
+    def add_suggestion_1(self):
+        return self.edit_unit(
+            'Hello, world!\n',
+            'Nazdar svete!\n',
+            suggest='yes'
+        )
+
+    def add_suggestion_2(self):
+        return self.edit_unit(
+            'Hello, world!\n',
+            'Ahoj svete!\n',
+            suggest='yes'
+        )
+
+    def test_add(self):
+        translate_url = self.get_translation().get_translate_url()
+        # Try empty suggestion (should not be added)
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            suggest='yes'
+        )
+        # We should stay on same message
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        # Add first suggestion
+        response = self.add_suggestion_1()
+        # We should get to second message
+        self.assertRedirectsOffset(response, translate_url, 1)
+
+        # Add second suggestion
+        response = self.add_suggestion_2()
+        # We should get to second message
+        self.assertRedirectsOffset(response, translate_url, 1)
+
+        # Reload from database
+        unit = self.get_unit()
+        translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        # Check number of suggestions
+        self.assertEqual(translation.have_suggestion, 1)
+        self.assertBackend(0)
+
+        # Unit should not be translated
+        self.assertEqual(len(unit.checks()), 0)
+        self.assertFalse(unit.translated)
+        self.assertFalse(unit.fuzzy)
+        self.assertEqual(len(self.get_unit().suggestions()), 2)
+
+    def test_delete(self):
+        translate_url = self.get_translation().get_translate_url()
+        # Create two suggestions
+        self.add_suggestion_1()
+        self.add_suggestion_2()
+
+        # Get ids of created suggestions
+        suggestions = [sug.pk for sug in self.get_unit().suggestions()]
+        self.assertEqual(len(suggestions), 2)
+
+        # Delete one of suggestions
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            delete=suggestions[0],
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        # Reload from database
+        unit = self.get_unit()
+        translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        # Check number of suggestions
+        self.assertEqual(translation.have_suggestion, 1)
+        self.assertBackend(0)
+
+        # Unit should not be translated
+        self.assertEqual(len(unit.checks()), 0)
+        self.assertFalse(unit.translated)
+        self.assertFalse(unit.fuzzy)
+        self.assertEqual(len(self.get_unit().suggestions()), 1)
+
+    def test_accept(self):
+        translate_url = self.get_translation().get_translate_url()
+        # Create two suggestions
+        self.add_suggestion_1()
+        self.add_suggestion_2()
+
+        # Get ids of created suggestions
+        suggestions = [sug.pk for sug in self.get_unit().suggestions()]
+        self.assertEqual(len(suggestions), 2)
+
+        # Accept one of suggestions
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            accept=suggestions[1],
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        # Reload from database
+        unit = self.get_unit()
+        translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        # Check number of suggestions
+        self.assertEqual(translation.have_suggestion, 1)
+
+        # Unit should be translated
+        self.assertEqual(len(unit.checks()), 0)
+        self.assertTrue(unit.translated)
+        self.assertFalse(unit.fuzzy)
+        self.assertEqual(unit.target, 'Ahoj svete!\n')
+        self.assertBackend(1)
+        self.assertEqual(len(self.get_unit().suggestions()), 1)
+
+    def test_accept_anonymous(self):
+        translate_url = self.get_translation().get_translate_url()
+        self.client.logout()
+        # Create suggestions
+        self.add_suggestion_1()
+
+        self.client.login(username='testuser', password='testpassword')
+
+        # Get ids of created suggestion
+        suggestions = list(self.get_unit().suggestions())
+        self.assertEqual(len(suggestions), 1)
+
+        self.assertIsNone(suggestions[0].user)
+
+        # Accept one of suggestions
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            accept=suggestions[0].pk,
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        # Reload from database
+        unit = self.get_unit()
+        translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        # Check number of suggestions
+        self.assertEqual(translation.have_suggestion, 0)
+
+        # Unit should be translated
+        self.assertEqual(unit.target, 'Nazdar svete!\n')
+
+    def test_vote(self):
+        translate_url = self.get_translation().get_translate_url()
+        self.subproject.suggestion_voting = True
+        self.subproject.suggestion_autoaccept = 0
+        self.subproject.save()
+
+        self.add_suggestion_1()
+
+        suggestion_id = self.get_unit().suggestions()[0].pk
+
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            upvote=suggestion_id,
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        suggestion = Suggestion.objects.get(pk=suggestion_id)
+        self.assertEqual(
+            suggestion.get_num_votes(),
+            1
+        )
+
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            downvote=suggestion_id,
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        suggestion = Suggestion.objects.get(pk=suggestion_id)
+        self.assertEqual(
+            suggestion.get_num_votes(),
+            -1
+        )
+
+    def test_vote_autoaccept(self):
+        self.add_suggestion_1()
+
+        translate_url = self.get_translation().get_translate_url()
+        self.subproject.suggestion_voting = True
+        self.subproject.suggestion_autoaccept = 1
+        self.subproject.save()
+
+        suggestion_id = self.get_unit().suggestions()[0].pk
+
+        response = self.edit_unit(
+            'Hello, world!\n',
+            '',
+            upvote=suggestion_id,
+        )
+        self.assertRedirectsOffset(response, translate_url, 0)
+
+        # Reload from database
+        unit = self.get_unit()
+        translation = self.subproject.translation_set.get(
+            language_code='cs'
+        )
+        # Check number of suggestions
+        self.assertEqual(translation.have_suggestion, 0)
+
+        # Unit should be translated
+        self.assertEqual(len(unit.checks()), 0)
+        self.assertTrue(unit.translated)
+        self.assertFalse(unit.fuzzy)
+        self.assertEqual(unit.target, 'Nazdar svete!\n')
+        self.assertBackend(1)
