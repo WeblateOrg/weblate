651fa77aee3a4937294dd455f65cedf8845a6d50
Michal Čihař <michal@cihar.com>
2014-11-27 11:38:05 +0100

    Test SSH wrapper generating
    Issue #597
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/trans/tests/test_ssh.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2014 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import tempfile
+import os
+import shutil
+from django.test import TestCase
+import weblate.trans.ssh
+from weblate.trans.tests.utils import get_test_file
+
+
+TEST_HOSTS = get_test_file('known_hosts')
+
+
+class SSHTest(TestCase):
+    '''
+    Tests for customized admin interface.
+    '''
+    _tempdir = None
+
+    def setUp(self):
+        super(SSHTest, self).setUp()
+        self._tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        if self._tempdir is not None:
+            shutil.rmtree(self._tempdir)
+
+    def test_parse(self):
+        try:
+            backup = weblate.trans.ssh.KNOWN_HOSTS_FILE
+            weblate.trans.ssh.KNOWN_HOSTS_FILE = TEST_HOSTS
+            hosts = weblate.trans.ssh.get_host_keys()
+            self.assertEqual(len(hosts), 50)
+        finally:
+            weblate.trans.ssh.KNOWN_HOSTS_FILE = backup
+
+    def test_create_ssh_wrapper(self):
+        try:
+            backup_dir = weblate.trans.ssh.WEBLATE_DIR
+            backup_ssh_wrapper = weblate.trans.ssh.SSH_WRAPPER
+            weblate.trans.ssh.WEBLATE_DIR = os.path.join(self._tempdir, 'wl')
+            weblate.trans.ssh.SSH_WRAPPER = os.path.join(
+                weblate.trans.ssh.WEBLATE_DIR, 'ssh-wrapper'
+            )
+            weblate.trans.ssh.create_ssh_wrapper()
+            with open(weblate.trans.ssh.SSH_WRAPPER, 'r') as handle:
+                self.assertTrue(
+                    weblate.trans.ssh.KNOWN_HOSTS_FILE in handle.read()
+                )
+            self.assertTrue(
+                os.access(weblate.trans.ssh.SSH_WRAPPER, os.X_OK)
+            )
+        finally:
+            weblate.trans.ssh.WEBLATE_DIR = backup_dir
+            weblate.trans.ssh.SSH_WRAPPER = backup_ssh_wrapper
