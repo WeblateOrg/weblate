b3ee3b52e209c5f4429e0d8bf3232b953edb4b57
Michal Čihař <michal@cihar.com>
2014-12-02 13:54:16 +0100

    Allow to inject extra vars to environment
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/util.py
+++ weblate/trans/util.py
@@ -141,11 +141,13 @@ def get_configuration_errors():
     return cache.get('configuration-errors', [])
 
 
-def get_clean_env():
+def get_clean_env(extra=None):
     """
     Returns cleaned up environment for subprocess execution.
     """
     environ = {}
+    if extra is not None:
+        environ.update(extra)
     variables = ('HOME', 'PATH', 'LANG', 'LD_LIBRARY_PATH')
     for var in variables:
         if var in os.environ:
