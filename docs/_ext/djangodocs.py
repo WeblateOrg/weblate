8abed31b1f748f0f07b3eb5eb89020bb0f3d3501
Michal Čihař <michal@cihar.com>
2014-12-02 11:47:59 +0100

    Remove not used translator
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/_ext/djangodocs.py
+++ docs/_ext/djangodocs.py
@@ -55,54 +55,6 @@ def setup(app):
     )
 
 
-class DjangoHTMLTranslator(SmartyPantsHTMLTranslator):
-    """
-    Django-specific reST to HTML tweaks.
-    """
-
-    # Don't use border=1, which docutils does by default.
-    def visit_table(self, node):
-        self.context.append(self.compact_p)
-        self.compact_p = True
-        self._table_row_index = 0  # Needed by Sphinx
-        self.body.append(self.starttag(node, 'table', CLASS='docutils'))
-
-    def depart_table(self, node):
-        self.compact_p = self.context.pop()
-        self.body.append('</table>\n')
-
-    def visit_desc_parameterlist(self, node):
-        self.body.append('(')  # by default sphinx puts <big> around the "("
-        self.first_param = 1
-        self.optional_param_level = 0
-        self.param_separator = node.child_text_separator
-        self.required_params_left = sum([isinstance(c, addnodes.desc_parameter)
-                                         for c in node.children])
-
-    def depart_desc_parameterlist(self, node):
-        self.body.append(')')
-
-    if sphinx_ver < '1.0.8':
-        #
-        # Don't apply smartypants to literal blocks
-        #
-        def visit_literal_block(self, node):
-            self.no_smarty += 1
-            SmartyPantsHTMLTranslator.visit_literal_block(self, node)
-
-        def depart_literal_block(self, node):
-            SmartyPantsHTMLTranslator.depart_literal_block(self, node)
-            self.no_smarty -= 1
-
-    # Give each section a unique ID -- nice for custom CSS hooks
-    def visit_section(self, node):
-        old_ids = node.get('ids', [])
-        node['ids'] = ['s-' + i for i in old_ids]
-        node['ids'].extend(old_ids)
-        SmartyPantsHTMLTranslator.visit_section(self, node)
-        node['ids'] = old_ids
-
-
 def parse_django_admin_node(env, sig, signode):
     command = sig.split(' ')[0]
     env._django_curr_admin_command = command
