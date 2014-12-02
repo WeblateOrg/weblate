432435117459f0dd2573e94eccf08fcd091d29dd
Michal Čihař <michal@cihar.com>
2014-12-02 11:48:37 +0100

    Remove unused tags
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/_ext/djangodocs.py
+++ docs/_ext/djangodocs.py
@@ -26,21 +26,6 @@ def setup(app):
         rolename="setting",
         indextemplate="pair: %s; setting",
     )
-    app.add_crossref_type(
-        directivename="templatetag",
-        rolename="ttag",
-        indextemplate="pair: %s; template tag"
-    )
-    app.add_crossref_type(
-        directivename="templatefilter",
-        rolename="tfilter",
-        indextemplate="pair: %s; template filter"
-    )
-    app.add_crossref_type(
-        directivename="fieldlookup",
-        rolename="lookup",
-        indextemplate="pair: %s; field lookup type",
-    )
     app.add_description_unit(
         directivename="django-admin",
         rolename="djadmin",
