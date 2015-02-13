a229a8ca14c9b099808a11bfb3bbaa348ee7f265
Michal Čihař <michal@cihar.com>
2015-02-13 14:07:39 +0100

    Fix typo in comment
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/models/translation.py
+++ weblate/trans/models/translation.py
@@ -514,7 +514,7 @@ class Translation(models.Model, URLMixin, PercentMixin, LoggerMixin):
         # Following query can get huge, so we should find better way
         # to delete stale units, probably sort of garbage collection
 
-        # We should also do cleanup on source stings tracking objects
+        # We should also do cleanup on source strings tracking objects
 
         # Get lists of stale units to delete
         units_to_delete = self.unit_set.exclude(
