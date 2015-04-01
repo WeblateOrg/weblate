12d7a8c23c09d581ae9cd105a19404cf35e7fd1d
Michal Čihař <michal@cihar.com>
2015-04-01 10:53:22 +0200

    Simplify mercurial log conditions
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/vcs.py
+++ weblate/trans/vcs.py
@@ -836,17 +836,14 @@ class HgRepository(Repository):
         Checks whether repository needs merge with upstream
         (is missing some revisions).
         """
-        missing_revs = self.execute(
-            ['log', '--branch', 'tip', '--prune', '.']
-        )
-        return missing_revs != ''
+        return self.execute(['log', '-r', 'only(tip,.)']) != ''
 
     def needs_push(self, branch):
         """
         Checks whether repository needs push to upstream
         (has additional revisions).
         """
-        return self.execute(['log', '-r', 'not public()']) != ''
+        return self.execute(['log', '-r', 'outgoing()']) != ''
 
     @classmethod
     def _get_version(cls):
