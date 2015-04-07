d9e24ab81754e125868ff53bc32535c76d5adc84
Michal Čihař <michal@cihar.com>
2015-04-07 11:20:06 +0200

    Force English locales for all spawned programs
    Fixes #697
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/util.py
+++ weblate/trans/util.py
@@ -155,10 +155,12 @@ def get_clean_env(extra=None):
     """
     Returns cleaned up environment for subprocess execution.
     """
-    environ = {}
+    environ = {
+        'LANG': 'en_US.UTF-8'
+    }
     if extra is not None:
         environ.update(extra)
-    variables = ('HOME', 'PATH', 'LANG', 'LD_LIBRARY_PATH')
+    variables = ('HOME', 'PATH', 'LD_LIBRARY_PATH')
     for var in variables:
         if var in os.environ:
             environ[var] = os.environ[var]
