8f91c2e9c388a3a21509d096120b071b73a3e0f1
Michal Čihař <michal@cihar.com>
2015-04-28 16:27:33 +0200

    Fail in case virtualenv can not be loaded
    This usually indicates problem.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- openshift/virtualenv.py
+++ openshift/virtualenv.py
@@ -22,7 +22,4 @@ import os
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+execfile(virtualenv, dict(__file__=virtualenv))
