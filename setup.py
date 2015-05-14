943909f8338eaf19b6fb36bcc304de08a505d70d
Michal Čihař <michal@cihar.com>
2015-05-14 10:04:42 +0200

    Fix  setup initialization
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@
 import os
 from setuptools import setup
 
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 with open('requirements.txt') as requirements:
     REQUIRES = requirements.read().splitlines()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='weblate',
