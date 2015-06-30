ba6ab1fd5643bfa7acf7b71e328a0c06885a351c
Michal Čihař <michal@cihar.com>
2015-06-30 14:08:47 +0200

    Migrate languges to new meta opts
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/lang/migrations/0002_auto_20150630_1208.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lang', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='language',
+            options={'ordering': ['name'], 'verbose_name': 'Language', 'verbose_name_plural': 'Languages'},
+        ),
+    ]
