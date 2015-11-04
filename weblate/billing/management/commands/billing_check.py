d6dac39cade16b12459fb42ba7980313b1777e75
Michal Čihař <michal@cihar.com>
2015-11-04 14:44:38 +0100

    Add management command for checking billing limits
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/billing/management/commands/billing_check.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2015 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from django.core.management.base import BaseCommand, CommandError
+from weblate.billing.models import Billing
+
+
+class Command(BaseCommand):
+    """
+    Command for billing check.
+    """
+    help = 'checks billing limits'
+
+    def handle(self, *args, **options):
+        header = False
+        for bill in Billing.objects.all():
+            if not bill.in_limits():
+                if not header:
+                    self.stdout.write('Following billings are over limit:')
+                    header = True
+                self.stdout.write(
+                    ' * {0}'.format(bill)
+                )
--- weblate/billing/tests.py
+++ weblate/billing/tests.py
@@ -18,20 +18,45 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from StringIO import StringIO
+
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.core.management import call_command
 
 from weblate.billing.models import Plan, Billing
 from weblate.trans.models import Project
 
 
 class BillingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='bill')
+        self.plan = Plan.objects.create(name='test', limit_projects=1, price=0)
+        self.billing = Billing.objects.create(user=self.user, plan=self.plan)
+        self.projectnum = 0
+
+    def add_project(self):
+        name = 'test{0}'.format(self.projectnum)
+        self.projectnum += 1
+        self.billing.projects.add(
+            Project.objects.create(name=name, slug=name)
+        )
+
     def test_limit_projects(self):
-        user = User.objects.create(username='bill')
-        plan = Plan.objects.create(name='test', limit_projects=1, price=0)
-        billing = Billing.objects.create(user=user, plan=plan)
-        self.assertTrue(billing.in_limits())
-        billing.projects.add(Project.objects.create(name='test1', slug='1'))
-        self.assertTrue(billing.in_limits())
-        billing.projects.add(Project.objects.create(name='test2', slug='2'))
-        self.assertFalse(billing.in_limits())
+        self.assertTrue(self.billing.in_limits())
+        self.add_project()
+        self.assertTrue(self.billing.in_limits())
+        self.add_project()
+        self.assertFalse(self.billing.in_limits())
+
+    def test_commands(self):
+        out = StringIO()
+        call_command('billing_check', stdout=out)
+        self.assertEqual(out.getvalue(), '')
+        self.add_project()
+        self.add_project()
+        call_command('billing_check', stdout=out)
+        self.assertEqual(
+            out.getvalue(),
+            'Following billings are over limit:\n * bill (test)\n'
+        )
