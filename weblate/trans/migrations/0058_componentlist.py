6b6cc1a406338839f955208e2b1e940e3c34f6ab
Jitka Novotna <jitka@ucw.cz>
2016-02-09 17:13:08 +0100

    migration deleted

--- weblate/trans/migrations/0058_componentlist.py
+++ weblate/trans/migrations/0058_componentlist.py
@@ -15,7 +15,8 @@ class Migration(migrations.Migration):
             name='ComponentList',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=100)),
+                ('name', models.CharField(help_text='Name to display', unique=True, max_length=100, verbose_name='Component list name')),
+                ('slug', models.SlugField(help_text='Name used in URLs and file names.', unique=True, max_length=100, verbose_name='URL slug')),
                 ('components', models.ManyToManyField(to='trans.SubProject')),
             ],
             options={
