4d6fe1e9cec1c3ea7aa85cc49b1beb0970c8c9c2
Jitka Novotna <jitka@ucw.cz>
2016-02-11 15:55:09 +0100

    get instead []

--- weblate/accounts/forms.py
+++ weblate/accounts/forms.py
@@ -243,9 +243,9 @@ class DashboardSettingsForm(forms.ModelForm):
         '''
         Check if component list is selected when required.
         '''
-        if self.cleaned_data['dashboard_view'] == \
+        if self.cleaned_data.get('dashboard_view') == \
                 Profile.DASHBOARD_COMPONENT_LIST and \
-                self.cleaned_data['dashboard_component_list'] is None:
+                self.cleaned_data.get('dashboard_component_list') is None:
             raise forms.ValidationError(
                 _("Component list must be selected when used as default."))
 
