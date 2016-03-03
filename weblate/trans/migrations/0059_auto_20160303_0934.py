b369c30fadf32c289a2a19ace22ed1d32ec0d086
Michal Čihař <michal@cihar.com>
2016-03-03 16:48:53 +0100

    Ignore errors when profile does not exist
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/migrations/0059_auto_20160303_0934.py
+++ weblate/trans/migrations/0059_auto_20160303_0934.py
@@ -9,15 +9,22 @@ def fill_in_subscriptions(apps, schema_editor):
     """Adds subscriptions to owners or ACL enabled users"""
     Project = apps.get_model('trans', 'Project')
     Group = apps.get_model('auth', 'Group')
+    Profile = apps.get_model('accounts', 'Profile')
 
     for project in Project.objects.all():
         for owner in project.owners.all():
-            owner.profile.subscriptions.add(project)
+            try:
+                owner.profile.subscriptions.add(project)
+            except Profile.DoesNotExist:
+                pass
 
         if project.enable_acl:
             group = Group.objects.get(name=project.name)
             for user in group.user_set.all():
-                user.profile.subscriptions.add(project)
+                try:
+                    user.profile.subscriptions.add(project)
+                except Profile.DoesNotExist:
+                    pass
 
 
 class Migration(migrations.Migration):
