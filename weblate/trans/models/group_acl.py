5f2a8092f75cff608f74c9e48a1eee0534953f40
Michal Čihař <michal@cihar.com>
2016-03-07 09:32:28 +0100

    Use unicode literals
    It's needed for __str__ method properly handle unicode.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/models/group_acl.py
+++ weblate/trans/models/group_acl.py
@@ -20,6 +20,8 @@
 
 """Group ACL."""
 
+from __future__ import unicode_literals
+
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.encoding import python_2_unicode_compatible
