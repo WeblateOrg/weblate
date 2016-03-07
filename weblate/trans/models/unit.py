e2095888417906312606b44fae71f4cfeb1cd5b0
Michal Čihař <michal@cihar.com>
2016-03-07 12:03:57 +0100

    Add missing unicode literals usage
    Fixes rb#580
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/models/unit.py
+++ weblate/trans/models/unit.py
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from __future__ import unicode_literals
+
 import functools
 import traceback
 import multiprocessing
--- weblate/trans/models/unitdata.py
+++ weblate/trans/models/unitdata.py
@@ -19,6 +19,7 @@
 #
 
 from __future__ import unicode_literals
+
 from django.db import models
 from django.db.models import Count
 from django.contrib.auth.models import User
