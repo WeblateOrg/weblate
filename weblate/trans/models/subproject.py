09e4c924a78beec22e34c326b77622839ba44e9d
Michal Čihař <michal@cihar.com>
2016-03-08 16:08:33 +0100

    Do not generate Change on unsaved object
    Fixes #1005
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/models/subproject.py
+++ weblate/trans/models/subproject.py
@@ -854,11 +854,12 @@ class SubProject(models.Model, PercentMixin, URLMixin, PathMixin):
             translation,
             str(error)
         )
-        Change.objects.create(
-            subproject=self,
-            translation=translation,
-            action=Change.ACTION_PARSE_ERROR,
-        )
+        if self.id:
+            Change.objects.create(
+                subproject=self,
+                translation=translation,
+                action=Change.ACTION_PARSE_ERROR,
+            )
         raise ParseError(str(error))
 
     def update_branch(self, request=None, method=None):
