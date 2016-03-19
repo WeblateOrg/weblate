2d399951708b4d6e96c76ab64c9abfec310ac3fb
Michal Čihař <michal@cihar.com>
2016-03-19 10:14:47 +0100

    Remove phrase stripping from Glosbe MT
    It doesn't seem to be neede.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/machine/glosbe.py
+++ weblate/trans/machine/glosbe.py
@@ -48,7 +48,7 @@ class GlosbeTranslation(MachineTranslation):
             'from': appsettings.SOURCE_LANGUAGE,
             'dest': language,
             'format': 'json',
-            'phrase': text.strip(',.:?! \n\r').encode('utf-8')
+            'phrase': text.encode('utf-8')
         }
         response = self.json_req(
             'http://glosbe.com/gapi/translate',
