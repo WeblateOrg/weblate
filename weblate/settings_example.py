4662f0963440b500a01d43b4a6e828a44279b0b1
Michal Čihař <michal@cihar.com>
2016-03-22 10:14:38 +0100

    Add basic throttling setup
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/settings_example.py
+++ weblate/settings_example.py
@@ -561,7 +561,15 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
 }
 
 # Example for restricting access to logged in users
