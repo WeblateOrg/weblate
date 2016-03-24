79dee932687d774f26386eb2b33cd0f0b6360451
Michal Čihař <michal@cihar.com>
2016-03-24 19:33:43 +0100

    Simplify permissions setup for API
    We do explicit check for every write operation, so do not do any
    permission check at REST framework level.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/settings_example.py
+++ weblate/settings_example.py
@@ -561,7 +561,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
