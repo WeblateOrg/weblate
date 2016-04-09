6e3796d6870014d5ef5efb7829c283d4f2692e61
Michal Čihař <michal@cihar.com>
2016-04-09 09:45:45 +0200

    Use default theme by default
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/conf.py
+++ docs/conf.py
@@ -106,14 +106,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('READTHEDOCS', None) == 'True':
-    html_theme = 'default'
-else:
-    try:
-        import alabaster
-        html_theme = alabaster.__name__
-    except ImportError:
-        html_theme = 'default'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
