b1e0ab905930651e70670339035a677d54a3d01d
Michal Čihař <michal@cihar.com>
2016-06-21 14:09:30 +0200

    Use unicode literals
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/util.py
+++ weblate/trans/util.py
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from __future__ import unicode_literals
+
 from importlib import import_module
 import hashlib
 import os
