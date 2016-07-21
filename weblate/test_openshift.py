90fb85899a30e3936023271c29c5716cba60a27a
Michal Čihař <michal@cihar.com>
2016-07-21 15:39:27 +0200

    Use Django settings instead of faking it
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/test_openshift.py
+++ weblate/test_openshift.py
@@ -21,11 +21,10 @@
 
 from unittest import TestCase
 import os
+
+from django.conf import BaseSettings
+
 from weblate.openshiftlib import get_openshift_secret_key, import_env_vars
-
-
-class FakeStorage(object):
-    pass
 
 
 class OpenShiftTest(TestCase):
@@ -56,26 +55,26 @@ class OpenShiftTest(TestCase):
         del os.environ['OPENSHIFT_APP_UUID']
 
     def test_import_env_string(self):
-        storage = FakeStorage()
+        storage = BaseSettings()
         import_env_vars({'WEBLATE_FOO': '"bar"'}, storage)
         self.assertEqual(storage.FOO, 'bar')
 
     def test_import_env_int(self):
-        storage = FakeStorage()
+        storage = BaseSettings()
         import_env_vars({'WEBLATE_FOO': '1234'}, storage)
         self.assertEqual(storage.FOO, 1234)
 
     def test_import_env_tuple(self):
-        storage = FakeStorage()
+        storage = BaseSettings()
         import_env_vars({'WEBLATE_FOO': '(1, 2)'}, storage)
         self.assertEqual(storage.FOO, (1, 2))
 
     def test_import_env_env(self):
-        storage = FakeStorage()
+        storage = BaseSettings()
         import_env_vars({'WEBLATE_FOO': '"$BAR"', 'BAR': 'baz'}, storage)
         self.assertEqual(storage.FOO, 'baz')
 
     def test_import_env_raw(self):
-        storage = FakeStorage()
+        storage = BaseSettings()
         import_env_vars({'WEBLATE_FOO': '(r"/project/(.*)$$",)'}, storage)
         self.assertEqual(storage.FOO, ('/project/(.*)$',))
