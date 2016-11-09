609df48d3baecc7122818e071ca66a857cf64e54
Michal Čihař <michal@cihar.com>
2016-11-09 11:59:51 +0100

    Consistent UI for help
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/forms.py
+++ weblate/trans/forms.py
@@ -79,7 +79,9 @@ EDITOR_TEMPLATE = '''
 </div>
 '''
 PLURALS_TEMPLATE = '''
-<p class="help-block pull-right flip"><a href="{0}">{1}</a></p>
+<p class="help-block pull-right flip">
+<a href="{0}" title="{1}"><i class="fa fa-question-circle-o" aria-hidden="true"></i></a>
+</p>
 <p class="help-block">{2}</p>
 '''
 COPY_TEMPLATE = '''
