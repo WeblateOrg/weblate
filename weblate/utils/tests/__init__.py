dcdd854153dedb2a2e17dcf749081fd0ffca115f
Michal Čihař <michal@cihar.com>
2016-11-09 11:36:17 +0100

    Move tests to utils module
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/utils/tests/__init__.py
+++ weblate/utils/tests/__init__.py
@@ -17,6 +17,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from django.test import TestCase
-
-# Create your tests here.
