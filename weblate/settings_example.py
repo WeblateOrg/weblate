ae67f8da38881edd1828c28a516a812f9c153dce
Michal Čihař <michal@cihar.com>
2017-04-28 10:09:56 +0200

    Avoid double association by email in pipeline
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/settings_example.py
+++ weblate/settings_example.py
@@ -233,7 +233,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'weblate.accounts.pipeline.require_email',
