d00adeab3b959ee8f101f2a996e197afc093c8b3
Michal Čihař <michal@cihar.com>
2017-05-03 09:02:15 +0200

    Use https for gnu.org
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/vcs.py
+++ weblate/trans/vcs.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Minimal distributed version control system abstraction for Weblate needs."""
 from __future__ import unicode_literals
