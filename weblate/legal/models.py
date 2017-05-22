e6dfbf754a8519ce0a5b49c2eea8a5b6786a63e3
Michal Čihař <michal@cihar.com>
2017-05-22 17:00:44 +0200

    Add model to track agreement with terms of service
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/legal/models.py
+++ weblate/legal/models.py
@@ -1,5 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2017 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 from __future__ import unicode_literals
 
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# Current TOS date
+TOS_DATE = date(2017, 6, 1)
+
+
+@python_2_unicode_compatible
+class Agreement(models.Model):
+    user = models.OneToOneField(User, unique=True, editable=False)
+    tos = models.DateField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta(object):
+        ordering = ['user__username']
+
+    def __str__(self):
+        return '{0}:{1}'.format(self.user.username, self.tos)
