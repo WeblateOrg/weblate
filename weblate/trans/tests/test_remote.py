37feb30fac306da0c4ff4d2bfb99f11672cf2e28
Michal Čihař <michal@cihar.com>
2017-06-18 14:43:05 +0200

    Revert "Add multiprocess based tests for updating"
    This reverts commit 3f5ddcbe03aa22ff934af0fb815fd831872c284b.
    
    Apparently using multiple processes with Django ORM is not way to go,
    see https://code.djangoproject.com/ticket/20562.
    
    Also there is quite high chance that this test would not actually
    manifest described problem.

--- weblate/trans/tests/test_remote.py
+++ weblate/trans/tests/test_remote.py
@@ -19,7 +19,6 @@
 #
 """Test for changes done in remote repository."""
 
-from multiprocessing import Process
 import shutil
 import os
 from unittest import SkipTest
@@ -171,25 +170,6 @@ class MultiRepoTest(ViewTestCase):
         self.subproject2.merge_style = 'rebase'
         self.subproject2.save()
         self.test_update()
-
-    def test_process(self):
-        self.push_first(False)
-        self.subproject2.merge_style = 'rebase'
-        self.subproject2.save()
-        process = Process(
-            target=self.subproject2.do_update,
-            args=(self.request,)
-        )
-        process2 = Process(
-            target=self.subproject2.do_update,
-            args=(self.request,)
-        )
-        process.start()
-        process2.start()
-        process.join()
-        process2.join()
-        self.assertEqual(process.exitcode, 0)
-        self.assertEqual(process2.exitcode, 0)
 
     def test_conflict(self):
         """Test conflict handling."""
