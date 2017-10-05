2677e00df8a2eddc95ec999920cafa09a5ef4171
Michal Čihař <michal@cihar.com>
2017-10-05 16:22:10 +0200

    Allow to add own mail to the user
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/accounts/views.py
+++ weblate/accounts/views.py
@@ -631,7 +631,8 @@ def email_login(request):
         if settings.REGISTRATION_CAPTCHA:
             captcha = CaptchaForm(request, form, request.POST)
         if (captcha is None or captcha.is_valid()) and form.is_valid():
-            if form.cleaned_data['email_user']:
+            email_user = form.cleaned_data['email_user']
+            if email_user and email_user != request.user:
                 notify_account_activity(
                     form.cleaned_data['email_user'],
                     request,
