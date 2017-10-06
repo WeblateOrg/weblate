a640e715aa2f2cebc8b4f61bb45bb9d0b9a1bbf8
Michal Čihař <michal@cihar.com>
2017-10-06 15:54:31 +0200

    Update TIME_ZONE comment to match current Django
    Fixes #1651
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/settings_example.py
+++ weblate/settings_example.py
@@ -65,10 +65,7 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
