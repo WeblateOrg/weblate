a8dbc0c5705cf993862647a3fdfb195c0816c430
Michal Čihař <michal@cihar.com>
2017-11-17 19:48:36 +0100

    Clear dictionary form after adding
    Issue #1634
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/static/loader-bootstrap.js
+++ weblate/static/loader-bootstrap.js
@@ -1239,6 +1239,7 @@ $(function () {
                     form.find('tbody').html(data.results);
                     form.find('[name=words]').attr('value', data.words);
                 }
+                form.trigger('reset');
             },
             error: function (xhr, textStatus, errorThrown) {
                 decreaseLoading('#glossary-add-loading');
