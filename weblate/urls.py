6f1f7c331f26259f7d5915b8afd8f2d6618a75a6
Michal Čihař <michal@cihar.com>
2017-12-03 21:41:46 +0100

    Avoid using deprecated javascript_catalog
    It is removed in Django 2.0
    
    Issue #1707
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/urls.py
+++ weblate/urls.py
@@ -721,7 +721,7 @@ urlpatterns = [
     ),
     url(
         r'^js/i18n/$',
-        django.views.i18n.javascript_catalog,
+        django.views.i18n.JavaScriptCatalog.as_view(),
         {'packages': ('weblate',)},
         name='js-catalog'
     ),
