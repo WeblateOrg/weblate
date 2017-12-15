beea8329914b76eb495e1f32f4fd5b661ea493fa
Michal Čihař <michal@cihar.com>
2017-12-15 15:13:35 +0100

    Setting version to 2.19
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ copyright = '2012 - 2017 Michal Čihař'
 # built documents.
 #
 # The short X.Y version.
-version = '2.18'
+version = '2.19'
 # The full version, including alpha/beta/rc tags.
 release = version
 
