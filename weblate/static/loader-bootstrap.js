eba037fa53605555eed8546da6207a30c832eb0a
Manuel Laggner <mlaggner@users.noreply.github.com>
2018-01-16 19:47:49 +0100

    Added visualization for machine translation score (#1792)
    In the machine translation tab there is a new column with a progress bar
    indicating the quality score of the machine translation
    
    Fixes #1785
    
    Signed-off-by: Manuel Laggner <manuel.laggner@gmail.com>

--- weblate/static/loader-bootstrap.js
+++ weblate/static/loader-bootstrap.js
@@ -256,6 +256,17 @@ function processMachineTranslation(data) {
             newRow.append($('<td/>').attr('class', 'target').attr('lang', data.lang).attr('dir', data.dir).text(el.text));
             newRow.append($('<td/>').text(el.source));
             newRow.append($('<td/>').text(el.service));
+            /* Quality score as bar with the text */
+            newRow.append($(
+                '<td>' +
+                '<div class="progress" title="' + el.quality + ' / 100">' +
+                '<div class="progress-bar ' +
+                ( el.quality >= 70 ? 'progress-bar-success' : el.quality >= 50 ? 'progress-bar-warning' : 'progress-bar-danger' ) + '"' +
+                ' role="progressbar" aria-valuenow="' + el.quality + '"' +
+                ' aria-valuemin="0" aria-valuemax="100" style="width: ' + el.quality + '%;"></div>' +
+                '</div>' +
+                '</td>'
+            ));
             /* Translators: Verb for copy operation */
             newRow.append($(
                 '<td>' +
