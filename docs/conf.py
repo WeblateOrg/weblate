96ed453cf650a5237e40e867b9cf50bbad44a59b
Michal Čihař <michal@cihar.com>
2018-01-22 20:19:11 +0100

    Update copyright year
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Weblate'
-copyright = '2012 - 2017 Michal Čihař'
+copyright = '2012 - 2018 Michal Čihař'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2012 - 2017 Michal Čihař <michal@cihar.com>
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
 #
 # This file is part of Weblate <https://weblate.org/>
 #
