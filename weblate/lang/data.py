e33f525eafe1364436f6d49e8f27d45adc9ff625
Ilya Ivanov <ilya@devopsx.com>
2018-01-23 02:26:41 +0700

    add Korean alias

--- weblate/lang/data.py
+++ weblate/lang/data.py
@@ -346,6 +346,7 @@ LOCALE_ALIASES = {
     'schinese': 'zh_Hans',
     'chinese_zh': 'zh_Hant',
     'tchinese': 'zh_Hant',
+    'korean': 'ko_KR',
     'dutch_be': 'nl_BE',
     'english': 'en',
     'english-uk': 'en_GB',
