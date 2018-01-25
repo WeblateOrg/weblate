f824aa6b618810674a92667e8d4d97015c3934b1
Michal Čihař <michal@cihar.com>
2018-01-25 13:57:42 +0100

    Use apps to access to related model
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/models/source.py
+++ weblate/trans/models/source.py
@@ -18,10 +18,12 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from django.apps import apps
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.urls import reverse
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.functional import cached_property
 
 from weblate.trans.validators import validate_check_flags
 from weblate.trans.util import PRIORITY_CHOICES
@@ -52,6 +54,12 @@ class Source(models.Model):
         app_label = 'trans'
         unique_together = ('id_hash', 'subproject')
         ordering = ('id', )
+
+    @cached_property
+    def units_model(self):
+        # Can't cache this property until all the models are loaded.
+        apps.check_models_ready()
+        return apps.get_model('trans', 'Unit')
 
     def __init__(self, *args, **kwargs):
         super(Source, self).__init__(*args, **kwargs)
@@ -87,8 +95,7 @@ class Source(models.Model):
             return None
 
     def units(self):
-        from weblate.trans.models import Unit
-        return Unit.objects.filter(
+        return self.units_model.objects.filter(
             id_hash=self.id_hash,
             translation__subproject=self.subproject
         )
