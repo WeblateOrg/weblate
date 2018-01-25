b0678883def36070e87f164326e3a1b086bdf283
Michal Čihař <michal@cihar.com>
2018-01-25 15:40:32 +0100

    Coding style fixes in the mypretty module
    Apparently this is going to stay for quite some time.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/tests/mypretty.py
+++ weblate/trans/tests/mypretty.py
@@ -1,18 +1,23 @@
 # from : https://github.com/gabrielfalcao/HTTPretty/issues/242
 from httpretty import HTTPretty as OriginalHTTPretty
+import httpretty
+import httpretty.core
 
 try:
-    from requests.packages.urllib3.contrib.pyopenssl import inject_into_urllib3, extract_from_urllib3
+    from requests.packages.urllib3.contrib.pyopenssl import (
+        inject_into_urllib3, extract_from_urllib3
+    )
     pyopenssl_override = True
-except:
+except ImportError:
     pyopenssl_override = False
 
 
 class MyHTTPretty(OriginalHTTPretty):
-    """ pyopenssl monkey-patches the default ssl_wrap_socket() function in the 'requests' library,
-    but this can stop the HTTPretty socket monkey-patching from working for HTTPS requests.
-    Our version extends the base HTTPretty enable() and disable() implementations to undo
-    and redo the pyopenssl monkey-patching, respectively.
+    """ pyopenssl monkey-patches the default ssl_wrap_socket() function in the
+    'requests' library, but this can stop the HTTPretty socket monkey-patching
+    from working for HTTPS requests.  Our version extends the base HTTPretty
+    enable() and disable() implementations to undo and redo the pyopenssl
+    monkey-patching, respectively.
     """
 
     @classmethod
@@ -33,11 +38,9 @@ class MyHTTPretty(OriginalHTTPretty):
 # Substitute in our version
 HTTPretty = MyHTTPretty
 
-import httpretty.core
 httpretty.core.httpretty = MyHTTPretty
 
 # May need to set other module-level attributes here, e.g. enable, reset etc,
 # depending on your needs
-import httpretty
 httpretty.httpretty = MyHTTPretty
 httpretty.register_uri = httpretty.register_uri
