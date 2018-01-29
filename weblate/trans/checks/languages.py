af00c8e4123856a5cc48a92e18b1b21283c8c93b
Michal Čihař <michal@cihar.com>
2018-01-29 13:45:51 +0100

    Update language-data
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/checks/languages.py
+++ weblate/trans/checks/languages.py
@@ -24,6 +24,7 @@
 # pylint: disable=line-too-long,too-many-lines
 
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_noop as _
 
 
 # Generated list of languages, see scripts/generate-language-data
