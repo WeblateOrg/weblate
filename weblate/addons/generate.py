b6b0e131ac5f96dbd693ec9f28a950a025e02215
Michal Čihař <michal@cihar.com>
2018-02-08 14:31:29 +0100

    Use better icon for generate addon
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/addons/generate.py
+++ weblate/addons/generate.py
@@ -39,6 +39,7 @@ class GenerateFileAddon(BaseAddon):
         'about the translation.'
     )
     settings_form = GenerateForm
+    icon = 'bar-chart'
 
     @classmethod
     def is_compatible(cls, component):
