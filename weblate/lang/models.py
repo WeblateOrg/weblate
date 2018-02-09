7acbc943b2a0ce8f45967475d7eccd2b74ba7d8e
Michal Čihař <michal@cihar.com>
2018-02-09 09:28:12 +0100

    Always update plural type, it is generated field
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/lang/models.py
+++ weblate/lang/models.py
@@ -593,6 +593,5 @@ class Plural(models.Model):
             }
 
     def save(self, *args, **kwargs):
-        if self.type is None:
-            self.type = get_plural_type(self.language.code, self.equation)
+        self.type = get_plural_type(self.language.code, self.equation)
         super(Plural, self).save(*args, **kwargs)
