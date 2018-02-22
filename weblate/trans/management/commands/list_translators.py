69ac20f259220bd0890f735e6a1c091c6eb5a871
Michal Čihař <michal@cihar.com>
2018-02-22 19:44:11 +0100

    Use unicode literals in management command
    Fixes #1870
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/management/commands/list_translators.py
+++ weblate/trans/management/commands/list_translators.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+from __future__ import unicode_literals
 
 from weblate.trans.management.commands import WeblateComponentCommand
 from weblate.trans.models.change import Change
