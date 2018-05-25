1dbc92aa5d19272fd40e5b771532d4bebd11fde1
Georgy Bekh-Ivanov <georgebekh@mail.ru>
2018-05-25 11:47:17 +0300

    don't break permissions while using docker-compose
    Signed-off-by: Georgy Bekh-Ivanov <georgebekh@mail.ru>

--- rundev.sh
+++ rundev.sh
@@ -5,9 +5,12 @@ then
     cp ./weblate/settings_example.py ./weblate/settings.py
 fi
 
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
 export COMPOSE_FILE=docker-compose.dev.yml
 
 docker-compose build
 
-docker-compose up -d
+docker-compose up -d --force-recreate
 docker-compose logs -f
