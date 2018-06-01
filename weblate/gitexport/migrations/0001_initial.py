ded71af21146aaa5aef580e3d30a598c1ed6e982
Michal Čihař <michal@cihar.com>
2018-06-01 08:54:51 +0200

    Move gitexport initial migration to later phase
    We need to allow later installation of the app, but it has become
    problematic after SubProject -> Component rename as the migration had to
    be executed before rename, what is no longer achievable on new
    installations.
    
    Instead the initial migration is now empty and there is new migration
    which does it's job and is executed after renaming.
    
    On the other side changing depenendecies of initial migration and moving
    it to Component would not work for existing installs - they would have
    applied migration without its dependencies.
    
    Fixes #2065
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/gitexport/migrations/0001_initial.py
+++ weblate/gitexport/migrations/0001_initial.py
@@ -4,22 +4,6 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from weblate.gitexport.models import get_export_url, SUPPORTED_VCS
-
-
-def set_export_url(apps, schema_editor):
-    Component = apps.get_model('trans', 'Component')
-    matching = Component.objects.filter(
-        vcs__in=SUPPORTED_VCS
-    ).exclude(
-        repo__startswith='weblate:/'
-    )
-    for component in matching:
-        new_url = get_export_url(component)
-        if component.git_export != new_url:
-            component.git_export = new_url
-            component.save()
-
 
 class Migration(migrations.Migration):
 
@@ -27,13 +11,5 @@ class Migration(migrations.Migration):
         ('trans', '0069_source_screenshot'),
     ]
 
-    run_before = [
-        ('trans', '0131_auto_20180416_1610'),
-    ]
-
     operations = [
-        migrations.RunPython(
-            set_export_url,
-            reverse_code=set_export_url,
-        )
     ]
