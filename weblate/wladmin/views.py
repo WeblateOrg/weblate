8548d0eb48b359bf84429f195db28410c7324577
Michal Čihař <michal@cihar.com>
2018-06-01 09:14:49 +0200

    Move performance checks to separate module
    Issue #2061
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/wladmin/views.py
+++ weblate/wladmin/views.py
@@ -20,29 +20,17 @@
 
 from __future__ import unicode_literals
 
-import os.path
-
-from django.conf import settings
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
 
-import six
-
-from weblate.trans.models import Component, IndexUpdate
-from weblate import settings_example
-from weblate.accounts.avatar import HAS_LIBRAVATAR
-from weblate.trans.util import HAS_PYUCA, check_domain
+from weblate.trans.models import Component
 from weblate.vcs.ssh import (
     generate_ssh_key, get_key_data, add_host_key,
     get_host_keys, can_generate_key
 )
 from weblate.utils import messages
-from weblate.utils.site import get_site_url, get_site_domain
 from weblate.wladmin.models import ConfigurationError
-
-GOOD_CACHE = frozenset((
-    'MemcachedCache', 'PyLibMCCache', 'DatabaseCache', 'RedisCache'
-))
+from weblate.wladmin.performance import run_checks
 
 
 def report(request, admin_site):
@@ -54,23 +42,6 @@ def report(request, admin_site):
         "admin/report.html",
         context,
     )
-
-
-def get_first_loader():
-    """Return first loader from settings"""
-    if settings.TEMPLATES:
-        loaders = settings.TEMPLATES[0].get(
-            'OPTIONS', {}
-        ).get(
-            'loaders', [['']]
-        )
-    else:
-        loaders = settings.TEMPLATE_LOADERS
-
-    if isinstance(loaders[0], six.string_types):
-        return loaders[0]
-
-    return loaders[0][0]
 
 
 def handle_dismiss(request):
@@ -92,150 +63,9 @@ def performance(request, admin_site):
     """Show performance tuning tips."""
     if request.method == 'POST':
         return handle_dismiss(request)
-    checks = []
-    # Check for debug mode
-    checks.append((
-        _('Debug mode'),
-        not settings.DEBUG,
-        'production-debug',
-        settings.DEBUG,
-    ))
-    # Check for domain configuration
-    checks.append((
-        _('Site domain'),
-        check_domain(get_site_domain()),
-        'production-site',
-        get_site_url(),
-    ))
-    # Check database being used
-    checks.append((
-        _('Database backend'),
-        "sqlite" not in settings.DATABASES['default']['ENGINE'],
-        'production-database',
-        settings.DATABASES['default']['ENGINE'],
-    ))
-    # Check configured admins
-    checks.append((
-        _('Site administrator'),
-        len(settings.ADMINS) > 0 or
-        'noreply@weblate.org' in [x[1] for x in settings.ADMINS],
-        'production-admins',
-        ', '.join([x[1] for x in settings.ADMINS]),
-    ))
-    # Check offloading indexing
-    checks.append((
-        # Translators: Indexing is postponed to cron job
-        _('Indexing offloading'),
-        settings.OFFLOAD_INDEXING,
-        'production-indexing',
-        settings.OFFLOAD_INDEXING
-    ))
-    if settings.OFFLOAD_INDEXING:
-        if IndexUpdate.objects.count() < 20:
-            index_updates = True
-        elif IndexUpdate.objects.count() < 200:
-            index_updates = None
-        else:
-            index_updates = False
-
-        checks.append((
-            # Translators: Indexing is postponed to cron job
-            _('Indexing offloading processing'),
-            index_updates,
-            'production-indexing',
-            IndexUpdate.objects.count(),
-        ))
-    # Check for sane caching
-    caches = settings.CACHES['default']['BACKEND'].split('.')[-1]
-    if caches in GOOD_CACHE:
-        # We consider these good
-        caches = True
-    elif caches in ['DummyCache']:
-        # This one is definitely bad
-        caches = False
-    else:
-        # These might not be that bad
-        caches = None
-    checks.append((
-        _('Django caching'),
-        caches,
-        'production-cache',
-        settings.CACHES['default']['BACKEND'],
-    ))
-    # Avatar caching
-    checks.append((
-        _('Avatar caching'),
-        'avatar' in settings.CACHES,
-        'production-cache-avatar',
-        settings.CACHES['avatar']['BACKEND']
-        if 'avatar' in settings.CACHES else '',
-    ))
-    # Check email setup
-    default_mails = (
-        'root@localhost',
-        'webmaster@localhost',
-        'noreply@weblate.org'
-        'noreply@example.com'
-    )
-    checks.append((
-        _('Email addresses'),
-        (
-            settings.SERVER_EMAIL not in default_mails and
-            settings.DEFAULT_FROM_EMAIL not in default_mails
-        ),
-        'production-email',
-        ', '.join((settings.SERVER_EMAIL, settings.DEFAULT_FROM_EMAIL)),
-    ))
-    # libravatar library
-    checks.append((
-        _('Federated avatar support'),
-        HAS_LIBRAVATAR,
-        'production-avatar',
-        HAS_LIBRAVATAR,
-    ))
-    # pyuca library
-    checks.append((
-        _('pyuca library'),
-        HAS_PYUCA,
-        'production-pyuca',
-        HAS_PYUCA,
-    ))
-    # Cookie signing key
-    checks.append((
-        _('Secret key'),
-        settings.SECRET_KEY != settings_example.SECRET_KEY,
-        'production-secret',
-        '',
-    ))
-    # Allowed hosts
-    checks.append((
-        _('Allowed hosts'),
-        len(settings.ALLOWED_HOSTS) > 0,
-        'production-hosts',
-        ', '.join(settings.ALLOWED_HOSTS),
-    ))
-
-    loader = get_first_loader()
-    # Cached template loader
-    checks.append((
-        _('Cached template loader'),
-        'cached.Loader' in loader,
-        'production-templates',
-        loader,
-    ))
-
-    # Check for serving static files
-    checks.append((
-        _('Admin static files'),
-        os.path.exists(
-            os.path.join(settings.STATIC_ROOT, 'admin', 'js', 'core.js')
-        ),
-        'production-admin-files',
-        settings.STATIC_ROOT,
-    ))
 
     context = admin_site.each_context(request)
-    context['checks'] = checks
+    context['checks'] = run_checks()
     context['errors'] = ConfigurationError.objects.filter(ignored=False)
 
     return render(
