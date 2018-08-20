4c4958348207ccd32a0f640cc2396b00260c2e6e
Michal Čihař <michal@cihar.com>
2018-08-20 12:52:38 +0200

    Add basic Celery integration
    There are not taks, just the basic infrastructure.
    
    Issue #2103
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/__init__.py
+++ weblate/__init__.py
@@ -23,6 +23,10 @@ import os
 from weblate.vcs.base import RepositoryException
 from weblate.vcs.git import GitRepository
 
+from weblate.celery import app as celery_app
+
+__all__ = ('celery_app',)
+
 
 def get_root_dir():
     """Return Weblate root dir."""
