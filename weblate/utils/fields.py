e99a5759c3da4bfd21f27502cde4fb5da92943de
Michal Čihař <michal@cihar.com>
2018-08-27 23:19:43 +0200

    Return back to using **kwargs
    Using new API seems to break in some cases.
    
    This reverts commit 6ff219f0c26ac740815c72a42672e3c19ddca9e2.
    This reverts commit b7ce6739dfe15100eb9c643c5268c9cea6a42ca4.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/utils/fields.py
+++ weblate/utils/fields.py
@@ -45,10 +45,10 @@ class JSONField(models.TextField):
             return json.dumps(value, cls=DjangoJSONEncoder)
         return super(JSONField, self).get_prep_value(value)
 
-    def from_db_value(self, value, expression, connection):
+    def from_db_value(self, value, *args, **kwargs):
         return self.to_python(value)
 
-    def get_db_prep_save(self, value, connection):
+    def get_db_prep_save(self, value, *args, **kwargs):
         if not value:
             value = {}
         return json.dumps(value, cls=DjangoJSONEncoder)
