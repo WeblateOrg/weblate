f4b5bb57fa2616f52d87d768789b036e55c5a38f
Michal Čihař <michal@cihar.com>
2018-08-27 08:14:54 +0200

    Improve perfomance of index rebuild
    Rebuilt it per language so that we do not have to keep open all indices.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/management/commands/rebuild_index.py
+++ weblate/trans/management/commands/rebuild_index.py
@@ -19,8 +19,9 @@
 #
 
 from weblate.trans.management.commands import WeblateComponentCommand
-from weblate.trans.search import Fulltext
 from weblate.lang.models import Language
+from weblate.trans.models import Unit
+from weblate.trans.search import Fulltext
 
 
 class Command(WeblateComponentCommand):
@@ -52,16 +53,7 @@ class Command(WeblateComponentCommand):
             index = fulltext.get_target_index(lang.code)
             index.optimize()
 
-    def handle(self, *args, **options):
-        fulltext = Fulltext()
-        # Optimize index
-        if options['optimize']:
-            self.optimize_index(fulltext)
-            return
-        # Optionally rebuild indices from scratch
-        if options['clean']:
-            fulltext.cleanup()
-
+    def process_filtered(self, fulltext, **options):
         # Open writer
         source_writer = fulltext.get_source_index().writer()
         target_writers = {}
@@ -88,3 +80,37 @@ class Command(WeblateComponentCommand):
             source_writer.commit()
             for code in target_writers:
                 target_writers[code].commit()
+
+    def process_all(self, fulltext):
+        source_writer = fulltext.get_source_index().writer()
+
+        try:
+            for language in Language.objects.have_translation():
+                self.stdout.write('Processing {0}'.format(language.code))
+                writer = fulltext.get_target_index(language.code).writer()
+                try:
+                    units = Unit.objects.filter(translation__language=language)
+                    for unit in units.iterator():
+                        if unit.translation:
+                            fulltext.update_target_unit_index(writer, unit)
+                        # Update source index
+                        fulltext.update_source_unit_index(source_writer, unit)
+                finally:
+                    writer.commit()
+        finally:
+            source_writer.commit()
+
+    def handle(self, *args, **options):
+        fulltext = Fulltext()
+        # Optimize index
+        if options['optimize']:
+            self.optimize_index(fulltext)
+            return
+        # Optionally rebuild indices from scratch
+        if options['clean']:
+            fulltext.cleanup()
+
+        if options['all']:
+            self.process_all(fulltext)
+        else:
+            self.process_filtered(fulltext, **options)
