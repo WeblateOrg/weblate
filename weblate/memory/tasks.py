68c32ba429c6c734200b0eb87cb859fc5e0d2e4c
Michal Čihař <michal@cihar.com>
2018-09-12 16:48:10 +0200

    Add translation memory optimize scheduled task
    This is executed weekly to keep translation memory in a good shape.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/memory/tasks.py
+++ weblate/memory/tasks.py
@@ -79,10 +79,21 @@ def update_memory_task(self, *args, **kwargs):
         raise self.retry(exc=exc)
 
 
+@app.task
+def memory_optimize():
+    memory = TranslationMemory()
+    memory.index.optimize()
+
+
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         3600 * 24,
         memory_backup.s(),
         name='translation-memory-backup',
+    )
+    sender.add_periodic_task(
+        3600 * 24 * 7,
+        memory_optimize.s(),
+        name='translation-memory-optimize',
     )
