cf808aaf64ca361d7b1d17bbe8ff7928389d5d62
Michal Čihař <michal@cihar.com>
2018-09-21 10:59:54 +0200

    Fix exception handling in get_user
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/accounts/auth.py
+++ weblate/accounts/auth.py
@@ -55,8 +55,8 @@ class WeblateUserBackend(ModelBackend):
 
     def get_user(self, user_id):
         try:
-            user =User.objects.select_related('profile').get(pk=user_id)
-        except UserModel.DoesNotExist:
+            user = User.objects.select_related('profile').get(pk=user_id)
+        except User.DoesNotExist:
             return None
         return user if self.user_can_authenticate(user) else None
 
