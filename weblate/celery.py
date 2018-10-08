f2f42bfbc6fad7ec06ef4f4e2409b8da12d2473c
Michal Čihař <michal@cihar.com>
2018-10-08 22:23:11 +0200

    Log only errors from Celery
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/celery.py
+++ weblate/celery.py
@@ -81,4 +81,4 @@ def configure_error_handling(sender, **kargs):
     if HAS_RAVEN and hasattr(settings, 'RAVEN_CONFIG'):
         client = Client(settings.RAVEN_CONFIG['dsn'])
         register_signal(client, ignore_expected=True)
-        register_logger_signal(client, loglevel=logging.INFO)
+        register_logger_signal(client, loglevel=logging.ERROR)
