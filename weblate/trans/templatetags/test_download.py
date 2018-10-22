7bd19b4061ad6fb628c9fa51fc401525bba1dd96
root <root@data-forest-dev.localdomain>
2018-10-22 23:19:29 +0000

    added empty test for templatetags download

---
+++ weblate/trans/templatetags/test_download.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from unittest import TestCase
+from weblate.utils.views import get_page_limit
+
+class DownloadTest(TestCase):
+    def test_defaults(self):
+        self.assertEqual()
