12252a9d090238aa73aec8274f6515201c974867
Adrien Crivelli <adrien.crivelli@gmail.com>
2018-10-31 22:10:09 +1100

    Add support for XLIFF placeholders
    This should allow to translate units containing placeholders such
    as the one used by Angular i18n.
    
    PoXliff will not support placehoders, because it probably doesn't
    make sense to support XLIFF specific placeholders in a format that
    actually embed PO placeholders.
    
    Fixes #490
    Fixes #1535
    
    Signed-off-by: Adrien Crivelli <adrien.crivelli@gmail.com>

---
+++ weblate/trans/tests/test_xliff_placeholders.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""
+Tests for XLIFF rich string.
+"""
+
+from django.test import TestCase
+
+from weblate.trans.util import xliff_string_to_rich, rich_to_xliff_string
+from translate.storage.placeables.strelem import StringElem
+
+
+class XliffPlaceholdersTest(TestCase):
+
+    def test_bidirectional_xliff_string(self):
+        cases = [
+            'foo <x id="INTERPOLATION" equiv-text="{{ angularExpression }}"/> bar',
+            '',
+            'hello world',
+            'hello <p>world</p>'
+        ]
+
+        for string in cases:
+            rich = xliff_string_to_rich(string)
+            self.assertTrue(isinstance(rich, list))
+            self.assertTrue(isinstance(rich[0], StringElem))
+
+            final_string = rich_to_xliff_string(rich)
+            self.assertEqual(string, final_string)
