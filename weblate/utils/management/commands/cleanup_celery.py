089c82fe7efe3f35bda148443810adbf3cb09a90
Michal Čihař <michal@cihar.com>
2018-12-02 08:53:37 +0100

    Add management command to cleanup Celery
    See https://github.com/WeblateOrg/docker/issues/102
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/utils/management/commands/cleanup_celery.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from celery.beat import PersistentScheduler
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from weblate.celery import app
+
+
+class Command(BaseCommand):
+    help = 'removes incompatible celery schedule file'
+
+    def handle(self, *args, **options):
+        scheduler = PersistentScheduler(
+            schedule_filename=settings.CELERY_BEAT_SCHEDULE_FILENAME,
+            app=app
+        )
+        try:
+            scheduler.setup_schedule()
+        except Exception as error:
+            self.stderr.write(
+                'Removing corrupted schedule file: {:r}'.format(error)
+            )
+            scheduler._remove_db()
