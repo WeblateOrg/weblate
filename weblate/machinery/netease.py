bd799ae8d85c653754caba2c7a08e9e3b5a98fbb
Michal Čihař <michal@cihar.com>
2019-01-20 22:18:36 +0100

    Fix import ordering
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/machinery/netease.py
+++ weblate/machinery/netease.py
@@ -20,16 +20,16 @@
 
 from __future__ import unicode_literals
 
+import time
+import random
+from hashlib import sha1
+
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from weblate.machinery.base import (
     MachineTranslation, MachineTranslationError, MissingConfiguration
 )
-
-import time
-import random
-from hashlib import sha1
 
 NETEASE_API_ROOT = 'https://jianwai.netease.com/api/text/trans'
 
