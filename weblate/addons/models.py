cb68f03d5ee6adb607547128c9a47d40fdfe9397
Michal Čihař <michal@cihar.com>
2019-01-25 17:02:28 +0100

    Log addon execution
    This makes it easier to debug problems with addons.
    
    Issue #2522
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/addons/models.py
+++ weblate/addons/models.py
@@ -143,24 +143,28 @@ class AddonsConf(AppConf):
 @receiver(vcs_pre_push)
 def pre_push(sender, component, **kwargs):
     for addon in Addon.objects.filter_event(component, EVENT_PRE_PUSH):
+        component.log_info('running pre_push addon: %s', addon.name)
         addon.addon.pre_push(component)
 
 
 @receiver(vcs_post_push)
 def post_push(sender, component, **kwargs):
     for addon in Addon.objects.filter_event(component, EVENT_POST_PUSH):
+        component.log_info('running post_push addon: %s', addon.name)
         addon.addon.post_push(component)
 
 
 @receiver(vcs_post_update)
 def post_update(sender, component, previous_head, **kwargs):
     for addon in Addon.objects.filter_event(component, EVENT_POST_UPDATE):
+        component.log_info('running post_update addon: %s', addon.name)
         addon.addon.post_update(component, previous_head)
 
 
 @receiver(vcs_pre_update)
 def pre_update(sender, component, **kwargs):
     for addon in Addon.objects.filter_event(component, EVENT_PRE_UPDATE):
+        component.log_info('running pre_update addon: %s', addon.name)
         addon.addon.pre_update(component)
 
 
@@ -170,6 +174,7 @@ def pre_commit(sender, translation, author, **kwargs):
         translation.component, EVENT_PRE_COMMIT
     )
     for addon in addons:
+        component.log_info('running pre_commit addon: %s', addon.name)
         addon.addon.pre_commit(translation, author)
 
 
@@ -179,6 +184,7 @@ def post_commit(sender, translation, **kwargs):
         translation.component, EVENT_POST_COMMIT
     )
     for addon in addons:
+        component.log_info('running post_commit addon: %s', addon.name)
         addon.addon.post_commit(translation)
 
 
@@ -188,6 +194,7 @@ def post_add(sender, translation, **kwargs):
         translation.component, EVENT_POST_ADD
     )
     for addon in addons:
+        component.log_info('running post_add addon: %s', addon.name)
         addon.addon.post_add(translation)
 
 
@@ -197,6 +204,7 @@ def unit_pre_create_handler(sender, unit, **kwargs):
         unit.translation.component, EVENT_UNIT_PRE_CREATE
     )
     for addon in addons:
+        component.log_info('running unit_pre_create addon: %s', addon.name)
         addon.addon.unit_pre_create(unit)
 
 
@@ -207,6 +215,7 @@ def unit_post_save_handler(sender, instance, created, **kwargs):
         instance.translation.component, EVENT_UNIT_POST_SAVE
     )
     for addon in addons:
+        component.log_info('running unit_post_save addon: %s', addon.name)
         addon.addon.unit_post_save(instance, created)
 
 
@@ -216,4 +225,5 @@ def store_post_load_handler(sender, translation, store, **kwargs):
         translation.component, EVENT_STORE_POST_LOAD
     )
     for addon in addons:
+        component.log_info('running store_post_load addon: %s', addon.name)
         addon.addon.store_post_load(translation, store)
