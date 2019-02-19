5095762efe56bc5a4917496bb113bf2e40e04c8a
Michal Čihař <michal@cihar.com>
2019-02-19 11:17:29 +0100

    Always construct new list for filenames
    This avoids potential issue when get_filenames returns internal list.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/addons/git.py
+++ weblate/addons/git.py
@@ -61,10 +61,9 @@ class GitSquashAddon(BaseAddon):
         for origin in [component] + list(component.get_linked_childs()):
             for translation in origin.translation_set.all():
                 code = translation.language.code
-                if code in languages:
-                    languages[code].extend(translation.store.get_filenames())
-                else:
-                    languages[code] = translation.store.get_filenames()
+                if code not in languages:
+                    languages[code] = []
+                languages[code].extend(translation.store.get_filenames())
         return languages
 
     def squash_language(self, component, repository):
