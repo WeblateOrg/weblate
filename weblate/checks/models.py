c55abb51b47d625f828bcf3596015cded2ab2c0b
Michal Čihař <michal@cihar.com>
2019-03-07 16:29:27 +0100

    Add missing import
    Fixes HOSTED-6M
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/checks/models.py
+++ weblate/checks/models.py
@@ -23,6 +23,7 @@ from __future__ import unicode_literals
 from appconf import AppConf
 
 from django.db import models
+from django.db import IntegrityError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
