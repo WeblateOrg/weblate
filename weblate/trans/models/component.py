WeblateOrg/weblate

commit 44aae51b719e5bfc7b655364aae99570b44df67c

Do not allow to set branch for linked components
It might give false feeling that this value is actually used.

Signed-off-by: Michal Čihař <michal@cihar.com>

weblate/trans/models/component.py
@@ -1228,7 +1228,7 @@ class Component(models.Model, URLMixin, PathMixin):
 
     def set_default_branch(self):
         """Set default VCS branch if empty"""
-        if self.branch == '':
+        if not self.branch and not self.is_repo_link:
             self.branch = VCS_REGISTRY[self.vcs].default_branch
 
     def clean_repo_link(self):
@@ -1259,6 +1259,10 @@ class Component(models.Model, URLMixin, PathMixin):
         if self.push != '':
             raise ValidationError(
                 {'push': _('Push URL is not used when repository is linked!')}
+            )
+        if self.branch:
+            raise ValidationError(
+                {'branch': _('Repository branch is not used when repository is linked!')}
             )
         if self.git_export != '':
             raise ValidationError(

commit 530fc89725b8361cd28e2f7792a6d7e08000f0fd

Fix 'branch' rendering to repoweb url for linked components. This resolves regression from commit 44aae51b71.
Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

weblate/trans/models/component.py
@@ -593,13 +593,18 @@ class Component(models.Model, URLMixin, PathMixin):
                 return self.linked_component.get_repoweb_link(filename, line)
             return None
 
+        if self.is_repo_link:
+            repo_branch = self.linked_component.branch
+        else:
+            repo_branch = self.branch
+
         return self.repoweb % {
             'file': filename,
             '../file': filename.split('/', 1)[-1],
             '../../file': filename.split('/', 2)[-1],
             '../../../file': filename.split('/', 3)[-1],
             'line': line,
-            'branch': self.branch
+            'branch': repo_branch
         }
 
     def error_text(self, error):