eb7cb416c11aef3fe811ed721b6b1f6d76de1fd4
Michal Čihař <michal@cihar.com>
2019-05-23 09:09:05 +0200

    Tests: Adjust format checks to change in mock unit
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/checks/tests/test_format_checks.py
+++ weblate/checks/tests/test_format_checks.py
@@ -767,11 +767,11 @@ class JavaMessageFormatCheckTest(CheckTestCase):
     def test_skip(self):
         unit = MockUnit(source='source')
         self.assertTrue(self.check.should_skip(unit))
-        unit.flags = 'java-messageformat'
+        unit = MockUnit(source='source',  flags='java-messageformat')
         self.assertFalse(self.check.should_skip(unit))
-        unit.flags = 'auto-java-messageformat'
+        unit = MockUnit(source='source',  flags='auto-java-messageformat')
         self.assertTrue(self.check.should_skip(unit))
-        unit.source = '{0}'
+        unit = MockUnit(source='{0}',  flags='auto-java-messageformat')
         self.assertFalse(self.check.should_skip(unit))
 
     def test_quotes(self):
