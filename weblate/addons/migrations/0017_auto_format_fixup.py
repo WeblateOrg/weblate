1d749bcdc7a48a1c376e63b69cf3c41f28868e22
Michal Čihař <michal@cihar.com>
2019-06-25 11:37:43 +0200

    Fix escape sequence
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/addons/migrations/0017_auto_format_fixup.py
+++ weblate/addons/migrations/0017_auto_format_fixup.py
@@ -9,7 +9,7 @@ def resolve_auto_format(apps, schema_editor):
     Addon = apps.get_model("addons", "Addon")
     for addon in Addon.objects.filter(name="weblate.discovery.discovery"):
         if addon.configuration["file_format"] == "auto":
-            detect = detect_filename(addon.configuration["match"].replace("\.", "."))
+            detect = detect_filename(addon.configuration["match"].replace("\\.", "."))
             if detect is None:
                 raise Exception(
                     "Existing component discovery with auto format, can not detect "
