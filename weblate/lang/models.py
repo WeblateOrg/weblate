8c9cd9e4c424bc52aa7010bc88f173841b9d7569
Michal Čihař <michal@cihar.com>
2019-06-27 09:43:00 +0200

    UI: Show list of user languages in top navigation
    Issue #2835
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/lang/models.py
+++ weblate/lang/models.py
@@ -41,6 +41,7 @@ from django.utils.translation import ugettext_lazy
 from weblate.lang import data
 from weblate.langdata import languages
 from weblate.logger import LOGGER
+from weblate.trans.util import sort_unicode
 from weblate.utils.stats import LanguageStats
 from weblate.utils.validators import validate_pluraleq
 
@@ -348,6 +349,9 @@ class LanguageQuerySet(models.QuerySet):
 
     def order(self):
         return self.order_by('name')
+
+    def order_translated(self):
+        return sort_unicode(self, lambda x: x.name)
 
 
 def setup_lang(sender, **kwargs):
