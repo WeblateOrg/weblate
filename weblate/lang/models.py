f4df814031383b3e0937e3d67f5b67e9cf7d67b4
Michal Čihař <michal@cihar.com>
2019-07-22 15:02:18 +0200

    Languages: Fix typo in code
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/lang/models.py
+++ weblate/lang/models.py
@@ -404,7 +404,7 @@ class Language(models.Model):
 
     @property
     def show_language_code(self):
-        return self.code not in data.NO_CODE_LANGUAGES:
+        return self.code not in data.NO_CODE_LANGUAGES
 
     def get_absolute_url(self):
         return reverse('show_language', kwargs={'lang': self.code})
