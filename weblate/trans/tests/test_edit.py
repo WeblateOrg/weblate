0609f3784c6832af32f710b1001c0bb5d13e932f
Michal Čihař <michal@cihar.com>
2019-08-20 09:49:21 +0200

    Tests: Share code for generating url
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/trans/tests/test_edit.py
+++ weblate/trans/tests/test_edit.py
@@ -657,15 +657,15 @@ class EditComplexTest(ViewTestCase):
         self.assertEqual(len(unit.active_checks()), 0)
         self.assertEqual(unit.translation.stats.allchecks, 0)
         # Ignore check for all languages
-        response = self.client.post(
-            reverse('js-ignore-check-source', kwargs={'check_id': check_id, 'pk': unit.source_info.pk})
+        ignore_url = reverse(
+            'js-ignore-check-source',
+            kwargs={'check_id': check_id, 'pk': unit.source_info.pk}
         )
+        response = self.client.post(ignore_url)
         self.assertEqual(response.status_code, 403)
         self.user.is_superuser = True
         self.user.save()
-        response = self.client.post(
-            reverse('js-ignore-check-source', kwargs={'check_id': check_id, 'pk': unit.source_info.pk})
-        )
+        response = self.client.post(ignore_url)
         self.assertContains(response, 'ok')
         # Should have one less check
         unit = self.get_unit()
