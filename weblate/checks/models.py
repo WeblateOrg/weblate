dac7d73b4635f38f19d1da4b2323c552d808c715
Michal Čihař <michal@cihar.com>
2019-09-08 07:39:59 +0200

    Check: Enable safe html check by default
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/checks/models.py
+++ weblate/checks/models.py
@@ -78,6 +78,7 @@ class WeblateChecksConf(AppConf):
         'weblate.checks.markup.MarkdownLinkCheck',
         'weblate.checks.markup.MarkdownSyntaxCheck',
         'weblate.checks.markup.URLCheck',
+        'weblate.checks.markup.SafeHTMLCheck',
         'weblate.checks.placeholders.PlaceholderCheck',
         'weblate.checks.placeholders.RegexCheck',
         'weblate.checks.source.OptionalPluralCheck',
