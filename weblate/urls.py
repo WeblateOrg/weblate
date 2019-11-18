a7bb7694e9b61d45bfaf55de173fbaa1ced9a158
Michal Čihař <michal@cihar.com>
2019-11-18 20:41:00 +0100

    UI: Use bigger and rounded avatar
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/urls.py
+++ weblate/urls.py
@@ -1125,7 +1125,7 @@ real_patterns = [
 
     # Avatars
     url(
-        r'^avatar/(?P<size>(16|32|80|128))/(?P<user>[^/]+)\.png$',
+        r'^avatar/(?P<size>(24|32|128))/(?P<user>[^/]+)\.png$',
         weblate.accounts.views.user_avatar,
         name='user_avatar',
     ),
