24a460e52787008cb8947ec4c25d96ce5c41e3f3
Michal Čihař <michal@cihar.com>
2020-01-14 11:06:46 +0100

    Search: Black coding style
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate/utils/state.py
+++ weblate/utils/state.py
@@ -29,11 +29,11 @@ STATE_APPROVED = 30
 STATE_READONLY = 100
 
 STATE_CHOICES = (
-    (STATE_EMPTY, pgettext_lazy("String state", 'Empty')),
-    (STATE_FUZZY, pgettext_lazy("String state", 'Needs editing')),
-    (STATE_TRANSLATED, pgettext_lazy("String state", 'Translated')),
-    (STATE_APPROVED, pgettext_lazy("String state", 'Approved')),
-    (STATE_READONLY, pgettext_lazy("String state", 'Read only')),
+    (STATE_EMPTY, pgettext_lazy("String state", "Empty")),
+    (STATE_FUZZY, pgettext_lazy("String state", "Needs editing")),
+    (STATE_TRANSLATED, pgettext_lazy("String state", "Translated")),
+    (STATE_APPROVED, pgettext_lazy("String state", "Approved")),
+    (STATE_READONLY, pgettext_lazy("String state", "Read only")),
 )
 
 STATE_NAMES = {
