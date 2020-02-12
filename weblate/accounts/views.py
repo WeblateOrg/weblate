2393597c001e9ffdd28ab419b8026992bff236ea
Michal Čihař <michal@cihar.com>
2020-02-12 13:19:09 +0100

    Accounts: Black coding style

--- weblate/accounts/views.py
+++ weblate/accounts/views.py
@@ -887,8 +887,7 @@ class SuggestionView(ListView):
             user = get_object_or_404(User, username=self.kwargs['user'])
         allowed_projects = self.request.user.allowed_projects
         return Suggestion.objects.filter(
-            user=user,
-            unit__translation__component__project__in=allowed_projects
+            user=user, unit__translation__component__project__in=allowed_projects
         ).order()
 
     def get_context_data(self):
