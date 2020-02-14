63e7fd895e58a906015c97ef1817754903f71cee
Michal Čihař <michal@cihar.com>
2020-02-14 12:38:39 +0100

    Addons: Use generic code for changing addon events
    We are doing same thing several times, so better to share the code.

--- weblate/addons/migrations/0011_squash_addon.py
+++ weblate/addons/migrations/0011_squash_addon.py
@@ -5,18 +5,18 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 from weblate.addons.events import EVENT_POST_COMMIT, EVENT_PRE_PUSH
+from weblate.addons.utils import adjust_addon_events
 
 
 def update_squash_addon(apps, schema_editor):
     """Update events setup for weblate.git.squash addon."""
-    Addon = apps.get_model("addons", "Addon")
-    Event = apps.get_model("addons", "Event")
-    db_alias = schema_editor.connection.alias
-    for addon in Addon.objects.using(db_alias).filter(name="weblate.git.squash"):
-        Event.objects.using(db_alias).get_or_create(
-            addon=addon, event=EVENT_POST_COMMIT
-        )
-        addon.event_set.filter(event=EVENT_PRE_PUSH).delete()
+    adjust_addon_events(
+        apps,
+        schema_editor,
+        ["weblate.git.squash"],
+        [EVENT_POST_COMMIT],
+        [EVENT_PRE_PUSH],
+    )
 
 
 class Migration(migrations.Migration):
---
+++ weblate/addons/utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from __future__ import unicode_literals
+
+
+def adjust_addon_events(apps, schema_editor, names, add, remove):
+    Addon = apps.get_model("addons", "Addon")
+    Event = apps.get_model("addons", "Event")
+    db_alias = schema_editor.connection.alias
+    for addon in Addon.objects.using(db_alias).filter(name__in=names):
+        for event in add:
+            Event.objects.using(db_alias).get_or_create(addon=addon, event=event)
+        addon.event_set.filter(event__in=remove).delete()
