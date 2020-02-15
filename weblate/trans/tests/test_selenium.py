f72e24f116167be8ad2ddd9bc8f9339ff06a2c7f
Michal Čihař <michal@cihar.com>
2020-02-15 23:24:26 +0100

    Tests: Fix Selenium condition

--- weblate/trans/tests/test_selenium.py
+++ weblate/trans/tests/test_selenium.py
@@ -115,7 +115,7 @@ class SeleniumTests(BaseLiveServerTestCase, RegistrationTestMixin, TempDirMixin)
             cls.driver = webdriver.Chrome(options=options)
         except WebDriverException as error:
             cls.driver_error = str(error)
-            if 'DO_SELENIUM' in os.environ:
+            if 'CI_SELENIUM' in os.environ:
                 raise
 
         if cls.driver is not None:
