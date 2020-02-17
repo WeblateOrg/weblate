8e70cd58c2deebd876c953eb653cff35bdc92c7a
Michal Čihař <michal@cihar.com>
2020-02-17 12:44:35 +0100

    Setup: Fix broken entry point

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    entry_points={"console_scripts": ["weblate = django.core.management:execute_from_command_line"]}
+    entry_points={"console_scripts": ["weblate = weblate.runner:main"]},
     tests_require=("selenium", "httpretty", "boto3"),
     test_suite="runtests.runtests",
 )
