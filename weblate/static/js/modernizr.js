58635642794d51f62e5cd0343d271e72ea24e8a1
Michal Čihař <michal@cihar.com>
2020-02-18 07:18:49 +0100

    JS: Update modernizr (#3495)

--- weblate/static/js/modernizr.js
+++ weblate/static/js/modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * modernizr v3.9.0
+ * modernizr v3.9.1
  * Build https://modernizr.com/download?-inputtypes-dontmin
  *
  * Copyright (c)
@@ -36,7 +36,7 @@
    */
   var ModernizrProto = {
     // The current version, dummy
-    _version: '3.9.0',
+    _version: '3.9.1',
 
     // Any settings that don't work as separate modules
     // can go in here as configuration.
