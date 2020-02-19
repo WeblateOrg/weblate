b41a9205a9686e50047417e5e9fe02c09b3445eb
Michal Čihař <michal@cihar.com>
2020-02-19 19:22:31 +0100

    GPG: Fix typo, coding style

--- weblate/vcs/gpg.py
+++ weblate/vcs/gpg.py
@@ -26,38 +26,38 @@ from django.conf import settings
 from django.core.cache import cache
 from django.utils.encoding import force_text
 
-from weblate.utils.errors import report_error
 from weblate.trans.util import (
     add_configuration_error,
     delete_configuration_error,
     get_clean_env,
 )
+from weblate.utils.errors import report_error
 
 
 def generate_gpg_key():
     try:
         subprocess.check_output(
             [
-                'gpg',
-                '--batch',
-                '--pinentry-mode',
-                'loopback',
-                '--passphrase',
-                '',
-                '--quick-generate-key',
+                "gpg",
+                "--batch",
+                "--pinentry-mode",
+                "loopback",
+                "--passphrase",
+                "",
+                "--quick-generate-key",
                 settings.WEBLATE_GPG_IDENTITY,
                 settings.WEBLATE_GPG_ALGO,
-                'default',
-                'never',
+                "default",
+                "never",
             ],
             stderr=subprocess.STDOUT,
             env=get_clean_env(),
         )
-        delete_configuration_error('GPG key generating')
+        delete_configuration_error("GPG key generating")
         return get_gpg_key()
     except (subprocess.CalledProcessError, OSError) as exc:
-        report_error(exp, prefix='GPG key generating')
-        add_configuration_error('GPG key generating', force_text(exc))
+        report_error(exc, prefix="GPG key generating")
+        add_configuration_error("GPG key generating", force_text(exc))
         return None
 
 
@@ -65,25 +65,25 @@ def get_gpg_key(silent=False):
     try:
         output = subprocess.check_output(
             [
-                'gpg',
-                '--batch',
-                '--with-colons',
-                '--list-secret-keys',
+                "gpg",
+                "--batch",
+                "--with-colons",
+                "--list-secret-keys",
                 settings.WEBLATE_GPG_IDENTITY,
             ],
             stderr=subprocess.STDOUT,
             env=get_clean_env(),
-        ).decode('utf-8')
+        ).decode("utf-8")
         for line in output.splitlines():
-            if not line.startswith('fpr:'):
+            if not line.startswith("fpr:"):
                 continue
-            delete_configuration_error('GPG key listing')
-            return line.split(':')[9]
+            delete_configuration_error("GPG key listing")
+            return line.split(":")[9]
         return None
     except (subprocess.CalledProcessError, OSError) as exc:
-        report_error(exp, prefix='GPG key listing')
+        report_error(exc, prefix="GPG key listing")
         if not silent:
-            add_configuration_error('GPG key listing', force_text(exc))
+            add_configuration_error("GPG key listing", force_text(exc))
         return None
 
 
@@ -91,13 +91,13 @@ def get_gpg_sign_key():
     """High level wrapper to cache key ID."""
     if not settings.WEBLATE_GPG_IDENTITY:
         return None
-    keyid = cache.get('gpg-key-id')
+    keyid = cache.get("gpg-key-id")
     if keyid is None:
         keyid = get_gpg_key(silent=True)
         if keyid is None:
             keyid = generate_gpg_key()
         if keyid:
-            cache.set('gpg-key-id', keyid, 7 * 86400)
+            cache.set("gpg-key-id", keyid, 7 * 86400)
     return keyid
 
 
@@ -105,18 +105,18 @@ def get_gpg_public_key():
     key = get_gpg_sign_key()
     if key is None:
         return None
-    data = cache.get('gpg-key-public')
+    data = cache.get("gpg-key-public")
     if not data:
         try:
             data = subprocess.check_output(
-                ['gpg', '--batch', '-armor', '--export', key],
+                ["gpg", "--batch", "-armor", "--export", key],
                 stderr=subprocess.STDOUT,
                 env=get_clean_env(),
-            ).decode('utf-8')
-            cache.set('gpg-key-public', data, 7 * 86400)
-            delete_configuration_error('GPG key public')
+            ).decode("utf-8")
+            cache.set("gpg-key-public", data, 7 * 86400)
+            delete_configuration_error("GPG key public")
         except (subprocess.CalledProcessError, OSError) as exc:
-            report_error(exp, prefix='GPG key public')
-            add_configuration_error('GPG key public', force_text(exc))
+            report_error(exc, prefix="GPG key public")
+            add_configuration_error("GPG key public", force_text(exc))
             return None
     return data
