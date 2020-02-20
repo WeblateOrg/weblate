79655490c80899d3f734361331dd0c7526e134a5
Michal Čihař <michal@cihar.com>
2020-02-20 13:01:13 +0100

    Management: Avoid adding console logger if already present
    This avoids double output in some cases and output multiplication in tests.

--- weblate/utils/management/base.py
+++ weblate/utils/management/base.py
@@ -26,16 +26,18 @@ from django.core.management.base import BaseCommand as DjangoBaseCommand
 class BaseCommand(DjangoBaseCommand):
     def execute(self, *args, **options):
         logger = logging.getLogger("weblate")
-        console = logging.StreamHandler()
-        verbosity = int(options["verbosity"])
-        if verbosity > 1:
-            console.setLevel(logging.DEBUG)
-        elif verbosity == 1:
-            console.setLevel(logging.INFO)
-        else:
-            console.setLevel(logging.ERROR)
-        console.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
-        logger.addHandler(console)
+        if not any(handler.get_name() == "console" for handler in logger.handlers):
+            console = logging.StreamHandler()
+            console.set_name("console")
+            verbosity = int(options["verbosity"])
+            if verbosity > 1:
+                console.setLevel(logging.DEBUG)
+            elif verbosity == 1:
+                console.setLevel(logging.INFO)
+            else:
+                console.setLevel(logging.ERROR)
+            console.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+            logger.addHandler(console)
         return super(BaseCommand, self).execute(*args, **options)
 
     def handle(self, *args, **options):
