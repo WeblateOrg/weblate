5696467140596f6b026201a728634e5b88ba8f25
Michal Čihař <michal@cihar.com>
2020-03-04 13:39:49 +0100

    Component: Invalidate stats for disappeared translations
    Fixes #3557

--- weblate/trans/models/component.py
+++ weblate/trans/models/component.py
@@ -1392,6 +1392,9 @@ class Component(models.Model, URLMixin, PathMixin):
                         "removing stale translations: %s",
                         ",".join(trans.language.code for trans in todelete),
                     )
+                    # Invalidate stats (most importantly to invalidate parent stats)
+                    for translation in todelete:
+                        translation.stats.invalidate()
                     todelete.delete()
 
         self.update_import_alerts()
