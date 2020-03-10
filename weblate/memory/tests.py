9c75b52fd088fe2029f4a445d999ef56fedc6042
Michal Čihař <michal@cihar.com>
2020-03-10 14:46:16 +0100

    Tests: Workaround MySQL bug in transactions and fulltext index
    See https://dev.mysql.com/doc/refman/5.6/en/innodb-fulltext-index.html#innodb-fulltext-index-transaction
    
    Issue #2825

--- weblate/memory/tests.py
+++ weblate/memory/tests.py
@@ -21,6 +21,7 @@
 import json
 from io import StringIO
 
+from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.urls import reverse
@@ -48,6 +49,16 @@ def add_document():
 
 
 class MemoryModelTest(FixtureTestCase):
+    @classmethod
+    def _databases_support_transactions(cls):
+        # This is workaroud for MySQL as FULL TEXT index does not work
+        # well inside a transaction, so we avoid using transactions for
+        # tests. Otherwise we end up with no matches for the query.
+        # See https://dev.mysql.com/doc/refman/5.6/en/innodb-fulltext-index.html
+        if settings.DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
+            return False
+        return super()._databases_support_transactions()
+
     def test_machine(self):
         add_document()
         machine_translation = WeblateMemory()
