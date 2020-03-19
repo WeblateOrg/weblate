2b3788ea5a34eeaf86625be689fced4122c33494
Filip Hron <filip.hron@gmail.com>
2020-03-19 14:33:26 +0100

    testing patterns change

--- weblate/checks/tests/test_same_checks.py
+++ weblate/checks/tests/test_same_checks.py
@@ -217,5 +217,5 @@ class SameCheckTest(CheckTestCase):
         self.do_test(True, ("Who is RMS?", "Who is RMS?", ""))
 
     def test_same_placeholders(self):
-        self.do_test(False, ("text $URL$", "text $URL$", "placeholders:$URL$"))
-        self.do_test(True, ("text $URL$", "text $URL$", ""))
+        self.do_test(False, ("DOMAIN", "DOMAIN", "placeholders:DOMAIN"))
+        self.do_test(True, ("PLACHOLDER", "PLACEHOLDER", "placeholders:DOMAIN"))
