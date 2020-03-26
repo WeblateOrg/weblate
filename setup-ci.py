c05d8da81a3e319a4bec0749407653fcd664a395
Branson Perreault <bperreault@vendasta.com>
2020-03-26 14:38:43 -0600

    NE-546: Reorder imports in setup-ci

--- setup-ci.py
+++ setup-ci.py
@@ -21,7 +21,7 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
