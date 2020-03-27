f188d12f965d717f27f23bc6edd45aaef8ccd6ab
Michal Čihař <michal@cihar.com>
2020-03-27 13:25:49 +0100

    Changes: Use compound index on timestamp
    The most frequent query is getting last change from translation, so
    let's optimize for this case.

--- weblate/trans/models/change.py
+++ weblate/trans/models/change.py
@@ -419,7 +419,7 @@ class Change(models.Model, UserDisplayMixin):
         related_name="author_set",
         on_delete=models.deletion.CASCADE,
     )
-    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     action = models.IntegerField(
         choices=ACTION_CHOICES, default=ACTION_CHANGE, db_index=True
     )
@@ -431,6 +431,9 @@ class Change(models.Model, UserDisplayMixin):
 
     class Meta:
         app_label = "trans"
+        index_together = [
+            ("timestamp", "translation"),
+        ]
 
     def __init__(self, *args, **kwargs):
         self.notify_state = {}
