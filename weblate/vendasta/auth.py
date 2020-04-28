31b5ec2856d2e55cd9129d8c5bf3a24f4a0c798e
Branson Perreault <bperreault@vendasta.com>
2020-04-28 17:11:16 -0600

    Use prod iam domain for login

--- weblate/vendasta/auth.py
+++ weblate/vendasta/auth.py
@@ -8,6 +8,6 @@ class VendastaOpenIdConnect(OpenIdConnectAuth):
     """Vendasta OpenID authentication Backend."""
 
     name = "vendasta"
-    OIDC_ENDPOINT = "http://iam-demo.vendasta-internal.com"
+    OIDC_ENDPOINT = "http://iam-prod.vendasta-internal.com"
     ACCESS_TOKEN_METHOD = "POST"
     EXTRA_DATA = [("sub", "id"), "namespace", "roles"]
