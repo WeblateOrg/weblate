b1a3dc114d5e2624e13eeffcba89b1ff5a27c4bd
Michal Čihař <michal@cihar.com>
2020-05-19 11:55:27 +0200

    Formats: Remove debugging code

--- weblate/formats/exporters.py
+++ weblate/formats/exporters.py
@@ -263,7 +263,6 @@ class PoXliffExporter(XMLExporter):
             converted_source = xliff_string_to_rich(unit.get_source_plurals())
             converted_target = xliff_string_to_rich(unit.get_target_plurals())
         except XMLSyntaxError:
-            raise
             return output
         output.rich_source = converted_source
         output.set_rich_target(converted_target, self.language.code)
