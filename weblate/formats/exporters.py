70af9fa8405af06760bad9787c0633e8c85f85dd
Michal Čihař <michal@cihar.com>
2020-05-19 11:55:05 +0200

    Formats: Fix exporting rich plurals to PoXliff

--- weblate/formats/exporters.py
+++ weblate/formats/exporters.py
@@ -258,12 +258,13 @@ class PoXliffExporter(XMLExporter):
         return multistring([self.string_filter(plural) for plural in plurals])
 
     def build_unit(self, unit):
+        output = super().build_unit(unit)
         try:
             converted_source = xliff_string_to_rich(unit.get_source_plurals())
             converted_target = xliff_string_to_rich(unit.get_target_plurals())
         except XMLSyntaxError:
-            return super().build_unit(unit)
-        output = self.storage.UnitClass("")
+            raise
+            return output
         output.rich_source = converted_source
         output.set_rich_target(converted_target, self.language.code)
         return output
