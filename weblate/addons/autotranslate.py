4afa2a1ad262e90aed5380b8e1363a4449e02805
Michal Čihař <michal@cihar.com>
2020-05-20 20:17:59 +0200

    Addons: Reduce frequency of auto translate trigger
    In most cases it is not useful to run it daily, so split the load by component
    ID and run every component once in a week.

--- weblate/addons/autotranslate.py
+++ weblate/addons/autotranslate.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from datetime import date
 
 from django.db import transaction
 from django.utils.translation import gettext_lazy as _
@@ -43,6 +44,10 @@ class AutoTranslateAddon(BaseAddon):
         self.daily(component)
 
     def daily(self, component):
+        # Translate every component once in a week to reduce load
+        if component.id % 7 != date.today.weekday():
+            return
+
         for translation in component.translation_set.iterator():
             if translation.is_source:
                 continue
