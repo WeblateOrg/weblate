4a0064bbcb7fb418dda6e7eae214540df84da018
Michal Čihař <michal@cihar.com>
2020-05-20 14:19:04 +0200

    Utils: Test ensure_stats command

--- weblate/utils/tests/test_commands.py
+++ weblate/utils/tests/test_commands.py
@@ -22,7 +22,7 @@ from glob import glob
 from io import StringIO
 
 from django.core.management import call_command
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.test.utils import override_settings
 
 from weblate.trans.tests.utils import TempDirMixin
@@ -57,3 +57,10 @@ class CommandTests(SimpleTestCase, TempDirMixin):
         output = StringIO()
         call_command("celery_queues", stdout=output)
         self.assertIn("celery:", output.getvalue())
+
+
+class DBCommandTests(TestCase):
+    def test_stats(self):
+        output = StringIO()
+        call_command("ensure_stats", stdout=output)
+        self.assertEqual("", output.getvalue())
