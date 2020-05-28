105938407c14973b62539419ce5159e167d50acc
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-05-28 09:14:16 +0200

    Editor: remove unneded check
    Because the script is now always run in the context of a regular/zen
    editor, '.translation-editor' is always present and hence checking for
    its presence is not needed.

--- weblate/static/editor.js
+++ weblate/static/editor.js
@@ -616,65 +616,63 @@ $('.bug-comment').click(function () {
 
 /* Translation editor */
 var translationEditor = $('.translation-editor');
-if (translationEditor.length > 0) {
-    $document.on('change', '.translation-editor', testChangeHandler);
-    $document.on('keypress', '.translation-editor', testChangeHandler);
-    $document.on('keydown', '.translation-editor', testChangeHandler);
-    $document.on('paste', '.translation-editor', testChangeHandler);
-    $document.on('focusin', '.translation-editor', function () { lastEditor = $(this); });
-    initEditor();
-    translationEditor.get(0).focus();
+$document.on('change', '.translation-editor', testChangeHandler);
+$document.on('keypress', '.translation-editor', testChangeHandler);
+$document.on('keydown', '.translation-editor', testChangeHandler);
+$document.on('paste', '.translation-editor', testChangeHandler);
+$document.on('focusin', '.translation-editor', function () { lastEditor = $(this); });
+initEditor();
+translationEditor.get(0).focus();
 
-    // TODO: move to non-zen editor
-    if ($('#button-first').length > 0) {
-        Mousetrap.bindGlobal('alt+end', function(e) {window.location = $('#button-end').attr('href'); return false;});
-        Mousetrap.bindGlobal('alt+pagedown', function(e) {window.location = $('#button-next').attr('href'); return false;});
-        Mousetrap.bindGlobal('alt+pageup', function(e) {window.location = $('#button-prev').attr('href'); return false;});
-        Mousetrap.bindGlobal('alt+home', function(e) {window.location = $('#button-first').attr('href'); return false;});
-        Mousetrap.bindGlobal('mod+o', function(e) {$('.translation-item .copy-text').click(); return false;});
-        Mousetrap.bindGlobal('mod+y', function(e) {$('input[name="fuzzy"]').click(); return false;});
-        Mousetrap.bindGlobal(
-            'mod+shift+enter',
-            function(e) {$('input[name="fuzzy"]').prop('checked', false); return submitForm(e);}
-        );
-        Mousetrap.bindGlobal(
-            'mod+e',
-            function(e) {
-                $('.translation-editor').get(0).focus();
-                return false;
-            }
-        );
-        Mousetrap.bindGlobal(
-            'mod+s',
-            function(e) {
-                $('#search-dropdown').click();
-                $('input[name="q"]').focus();
-                return false;
-            }
-        );
-        Mousetrap.bindGlobal(
-            'mod+u',
-            function(e) {
-                $('.nav [href="#comments"]').click();
-                $('textarea[name="comment"]').focus();
-                return false;
-            }
-        );
-        Mousetrap.bindGlobal(
-            'mod+j',
-            function(e) {
-                $('.nav [href="#nearby"]').click();
-                return false;
-            }
-        );
-        Mousetrap.bindGlobal(
-            'mod+m',
-            function(e) {
-                $('.nav [href="#machine"]').click();
-                return false;
-            }
-        );
-    }
+// TODO: move to non-zen editor
+if ($('#button-first').length > 0) {
+    Mousetrap.bindGlobal('alt+end', function(e) {window.location = $('#button-end').attr('href'); return false;});
+    Mousetrap.bindGlobal('alt+pagedown', function(e) {window.location = $('#button-next').attr('href'); return false;});
+    Mousetrap.bindGlobal('alt+pageup', function(e) {window.location = $('#button-prev').attr('href'); return false;});
+    Mousetrap.bindGlobal('alt+home', function(e) {window.location = $('#button-first').attr('href'); return false;});
+    Mousetrap.bindGlobal('mod+o', function(e) {$('.translation-item .copy-text').click(); return false;});
+    Mousetrap.bindGlobal('mod+y', function(e) {$('input[name="fuzzy"]').click(); return false;});
+    Mousetrap.bindGlobal(
+        'mod+shift+enter',
+        function(e) {$('input[name="fuzzy"]').prop('checked', false); return submitForm(e);}
+    );
+    Mousetrap.bindGlobal(
+        'mod+e',
+        function(e) {
+            $('.translation-editor').get(0).focus();
+            return false;
+        }
+    );
+    Mousetrap.bindGlobal(
+        'mod+s',
+        function(e) {
+            $('#search-dropdown').click();
+            $('input[name="q"]').focus();
+            return false;
+        }
+    );
+    Mousetrap.bindGlobal(
+        'mod+u',
+        function(e) {
+            $('.nav [href="#comments"]').click();
+            $('textarea[name="comment"]').focus();
+            return false;
+        }
+    );
+    Mousetrap.bindGlobal(
+        'mod+j',
+        function(e) {
+            $('.nav [href="#nearby"]').click();
+            return false;
+        }
+    );
+    Mousetrap.bindGlobal(
+        'mod+m',
+        function(e) {
+            $('.nav [href="#machine"]').click();
+            return false;
+        }
+    );
 }
 
 // TODO: move to zen mode
