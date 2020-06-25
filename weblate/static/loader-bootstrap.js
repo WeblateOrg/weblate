0b68e5f4d6deebeb194ac049a5bc1a7579c6caa7
Saptak S <saptak013@gmail.com>
2020-06-25 20:18:58 +0530

    Adds user suggestions in comment section

--- weblate/static/loader-bootstrap.js
+++ weblate/static/loader-bootstrap.js
@@ -904,6 +904,307 @@ $(function () {
         $('#id_q').insertAtCaret(' ' + $(this).closest('tr').find('code').text() + ' ');
     });
 
+    /* Autocomplete users */
+    $('#id_comment').on('keyup', function (event) {
+        console.log("Keypressed");
+        var comment = $('#id_comment').val();
+        var lastUsernameStartIndex = comment.lastIndexOf('@');
+        var lastSpaceIndex = comment.lastIndexOf(' ');
+        if (lastUsernameStartIndex !== -1 && lastSpaceIndex < lastUsernameStartIndex) {
+            var usernameSearch = comment.substring(lastUsernameStartIndex + 1);
+            console.log(usernameSearch);
+            if (usernameSearch.length > 2) {
+                $.ajax({
+                    type: 'GET',
+                    url: `/api/users/?username=${usernameSearch}`,
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data);
+                        var users = data.results;
+                        renderUserSuggestion(users);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        addAlert(errorThrown);
+                    }
+                });
+            }
+        }
+    });
+
+    function getCaretCoordinates(element, position) {
+        const properties = [
+            'direction',
+            'boxSizing',
+            'width',
+            'height',
+            'overflowX',
+            'overflowY',
+
+            'borderTopWidth',
+            'borderRightWidth',
+            'borderBottomWidth',
+            'borderLeftWidth',
+            'borderStyle',
+
+            'paddingTop',
+            'paddingRight',
+            'paddingBottom',
+            'paddingLeft',
+
+            'fontStyle',
+            'fontVariant',
+            'fontWeight',
+            'fontStretch',
+            'fontSize',
+            'fontSizeAdjust',
+            'lineHeight',
+            'fontFamily',
+
+            'textAlign',
+            'textTransform',
+            'textIndent',
+            'textDecoration',
+
+            'letterSpacing',
+            'wordSpacing',
+
+            'tabSize',
+            'MozTabSize',
+        ];
+        const isFirefox = typeof window !== 'undefined' && window['mozInnerScreenX'] != null;
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+
+        const style = div.style;
+        const computed = getComputedStyle(element);
+
+        style.whiteSpace = 'pre-wrap';
+        style.wordWrap = 'break-word';
+        style.position = 'absolute';
+        style.visibility = 'hidden';
+
+        properties.forEach(prop => {
+            style[prop] = computed[prop];
+        });
+
+        if (isFirefox) {
+            if (element.scrollHeight > parseInt(computed.height))
+                style.overflowY = 'scroll';
+        } else {
+            style.overflow = 'hidden';
+        }
+
+        div.textContent = element.value.substring(0, position);
+
+        const span = document.createElement('span');
+        span.textContent = element.value.substring(position) || '.';
+        div.appendChild(span);
+
+        const coordinates = {
+            top: span.offsetTop + parseInt(computed['borderTopWidth']),
+            left: span.offsetLeft + parseInt(computed['borderLeftWidth']),
+            height: span.offsetHeight
+        };
+
+        div.remove();
+
+        return coordinates;
+    }
+
+    var renderUserSuggestion = function(users) {
+        $('.user-suggestions').remove();
+        var $userList = $(document.createElement('ul'));
+        $userList.addClass('user-suggestions').addClass('dropdown-menu');
+        $.each(users, function(idx, user) {
+            $userList.append(
+                `<li>
+                    <a href="#">
+                        <span>${user.username}</span>
+                        <small>${user.full_name}</small>
+                    </a>
+                 </li>
+                `
+            );
+        });
+        var textarea = document.getElementById('id_comment');
+        $('#id_comment').parent().addClass('open');
+        $('#id_comment').after($userList);
+
+        var coords = getCaretCoordinates(textarea, textarea.selectionStart);
+        var { top, left } = $('#id_comment').closest('.panel-body').find('.help-block')[0].getBoundingClientRect();
+        var optionsLeft = window.scrollX  + coords.left + left + textarea.scrollLeft;
+        var optionsTop = window.scrollY +  coords.top + top + coords.height - textarea.scrollTop;
+        console.log(optionsLeft);
+        console.log(optionsTop);
+        $userList.css({top: `${optionsTop}px`, left: `${optionsLeft}px`});
+    }
+
+    class Mentionify {
+      constructor(ref, menuRef, resolveFn, replaceFn, menuItemFn) {
+        this.ref = ref
+        this.menuRef = menuRef
+        this.resolveFn = resolveFn
+        this.replaceFn = replaceFn
+        this.menuItemFn = menuItemFn
+        this.options = []
+
+        this.makeOptions = this.makeOptions.bind(this)
+        this.closeMenu = this.closeMenu.bind(this)
+        this.selectItem = this.selectItem.bind(this)
+        this.onInput = this.onInput.bind(this)
+        this.onKeyDown = this.onKeyDown.bind(this)
+        this.renderMenu = this.renderMenu.bind(this)
+
+        this.ref.addEventListener('input', this.onInput)
+        this.ref.addEventListener('keydown', this.onKeyDown)
+      }
+
+      async makeOptions(query) {
+        const options = await this.resolveFn(query)
+        if (options.lenght !== 0) {
+          this.options = options
+          this.renderMenu()
+        } else {
+          this.closeMenu()
+        }
+      }
+
+      closeMenu() {
+        setTimeout(() => {
+          this.options = []
+          this.left = undefined
+          this.top = undefined
+          this.triggerIdx = undefined
+          this.renderMenu()
+        }, 0)
+      }
+
+      selectItem(active) {
+        return () => {
+          const preMention = this.ref.value.substr(0, this.triggerIdx)
+          const option = this.options[active]
+          const mention = this.replaceFn(option, this.ref.value[this.triggerIdx])
+          const postMention = this.ref.value.substr(this.ref.selectionStart)
+          const newValue = `${preMention}${mention}${postMention}`
+          this.ref.value = newValue
+          const caretPosition = this.ref.value.length - postMention.length
+          this.ref.setSelectionRange(caretPosition, caretPosition)
+          this.closeMenu()
+          this.ref.focus()
+        }
+      }
+
+      onInput(ev) {
+        const positionIndex = this.ref.selectionStart
+        const textBeforeCaret = this.ref.value.slice(0, positionIndex)
+        const tokens = textBeforeCaret.split(/\s/)
+        const lastToken = tokens[tokens.length - 1]
+        const triggerIdx = textBeforeCaret.endsWith(lastToken)
+          ? textBeforeCaret.length - lastToken.length
+          : -1
+        const maybeTrigger = textBeforeCaret[triggerIdx]
+        const keystrokeTriggered = maybeTrigger === '@'
+
+        if (!keystrokeTriggered) {
+          this.closeMenu()
+          return
+        }
+
+        const query = textBeforeCaret.slice(triggerIdx + 1)
+        this.makeOptions(query)
+
+        const coords = getCaretCoordinates(this.ref, positionIndex)
+        const { top, left } = this.ref.getBoundingClientRect()
+
+        setTimeout(() => {
+          this.active = 0
+          this.left = window.scrollX  + coords.left + left + this.ref.scrollLeft
+          this.top = window.scrollY +  coords.top + top + coords.height - this.ref.scrollTop
+          this.triggerIdx = triggerIdx
+          this.renderMenu()
+        }, 0)
+      }
+
+      onKeyDown(ev) {
+        let keyCaught = false
+        if (this.triggerIdx !== undefined) {
+          switch (ev.key) {
+            case 'ArrowDown':
+              this.active = Math.min(this.active + 1, this.options.length - 1)
+              this.renderMenu()
+              keyCaught = true
+              break
+            case 'ArrowUp':
+              this.active = Math.max(this.active - 1, 0)
+              this.renderMenu()
+              keyCaught = true
+              break
+            case 'Enter':
+            case 'Tab':
+              this.selectItem(this.active)()
+              keyCaught = true
+              break
+          }
+        }
+
+        if (keyCaught) {
+          ev.preventDefault()
+        }
+      }
+
+      renderMenu() {
+        if (this.top === undefined) {
+          this.menuRef.hidden = true
+          return
+        }
+
+        this.menuRef.style.left = this.left + 'px'
+        this.menuRef.style.top = this.top + 'px'
+        this.menuRef.innerHTML = ''
+
+        this.options.forEach((option, idx) => {
+          this.menuRef.appendChild(this.menuItemFn(
+            option,
+            this.selectItem(idx),
+            this.active === idx))
+        })
+
+        this.menuRef.hidden = false
+      }
+    }
+
+    // const users = [
+    //   { username: 'john_doe' },
+    //   { username: 'jane_doe' },
+    // ]
+
+    // const resolveFn = prefix => prefix === ''
+    //   ? users
+    //   : users.filter(user => user.username.startsWith(prefix))
+
+    // const replaceFn = (user, trigger) => `${trigger}${user.username} `
+
+    // const menuItemFn = (user, setItem, selected) => {
+    //   const div = document.createElement('div')
+    //   div.setAttribute('role', 'option')
+    //   div.className = 'menu-item'
+    //   if (selected) {
+    //     div.classList.add('selected')
+    //     div.setAttribute('aria-selected', '')
+    //   }
+    //   div.textContent = user.username
+    //   div.onclick = setItem
+    //   return div
+    // }
+
+    // new Mentionify(
+    //   document.getElementById('textarea'),
+    //   document.getElementById('menu'),
+    //   resolveFn,
+    //   replaceFn,
+    //   menuItemFn
+    // )
+
     /* Clickable rows */
     $('tr[data-href]').click(function () {
         window.location = $(this).data('href');
