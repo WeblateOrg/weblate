f8f2221f0705e2f7bfd630ac21089d7b213ed98e
Michal Čihař <michal@cihar.com>
2020-06-29 14:38:03 +0200

    Misc: Remove not needed super() args

--- weblate/utils/forms.py
+++ weblate/utils/forms.py
@@ -63,7 +63,7 @@ class ContextDiv(Div):
 class SearchField(Field):
     def render(self, form, form_style, context, template_pack=TEMPLATE_PACK, **kwargs):
         extra_context = {"custom_filter_list": self.get_search_query_choices()}
-        return super(SearchField, self).render(
+        return super().render(
             form, form_style, context, template_pack, extra_context, **kwargs
         )
 
