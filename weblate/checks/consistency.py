5760e591a68a87ae958d6d86be7b379d14c5ec80
Abhilash KP <abhilashmhaisne@gmail.com>
2020-07-02 23:49:50 +0530

    Minor fix

--- weblate/checks/consistency.py
+++ weblate/checks/consistency.py
@@ -101,14 +101,13 @@ class TranslatedCheck(TargetCheck):
     def get_description(self, check=None):
         unit = check.unit
         target = self.check_target_unit(unit.source, unit.target, unit)
-        description = (
+        return (
             _(
                 'This string has been translated in the past. \
                 Last Translation was "%s"'
             )
             % target
         )
-        return description
 
     def check_target_unit(self, sources, targets, unit):
         if unit.translated:
