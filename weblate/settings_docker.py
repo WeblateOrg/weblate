379bb45269138517d0e2764fa6da462c08ae3619
stumbaumr <stumbaumr@yahoo.de>
2020-07-02 14:44:16 +0200

    Add WEBLATE_AUTH_LDAP_CONNECTION_OPTION_REFERRALS parameter to deactivate LDAP Referrals
    Comes in handy with Active Directory (AD) LDAP servers.
    
    Should fix https://github.com/WeblateOrg/docker/issues/724.
    
    No idea if this readable coding though, but it is working here...

--- weblate/settings_docker.py
+++ weblate/settings_docker.py
@@ -390,6 +390,11 @@ if "WEBLATE_AUTH_LDAP_SERVER_URI" in os.environ:
             os.environ.get("WEBLATE_AUTH_LDAP_USER_SEARCH_FILTER", "(uid=%(user)s)"),
         )
 
+    if not get_env_bool("WEBLATE_AUTH_LDAP_CONNECTION_OPTION_REFERRALS", True):
+        AUTH_LDAP_CONNECTION_OPTIONS = {
+            ldap.OPT_REFERRALS: 0,
+        }
+
 # Always include Weblate backend
 AUTHENTICATION_BACKENDS += ("weblate.accounts.auth.WeblateUserBackend",)
 
