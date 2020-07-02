cca0b28733331e1e09e36efd88d7cb7832f49c18
Michal Čihař <michal@cihar.com>
2020-07-02 10:03:26 +0200

    JS: Avoid error in case Zammad is not loaded

--- weblate/static/loader-bootstrap.js
+++ weblate/static/loader-bootstrap.js
@@ -921,55 +921,57 @@ $(function () {
     });
 
     /* Support form */
-    $('#support-form').ZammadForm({
-      messageTitle: gettext('Weblate feedback'),
-      messageSubmit: gettext("Get help"),
-      messageThankYou: gettext('Thank you for your inquiry (#%s)! We\'ll contact you as soon as possible.'),
-      showTitle: true,
-      modal: true,
-      attachmentSupport: true,
-      attributes: [
-        {
-          display: gettext("Subject"),
-          name: "title",
-          tag: 'input',
-          type: 'text',
-          placeholder: '',
-          defaultValue: 'Weblate feedback [' + window.location.hostname + ']',
-        },
-        {
-          display: gettext('Your name'),
-          name: 'name',
-          tag: 'input',
-          type: 'text',
-          placeholder: '',
-          defaultValue: $('#support-form').data("fullname"),
-        },
-        {
-          display: gettext('Your e-mail'),
-          name: 'email',
-          tag: 'input',
-          type: 'email',
-          placeholder: '',
-          defaultValue: $('#support-form').data("email"),
-        },
-        {
-          display: gettext('Message'),
-          name: 'body',
-          tag: 'textarea',
-          placeholder: gettext("Please contact us in English, otherwise we might be unable to process your request."),
-          defaultValue: '',
-          rows: 7,
-        },
-        {
-          display: gettext('Attachments'),
-          name: 'file[]',
-          tag: 'input',
-          type: 'file',
-          repeat: 3,
-        },
-      ]
-    });
+    if (typeof $.prototype.ZammadForm !== 'undefined') {
+      $('#support-form').ZammadForm({
+        messageTitle: gettext('Weblate feedback'),
+        messageSubmit: gettext("Get help"),
+        messageThankYou: gettext('Thank you for your inquiry (#%s)! We\'ll contact you as soon as possible.'),
+        showTitle: true,
+        modal: true,
+        attachmentSupport: true,
+        attributes: [
+          {
+            display: gettext("Subject"),
+            name: "title",
+            tag: 'input',
+            type: 'text',
+            placeholder: '',
+            defaultValue: 'Weblate feedback [' + window.location.hostname + ']',
+          },
+          {
+            display: gettext('Your name'),
+            name: 'name',
+            tag: 'input',
+            type: 'text',
+            placeholder: '',
+            defaultValue: $('#support-form').data("fullname"),
+          },
+          {
+            display: gettext('Your e-mail'),
+            name: 'email',
+            tag: 'input',
+            type: 'email',
+            placeholder: '',
+            defaultValue: $('#support-form').data("email"),
+          },
+          {
+            display: gettext('Message'),
+            name: 'body',
+            tag: 'textarea',
+            placeholder: gettext("Please contact us in English, otherwise we might be unable to process your request."),
+            defaultValue: '',
+            rows: 7,
+          },
+          {
+            display: gettext('Attachments'),
+            name: 'file[]',
+            tag: 'input',
+            type: 'file',
+            repeat: 3,
+          },
+        ]
+      });
+    }
 
     /* Warn users that they do not want to use developer console in most cases */
     console.log("%cStop!", "color: red; font-weight: bold; font-size: 50px;");
