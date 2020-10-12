bc40cfb41ec080074f3ccf642a742e3096347725
Michal Čihař <michal@cihar.com>
2020-10-12 15:07:28 +0200

    Declare Python 3.9 as supported
    It needs Selenium 4, the 3.x series is not compatible with Python 3.9,
    see https://github.com/SeleniumHQ/selenium/issues/8762
    
    This also simplifies test matrix by including wanted test combinations
    instead of excluding all the ones we want to skip in more complex
    setups.

--- setup.py
+++ setup.py
@@ -132,6 +132,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Internationalization",
         "Topic :: Software Development :: Localization",
         "Topic :: Internet :: WWW/HTTP",
