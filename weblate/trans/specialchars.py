976fe53f8de7423f6a74330769f227a7f6e4fd82
Michal Čihař <michal@cihar.com>
2020-10-29 10:57:26 +0100

    Specialchars: Add list of chars for Hebrew
    Issue #4772

--- weblate/trans/specialchars.py
+++ weblate/trans/specialchars.py
@@ -229,7 +229,10 @@ EM_DASH_LANGS = {
     "zh",
 }
 
-EXTRA_CHARS = {"brx": ("।", "॥")}
+EXTRA_CHARS = {
+    "brx": ("।", "॥"),
+    "he": ("־", "״", "׳"),
+}
 
 # Additional characters for RTL languages
 RTL_CHARS = (8204, 8205, 8206, 8207, 8234, 8235, 8236, 8237, 8238)
