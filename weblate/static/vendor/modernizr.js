2a2c73f2333b38129cb40cfc35e40c19e105b4fc
Michal Čihař <michal@cihar.com>
2020-11-30 09:13:30 +0100

    JS: Bump modernizr from 3.11.3 to 3.11.4 (#4940)

--- weblate/static/vendor/modernizr.js
+++ weblate/static/vendor/modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * modernizr v3.11.3
+ * modernizr v3.11.4
  * Build https://modernizr.com/download?-inputtypes-dontmin
  *
  * Copyright (c)
@@ -35,7 +35,7 @@
    * @access public
    */
   var ModernizrProto = {
-    _version: '3.11.3',
+    _version: '3.11.4',
 
     // Any settings that don't work as separate modules
     // can go in here as configuration.
