0fea02f413f03152737885b25c22ce97cdce7f2d
Michal Čihař <michal@cihar.com>
2020-12-04 16:38:29 +0100

    Glossary: Prefill new word form with current string
    Fixes #4630

--- weblate/static/loader-bootstrap.js
+++ weblate/static/loader-bootstrap.js
@@ -1037,6 +1037,24 @@ $(function () {
     }
   );
 
+  /* Prefill adding to glossary with current string */
+  $("#add-glossary-form").on("shown.bs.modal", (e) => {
+    if (e.target.hasAttribute("data-shown")) {
+      return;
+    }
+    /* Relies on clone source implementation */
+    let source = JSON.parse(
+      document.querySelector("[data-content]").getAttribute("data-content")
+    );
+    if (source.length < 200) {
+      document.getElementById("id_source").value = source;
+      document.getElementById("id_target").value = document.querySelector(
+        ".translation-editor"
+      ).value;
+    }
+    e.target.setAttribute("data-shown", true);
+  });
+
   /* Warn users that they do not want to use developer console in most cases */
   console.log("%cStop!", "color: red; font-weight: bold; font-size: 50px;");
   console.log(
