ca632f4191d7c74568c5753797855507864e52df
Michal Čihař <michal@cihar.com>
2020-12-22 14:15:30 +0100

    Docs: Ignore materialdesignicons.com error in linkcheck
    Hopefully this is a temporary failure and this can be reverted.

--- docs/conf.py
+++ docs/conf.py
@@ -214,7 +214,11 @@ nitpick_ignore = [
 # Number of retries and timeout for linkcheck
 linkcheck_retries = 10
 linkcheck_timeout = 10
-linkcheck_ignore = ["http://127.0.0.1:8080/"]
+linkcheck_ignore = [
+    "http://127.0.0.1:8080/",
+    # The site seems temporarily down
+    "https://materialdesignicons.com/",
+]
 
 # HTTP docs
 http_index_ignore_prefixes = ["/api/"]
