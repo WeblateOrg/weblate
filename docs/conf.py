aef14f4c98784f6256906bf5941d1db986245b82
Michal Čihař <michal@cihar.com>
2021-01-14 14:28:21 +0100

    Setting version to 4.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = "2012 - 2021 Michal Čihař"
 author = "Michal Čihař"
 
 # The full version, including alpha/beta/rc tags
-release = "4.4.1"
+release = "4.4.2"
 
 
 # -- General configuration ---------------------------------------------------
