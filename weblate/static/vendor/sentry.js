dd5307b6a20cb576e56606213e1f5eb09f4cb8f7
Michal Čihař <michal@cihar.com>
2021-01-22 20:20:03 +0100

    JS: Bump @sentry/browser from 6.0.0 to 6.0.1 (#5252)

--- weblate/static/vendor/sentry.js
+++ weblate/static/vendor/sentry.js
@@ -1,4 +1,4 @@
-/*! @sentry/browser 6.0.0 (22de19a) | https://github.com/getsentry/sentry-javascript */
+/*! @sentry/browser 6.0.1 (a82493c) | https://github.com/getsentry/sentry-javascript */
 var Sentry = (function (exports) {
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
@@ -4227,7 +4227,7 @@ var Sentry = (function (exports) {
         hub.bindClient(client);
     }
 
-    var SDK_VERSION = '6.0.0';
+    var SDK_VERSION = '6.0.1';
 
     var originalFunctionToString;
     /** Patch toString calls to return proper name for wrapped functions */
