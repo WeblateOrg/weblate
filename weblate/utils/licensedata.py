9cb95ca408e93bcee5c176c17920e567a827df93
Michal Čihař <michal@cihar.com>
2021-02-05 07:33:43 +0100

    Update license data
    See #5342

--- weblate/utils/licensedata.py
+++ weblate/utils/licensedata.py
@@ -1989,7 +1989,7 @@ LICENSES = (
         "OFL-1.0",
         "SIL Open Font License 1.0",
         "http://scripts.sil.org/cms/scripts/page.php?item_id=OFL10_web",
-        True,
+        False,
     ),
     (
         "OFL-1.0-no-RFN",
