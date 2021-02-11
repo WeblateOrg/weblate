98a72fe1705bed5e0fca4e05333ccdd868c4453b
Michal Čihař <michal@cihar.com>
2021-02-11 09:32:34 +0100

    Machinery: Prefetch units in machinery
    We display component name in the results, so fetch these.

--- weblate/machinery/weblatetm.py
+++ weblate/machinery/weblatetm.py
@@ -67,7 +67,7 @@ class WeblateTranslation(BatchStringMachineTranslation):
             translation__component__source_language=source,
             translation__language=language,
             state__gte=STATE_TRANSLATED,
-        )
+        ).prefetch()
 
         # We want only close matches here
         adjust_similarity_threshold(0.95)
