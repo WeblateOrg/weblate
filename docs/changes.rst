161aa7c9d74b539fe9d4f3d851930568486460e0
Michal Čihař <michal@cihar.com>
2021-03-05 21:29:52 +0100

    Setting version to 4.5.2

--- docs/changes.rst
+++ docs/changes.rst
@@ -1,3 +1,8 @@
+Weblate 4.5.2
+-------------
+
+Not yet released.
+
 Weblate 4.5.1
 -------------
 
--- weblate/utils/version.py
+++ weblate/utils/version.py
@@ -35,7 +35,7 @@ def get_root_dir():
 
 
 # Weblate version
-VERSION = "4.5.1"
+VERSION = "4.5.2-dev"
 
 # Version string without suffix
 VERSION_BASE = VERSION.replace("-dev", "")
