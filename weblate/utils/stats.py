e48c57b84f7a1c30b724d6203474fbd19f78716b
Michal Čihař <michal@cihar.com>
2021-03-08 19:10:35 +0100

    Utils: Bring back translation_set to ProjectLanguage

--- weblate/utils/stats.py
+++ weblate/utils/stats.py
@@ -757,6 +757,23 @@ class ProjectLanguage:
         )
 
     @cached_property
+    def translation_set(self):
+        result = (
+            self.language.translation_set.prefetch()
+            .filter(
+                Q(component__project=self.project) | Q(component__links=self.project)
+            )
+            .order_by("component__priority", "component__name")
+        )
+        for item in result:
+            item.is_shared = (
+                None
+                if item.component.project == self.project
+                else item.component.project
+            )
+        return result
+
+    @cached_property
     def is_source(self):
         return all(
             self.language.id == component.source_language_id
