d2c07bc76bd0a70fc701e3293f87b09c24a3f473
Michal Čihař <michal@cihar.com>
2021-03-17 20:01:27 +0100

    Checks: Use id_hash for consistency check
    This should be faster and avoids case-sensitivity issues on MySQL.

--- weblate/checks/consistency.py
+++ weblate/checks/consistency.py
@@ -110,7 +110,7 @@ class ConsistencyCheck(TargetCheck):
 
         # List strings with different targets
         matches = (
-            units.values("source", "context", "translation__language")
+            units.values("id_hash", "translation__language")
             .annotate(Count("target", distinct=True))
             .filter(target__count__gt=1)
         )
@@ -123,8 +123,7 @@ class ConsistencyCheck(TargetCheck):
                 reduce(
                     lambda x, y: x
                     | (
-                        Q(source=y["source"])
-                        & Q(context=y["context"])
+                        Q(source=y["id_hash"])
                         & Q(translation__language=y["translation__language"])
                     ),
                     matches,
