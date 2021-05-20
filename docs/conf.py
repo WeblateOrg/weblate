a677222078d62ba0dcece1b5050e1ad3e9877ece
Michal Čihař <michal@cihar.com>
2021-05-20 14:09:03 +0200

    docs: Exclude example from rendered docs

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,13 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "admin/install/steps/*.rst"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "admin/install/steps/*.rst",
+    "devel/reporting-example.rst",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
