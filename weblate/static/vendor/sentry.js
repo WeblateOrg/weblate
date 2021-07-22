c561f45750d7714f870a26e5dd3a4dd782ba09af
Michal Čihař <michal@cihar.com>
2021-07-22 20:20:15 +0200

    js: update dependency @sentry/browser to v6.10.0 #6324

--- weblate/static/vendor/sentry.js
+++ weblate/static/vendor/sentry.js
@@ -1,4 +1,4 @@
-/*! @sentry/browser 6.9.0 (7a46d42) | https://github.com/getsentry/sentry-javascript */
+/*! @sentry/browser 6.10.0 (1713dd9) | https://github.com/getsentry/sentry-javascript */
 var Sentry = (function (exports) {
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
@@ -4445,7 +4445,7 @@ var Sentry = (function (exports) {
         hub.bindClient(client);
     }
 
-    var SDK_VERSION = '6.9.0';
+    var SDK_VERSION = '6.10.0';
 
     var originalFunctionToString;
     /** Patch toString calls to return proper name for wrapped functions */
