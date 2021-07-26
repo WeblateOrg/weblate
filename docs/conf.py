e4804bbbeaab70a438fff3e5e41cded070407e45
Michal Čihař <michal@cihar.com>
2021-07-26 07:45:26 +0200

    docs: Use Source Sans 3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def setup(app):
-    app.add_css_file("https://s.weblate.org/cdn/font-source/source-sans-pro.css")
+    app.add_css_file("https://s.weblate.org/cdn/font-source/source-sans-3.css")
     app.add_css_file("https://s.weblate.org/cdn/font-source/source-code-pro.css")
     app.add_css_file("docs.css")
     # Used in Sphinx docs, needed for intersphinx links to it
