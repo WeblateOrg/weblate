46b3a95739b6d84ca29ad4df057ac752ba0ba110
Michal Čihař <michal@cihar.com>
2021-10-22 11:41:43 +0200

    backups: Clarify using remote URLs with a port
    Fixes #6683

--- weblate/wladmin/models.py
+++ weblate/wladmin/models.py
@@ -161,7 +161,8 @@ class BackupService(models.Model):
         verbose_name=gettext_lazy("Backup repository URL"),
         help_text=gettext_lazy(
             "Use /path/to/repo for local backups "
-            "or user@host:/path/to/repo for remote SSH backups."
+            "or user@host:/path/to/repo "
+            "or ssh://user@host:port/path/to/backups for remote SSH backups."
         ),
     )
     enabled = models.BooleanField(default=True)
