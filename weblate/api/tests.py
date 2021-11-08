8cffe044d84dab0811ea6766de90a80df092ac46
Michal Čihař <michal@cihar.com>
2021-11-08 12:21:27 +0100

    Update weblate/api/tests.py

--- weblate/api/tests.py
+++ weblate/api/tests.py
@@ -1843,7 +1843,7 @@ class ComponentAPITest(APIBaseTest):
         project = self.component.project
         project.access_control = Project.ACCESS_PROTECTED
         project.save(update_fields=["access_control"])
-        response = self.do_request(
+        self.do_request(
             "api:component-download-archive",
             self.component_kwargs,
             method="get",
