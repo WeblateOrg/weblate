dc5a02ee8fb42d0c08a0e717f60fc7d37a17e886
Michal Čihař <michal@cihar.com>
2021-11-22 10:35:36 +0100

    fonts: Move font definition to variable

--- weblate/trans/widgets.py
+++ weblate/trans/widgets.py
@@ -50,6 +50,7 @@ COLOR_DATA = {
 }
 
 WIDGETS = {}
+WIDGET_FONT = "Source Sans 3"
 
 
 def register_widget(widget):
@@ -155,8 +156,8 @@ class BitmapWidget(ContentWidget):
 
     def get_column_fonts(self):
         return [
-            Pango.FontDescription(f"Source Sans 3 {self.font_size * 1.5}"),
-            Pango.FontDescription(f"Source Sans 3 {self.font_size}"),
+            Pango.FontDescription(f"{WIDGET_FONT} {self.font_size * 1.5}"),
+            Pango.FontDescription(f"{WIDGET_FONT} {self.font_size}"),
         ]
 
     def render_additional(self, ctx):
@@ -307,8 +308,8 @@ class OpenGraphWidget(NormalWidget):
 
     def get_column_fonts(self):
         return [
-            Pango.FontDescription(f"Source Sans 3 {42}"),
-            Pango.FontDescription(f"Source Sans 3 {18}"),
+            Pango.FontDescription(f"{WIDGET_FONT} {42}"),
+            Pango.FontDescription(f"{WIDGET_FONT} {18}"),
         ]
 
     def get_name(self) -> str:
@@ -321,7 +322,7 @@ class OpenGraphWidget(NormalWidget):
     def render_additional(self, ctx):
         ctx.move_to(280, 170)
         layout = PangoCairo.create_layout(ctx)
-        layout.set_font_description(Pango.FontDescription(f"Source Sans 3 {52}"))
+        layout.set_font_description(Pango.FontDescription(f"{WIDGET_FONT} {52}"))
         name = self.get_name()
         layout.set_markup(self.get_title(name))
 
