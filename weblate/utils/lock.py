e7026943a57eb091c17ab94efc784f8c42382ac0
Michal Čihař <michal@cihar.com>
2022-01-05 12:57:18 +0100

    utils: Extend redis lock period
    This avoids missing lock renewal in case of long Python operations.
    
    Fixes WEBLATE-54D

--- weblate/utils/lock.py
+++ weblate/utils/lock.py
@@ -55,7 +55,7 @@ class WeblateLock:
             self._lock = Lock(
                 default_cache.client.get_client(),
                 name=self._format_template(cache_template),
-                expire=6,
+                expire=60,
                 auto_renewal=True,
             )
         else:
