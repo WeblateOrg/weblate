WeblateOrg/weblate

commit 2f93229b51356c5c58afab979fa68a04c79c9673

add-ons: Include only one Change-Id in the squashed commit
Otherwise Gerrit rejects such commmit.

Fixes #6414

weblate/addons/git.py
@@ -91,11 +91,21 @@ class GitSquashAddon(BaseAddon):
             if filenames:
                 command += ["--"] + filenames
 
-            trailer_lines = {
-                trailer
-                for trailer in repository.execute(command).split("\n")
-                if trailer.strip()
-            }
+            trailer_lines = set()
+            seen_change_id = False
+            for trailer in repository.execute(command).split("\n"):
+                # Skip blank lines
+                if not trailer.strip():
+                    continue
+
+                # Pick only first Change-Id, there suppose to be only one in the
+                # commit (used by Gerrit)
+                if trailer.startswith("Change-Id:"):
+                    if seen_change_id:
+                        continue
+                    seen_change_id = True
+
+                trailer_lines.add(trailer)
 
             if commit_message:
                 # Predefined commit message

commit f558a4f6cf88d264f5c43f6802ae2c4ad9c5665c

addons: Use last Change-Id instead of first one
Picking the first ends up duplicating commits, every time it gets squashed it gets a new Change-Id.

Pick the last instead to hopefully try and avoid that.

Fixes: 2f93229
See #7067

weblate/addons/git.py
@@ -92,20 +92,22 @@ class GitSquashAddon(BaseAddon):
                 command += ["--"] + filenames
 
             trailer_lines = set()
-            seen_change_id = False
+            change_id_line = None
             for trailer in repository.execute(command).split("\n"):
                 # Skip blank lines
                 if not trailer.strip():
                     continue
 
-                # Pick only first Change-Id, there suppose to be only one in the
+                # Pick only last Change-Id, there suppose to be only one in the
                 # commit (used by Gerrit)
                 if trailer.startswith("Change-Id:"):
-                    if seen_change_id:
-                        continue
-                    seen_change_id = True
+                    change_id_line = trailer
+                    continue
 
                 trailer_lines.add(trailer)
+
+            if change_id_line is not None:
+                trailer_lines.add(change_id_line)
 
             if commit_message:
                 # Predefined commit message