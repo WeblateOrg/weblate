b6e34609f1b0de690d0fa406fcb0fd6e68dec520
zzj <29055749+zjzh@users.noreply.github.com>
2022-01-21 23:51:19 +0800

    screenshots: Utilize set comprehension instead of nested loops (#7128)
    refactoring code with set comprehension which is more concise and efficient

--- weblate/screenshots/views.py
+++ weblate/screenshots/views.py
@@ -266,13 +266,13 @@ def ocr_search(request, pk):
     sources = dict(translation.unit_set.values_list("source", "pk"))
     strings = tuple(sources.keys())
 
-    results = set()
-
     # Extract and match strings
     with c_locale(), PyTessBaseAPI() as api:
-        for image in (original_image, scaled_image):
-            for match in ocr_extract(api, image, strings):
-                results.add(sources[match])
+        results = {
+            sources[match]
+            for image in (original_image, scaled_image)
+            for match in ocr_extract(api, image, strings)
+        }
 
     # Close images
     original_image.close()
