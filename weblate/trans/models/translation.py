2bed4294f1e3957b360e9bd407955e834cb7618c
Michal Čihař <michal@cihar.com>
2022-02-07 08:56:12 +0100

    translation: Log stale strings removal

--- weblate/trans/models/translation.py
+++ weblate/trans/models/translation.py
@@ -466,6 +466,7 @@ class Translation(
         # Delete stale units
         stale = set(dbunits) - set(updated)
         if stale:
+            self.log_debug("deleting %d stale strings", len(stale))
             self.unit_set.filter(id_hash__in=stale).delete()
             self.component.needs_cleanup = True
 
