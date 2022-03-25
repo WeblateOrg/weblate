019c8b061eebb03f9b6678d83dbb110b79a8cec6
nijel <nijel@users.noreply.github.com>
2022-03-25 13:29:05 +0000

    utils: Update SPDX license data

--- weblate/utils/licensedata.py
+++ weblate/utils/licensedata.py
@@ -1342,12 +1342,6 @@ LICENSES = (
     ),
     ("JSON", "JSON License", "https://spdx.org/licenses/JSON.html", False),
     (
-        "KiCad-libraries-exception",
-        "KiCad Libraries Exception",
-        "https://spdx.org/licenses/KiCad-libraries-exception.html",
-        False,
-    ),
-    (
         "LPPL-1.0",
         "LaTeX Project Public License v1.0",
         "https://spdx.org/licenses/LPPL-1.0.html",
