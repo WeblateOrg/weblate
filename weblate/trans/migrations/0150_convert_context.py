a7c3d0acc77dd8ffa92ab9a8d74edd543c163ca6
Michal Čihař <michal@cihar.com>
2022-05-17 16:10:22 +0200

    formats: Fixed migration for uplicate id_hashes

--- weblate/trans/migrations/0150_convert_context.py
+++ weblate/trans/migrations/0150_convert_context.py
@@ -6,9 +6,10 @@ from weblate.utils.hash import calculate_hash
 
 
 def fix_convert_context(apps, schema_editor):
+    Translation = apps.get_model("trans", "Translation")
     Unit = apps.get_model("trans", "Unit")
-    units = Unit.objects.using(schema_editor.connection.alias).filter(
-        translation__component__file_format__in=(
+    translations = Translation.objects.using(schema_editor.connection.alias).filter(
+        component__file_format__in=(
             "html",
             "idml",
             "rc",
@@ -17,12 +18,27 @@ def fix_convert_context(apps, schema_editor):
             "mediawiki",
         )
     )
+    for translation in translations:
+        hashes = set()
+        duplicate_hashes = set()
+        units = translation.unit_set.all()
+        for unit in units:
+            unit.tmp_id_hash = calculate_hash(unit.source, "")
+            unit.location = unit.context
+            if unit.tmp_id_hash in hashes:
+                duplicate_hashes.add(unit.tmp_id_hash)
+            hashes.add(unit.tmp_id_hash)
 
-    for unit in units.iterator():
-        unit.location = unit.context
-        unit.context = ""
-        unit.id_hash = calculate_hash(unit.source)
-        unit.save(update_fields=["location", "context", "id_hash"])
+        for unit in units:
+            if unit.tmp_id_hash in duplicate_hashes:
+                unit.id_hash = calculate_hash(unit.source, unit.context)
+            else:
+                unit.id_hash = unit.tmp_id_hash
+                unit.context = ""
+
+        Unit.objects.using(schema_editor.connection.alias).bulk_update(
+            units, ["location", "context", "id_hash"]
+        )
 
 
 class Migration(migrations.Migration):
