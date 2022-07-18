2e9c2bb84da86f40c152aa15854593d383be9c6c
Michal Čihař <michal@cihar.com>
2022-07-18 15:06:28 +0200

    formats: Add appstore languge naming
    This is merely a BCP style with few overrides.
    
    Fixes #5522

--- weblate/formats/txt.py
+++ weblate/formats/txt.py
@@ -191,7 +191,7 @@ class AppStoreFormat(TranslationFormat):
     monolingual = True
     unit_class = TextUnit
     simple_filename = False
-    language_format = "bcp_legacy"
+    language_format = "appstore"
     create_style = "directory"
 
     def load(self, storefile, template_store):
