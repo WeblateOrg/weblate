18d0e54820c4c4322e34a1e82829e73ed3863270
Michal Čihař <michal@cihar.com>
2023-01-24 08:56:17 +0100

    lang: Tighten language aliases handling
    Avoid doing too many aliases lookups in some corner cases.

--- weblate/lang/models.py
+++ weblate/lang/models.py
@@ -159,9 +159,9 @@ class LanguageQuerySet(models.QuerySet):
         # Alias language code only
         for newcode in codes:
             language, _sep, country = newcode.partition("_")
-            if country and language in ALIASES:
+            if country and len(language) > 2 and language in ALIASES:
                 testcode = f"{ALIASES[language]}_{country}"
-                ret = self.fuzzy_get(code=testcode)
+                ret = self.fuzzy_get(code=testcode, strict=True)
                 if ret is not None:
                     return ret
 
