f19fd16ba97f3a94f971dc7622e27d3b83a2fdd2
Michal Čihař <michal@cihar.com>
2023-02-03 12:22:32 +0100

    Update weblate/vcs/tests/test_vcs.py

--- weblate/vcs/tests/test_vcs.py
+++ weblate/vcs/tests/test_vcs.py
@@ -1360,7 +1360,7 @@ class VCSLocalTest(VCSGitTest):
         raise SkipTest("Not supported")
 
 
-@override_settings(BITBUCKETSERVER_USERNAME="test", BITBUCKETSERVER_TOKEN="token")
+@override_settings(BITBUCKETSERVER_CREDENTIALS={"api.selfhosted.com":{"username": "test", "token": "token"}})
 class VCSBitbucketServerTest(VCSGitUpstreamTest):
     _class = BitbucketServerFakeRepository
     _vcs = "git"
