99bbd3f497c5001d6a13e89b2a11950d56bbc53b
Michal Čihař <michal@cihar.com>
2023-05-17 09:46:59 +0200

    accounts: Improve SAML authentication session aging

--- weblate/accounts/utils.py
+++ weblate/accounts/utils.py
@@ -123,9 +123,17 @@ def cycle_session_keys(request, user):
 
 
 def adjust_session_expiry(request):
-    """Set longer expiry for authenticated users."""
-    next_url = request.POST.get("next", request.GET.get("next"))
-    if next_url == "/idp/login/process/":
+    """
+    Adjust session expiry based on scope.
+
+    - Set longer expiry for authenticated users.
+    - Set short lived session for SAML authentication flow.
+    """
+    if "saml_only" not in request.session:
+        next_url = request.POST.get("next", request.GET.get("next"))
+        request.session["saml_only"] = next_url == "/idp/login/process/"
+
+    if request.session["saml_only"]:
         # Short lived session for SAML authentication only
         request.session.set_expiry(60)
     else:
