dbc170fb7549396d8118a65a693447ca4efca4f5
Michal Čihař <michal@cihar.com>
2023-05-18 12:45:42 +0200

    unit: Add md5 index on source/target/context fields
    This improves exact strings lookups like done in automatic translation
    or consistency checks.
    
    Issue #9118
    Issue #1994

--- weblate/utils/apps.py
+++ weblate/utils/apps.py
@@ -5,6 +5,7 @@
 from django.apps import AppConfig
 from django.core.checks import register
 from django.db.models import CharField, TextField
+from django.db.models.functions import MD5
 from django.db.models.lookups import Regex
 
 from weblate.utils.checks import (
@@ -55,17 +56,19 @@ class UtilsConfig(AppConfig):
         init_error_collection()
 
         if using_postgresql():
-            lookups = (
+            lookups = [
                 (PostgreSQLSearchLookup,),
                 (PostgreSQLSubstringLookup,),
                 (PostgreSQLRegexLookup, "trgm_regex"),
-            )
+            ]
         else:
-            lookups = (
+            lookups = [
                 (MySQLSearchLookup,),
                 (MySQLSearchLookup, "substring"),
                 (Regex, "trgm_regex"),
-            )
+            ]
+
+        lookups.append((MD5,))
 
         for lookup in lookups:
             CharField.register_lookup(*lookup)
