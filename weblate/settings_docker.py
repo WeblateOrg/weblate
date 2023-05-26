485a9649eb244a1065208e986f1dc677550e1acd
Jesse Redl <jredl@vendasta.com>
2023-05-25 19:28:49 -0600

    need a trailing comma

--- weblate/settings_docker.py
+++ weblate/settings_docker.py
@@ -192,7 +192,10 @@ STATIC_URL = f"{URL_PREFIX}/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "/usr/src/weblate/static"
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    "/usr/src/weblate/static",
 )
 
 # List of finder classes that know how to find static files in
