266a3c943338793556e7927cc7b07f4f0c8b38b6
Michal Čihař <michal@cihar.com>
2023-06-07 09:13:47 +0200

    manage: let makemessages handle pgettext_noop

--- weblate/utils/management/commands/makemessages.py
+++ weblate/utils/management/commands/makemessages.py
@@ -10,3 +10,10 @@ from weblate.utils.files import should_skip
 class Command(BaseCommand):
     def find_files(self, root):
         return [obj for obj in super().find_files(root) if not should_skip(obj.path)]
+
+    def build_potfiles(self):
+        if self.domain == "django":
+            self.xgettext_options = self.xgettext_options[:] + [
+                "--keyword=pgettext_noop:1c,2"
+            ]
+        return super().build_potfiles()
