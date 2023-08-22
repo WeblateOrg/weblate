807951a90b29f1826c52b3398393e7ef5b8a6d07
Michal Čihař <michal@cihar.com>
2023-08-22 08:57:27 +0200

    diff: Include efficency cleanup
    This produces nicer diff in some situations.

--- weblate/utils/diff.py
+++ weblate/utils/diff.py
@@ -2,26 +2,36 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
 
 from diff_match_patch import diff_match_patch
 from django.utils.html import format_html
 
 
 class Differ:
+    DIFF_DELETE = diff_match_patch.DIFF_DELETE
+    DIFF_INSERT = diff_match_patch.DIFF_INSERT
+    DIFF_EQUAL = diff_match_patch.DIFF_EQUAL
+
     def __init__(self):
         self.dmp = diff_match_patch()
 
-    def highlight(self, new, old):
+    def compare(self, new: str, old: str) -> list[tuple(str, str)]:
         dmp = self.dmp
         diff = dmp.diff_main(old, new)
         dmp.diff_cleanupSemantic(diff)
+        dmp.diff_cleanupEfficiency(diff)
+        return diff
+
+    def highlight(self, new: str, old: str) -> str:
+        diff = self.compare(new, old)
         output = []
         for op, data in diff:
-            if op == dmp.DIFF_DELETE:
+            if op == self.DIFF_DELETE:
                 template = "<del>{}</del>"
-            elif op == dmp.DIFF_INSERT:
+            elif op == self.DIFF_INSERT:
                 template = "<ins>{}</ins>"
-            elif op == dmp.DIFF_EQUAL:
+            elif op == self.DIFF_EQUAL:
                 template = "{}"
             output.append(format_html(template, data))
         return "".join(output)
