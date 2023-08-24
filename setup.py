55ee25f678245f8ae6e911367bafe21dacb21849
Michal Čihař <michal@cihar.com>
2023-08-24 20:16:00 +0200

    Setting version to 5.0.1

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ class WeblateBuild(build):
 
 setup(
     name="Weblate",
-    version="5.0",
+    version="5.0.1",
     python_requires=">=3.9",
     packages=find_packages(),
     include_package_data=True,
