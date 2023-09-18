b065c8b28adcccd9ed1b143f4dd85da9fef5f55c
Michal Čihař <michal@cihar.com>
2023-09-18 10:44:51 +0200

    Setting version to 5.1

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ copyright = "Michal Čihař"
 author = "Michal Čihař"
 
 # The full version, including alpha/beta/rc tags
-release = "5.0.2"
+release = "5.1"
 
 
 # -- General configuration ---------------------------------------------------
