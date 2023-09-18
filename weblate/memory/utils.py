e06f217ea9cbbfafba15a7a26974cb63a7be254b
Michal Čihař <michal@cihar.com>
2023-09-18 11:24:26 +0200

    memory: tighten is_valid_entry API
    Make sure that all parameters are passed to it.

--- weblate/memory/utils.py
+++ weblate/memory/utils.py
@@ -2,7 +2,14 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from weblate.trans.util import split_plural
+
+if TYPE_CHECKING:
+    from weblate.lang.models import Language
 
 CATEGORY_FILE = 1
 CATEGORY_SHARED = 2
@@ -25,6 +32,13 @@ def parse_category(category):
     return False, False, None, category - CATEGORY_USER_OFFSET
 
 
-def is_valid_entry(source: str, target: str, **kwargs):
+def is_valid_entry(
+    source: str,
+    target: str,
+    source_language: Language,
+    target_language: Language,
+    origin: str,
+    **kwargs,
+):
     """Validates whether translation memory entry has content."""
     return any(split_plural(source)) and any(split_plural(target))
