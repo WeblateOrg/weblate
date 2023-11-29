958bfc3d65cd10e046dbfef9cfda2249a72756cd
Michal Čihař <michal@cihar.com>
2023-11-29 15:26:40 +0100

    autotranslate: add validation on source language
    Without that, the automatic translation crashes with a ValueError.
    
    Fixes #10480

--- weblate/trans/forms.py
+++ weblate/trans/forms.py
@@ -874,7 +874,7 @@ class AutoForm(forms.Form):
         initial="others",
     )
     component = forms.ChoiceField(
-        label=gettext_lazy("Components"),
+        label=gettext_lazy("Component"),
         required=False,
         help_text=gettext_lazy(
             "Turn on contribution to shared translation memory for the project to "
@@ -994,6 +994,12 @@ class AutoForm(forms.Form):
                     raise ValidationError(gettext("Component not found!"))
             else:
                 raise ValidationError(gettext("Please provide valid component slug!"))
+        if result.source_language != self.obj.source_language:
+            raise ValidationError(
+                gettext(
+                    "Source component needs to have same source language as target one."
+                )
+            )
         return result.pk
 
 
