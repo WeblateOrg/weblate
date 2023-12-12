aee40a0a9483840b97df104733a1b02e8df5eb1e
Michal Čihař <michal@cihar.com>
2023-12-12 19:43:07 +0100

    git-exporter: use streaming response to send response from git
    This avoids buffering the git objects several times in the memory.

--- weblate/gitexport/views.py
+++ weblate/gitexport/views.py
@@ -6,9 +6,11 @@ import os.path
 import subprocess
 from base64 import b64decode
 from email import message_from_string
+from functools import partial
+from selectors import EVENT_READ, DefaultSelector
 
-from django.core.exceptions import PermissionDenied
-from django.http import Http404
+from django.core.exceptions import PermissionDenied, SuspiciousOperation
+from django.http import Http404, StreamingHttpResponse
 from django.http.response import HttpResponse, HttpResponseServerError
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -107,55 +109,117 @@ def git_export(request, path, git_request):
     return run_git_http(request, obj, git_request)
 
 
-def run_git_http(request, obj, git_request):
-    """Git HTTP backend execution wrapper."""
-    # Find Git HTTP backend
-    git_http_backend = find_git_http_backend()
-    if git_http_backend is None:
-        return HttpResponseServerError("git-http-backend not found")
+class GitHTTPBackendWrapper:
+    def __init__(self, obj, request, git_request: str):
+        self.path = os.path.join(obj.full_path, git_request)
+        self.obj = obj
+        self.request = request
+        self.selector = DefaultSelector()
+        self._headers = None
+        self._stderr = []
+        self._stdout = []
 
-    # Invoke Git HTTP backend
-    query = request.META.get("QUERY_STRING", "")
-    process_env = {
-        "REQUEST_METHOD": request.method,
-        "PATH_TRANSLATED": os.path.join(obj.full_path, git_request),
-        "GIT_HTTP_EXPORT_ALL": "1",
-        "CONTENT_TYPE": request.headers.get("content-type", ""),
-        "QUERY_STRING": query,
-        "HTTP_CONTENT_ENCODING": request.headers.get("content-encoding", ""),
-    }
-    process = subprocess.Popen(
-        [git_http_backend],
-        env=process_env,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    output, output_err = process.communicate(request.body)
-    retcode = process.poll()
+        # Find Git HTTP backend
+        git_http_backend = find_git_http_backend()
+        if git_http_backend is None:
+            raise SuspiciousOperation("git-http-backend not found")
 
-    # Log error
-    if output_err:
-        output_err = output_err.decode()
-        report_error(
-            cause="Git backend failure",
-            project=obj.project,
-            level="error",
-            message=True,
+        # Invoke Git HTTP backend
+        self.process = subprocess.Popen(
+            [git_http_backend],
+            env=self.get_env(),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            close_fds=True,
+            bufsize=0,
         )
 
-    # Handle failure
-    if retcode:
-        return HttpResponseServerError(output_err)
+    def get_env(self):
+        result = {
+            "REQUEST_METHOD": self.request.method,
+            "PATH_TRANSLATED": self.path,
+            "GIT_HTTP_EXPORT_ALL": "1",
+            "CONTENT_TYPE": self.request.headers.get("content-type", ""),
+            "QUERY_STRING": self.request.META.get("QUERY_STRING", ""),
+            "HTTP_CONTENT_ENCODING": self.request.headers.get("content-encoding", ""),
+        }
+        # Fault injection in tests
+        if "X_WEBLATE_NO_EXPORT" in self.request.headers:
+            del result["GIT_HTTP_EXPORT_ALL"]
+        return result
 
-    headers, content = output.split(b"\r\n\r\n", 1)
-    message = message_from_string(headers.decode())
+    def send_body(self):
+        self.process.stdin.write(self.request.body)
+        self.process.stdin.close()
 
-    # Handle status in response
-    if "status" in message:
-        return HttpResponse(status=int(message["status"].split()[0]))
+    def fetch_headers(self):
+        """Fetch initial chunk of response to parse headers."""
+        while True:
+            for key, _mask in self.selector.select(timeout=1):
+                if key.data:
+                    self._stdout.append(self.process.stdout.read(1024))
+                    headers = b"".join(self._stdout)
+                    if b"\r\n\r\n" in headers:
+                        self._headers, body = headers.split(b"\r\n\r\n", 1)
+                        self._stdout = [body]
+                else:
+                    self._stderr.append(self.process.stderr.read())
+            if self.process.poll() is not None or self._headers is not None:
+                break
 
-    # Send content
-    response = HttpResponse(content_type=message["content-type"])
-    response.write(content)
-    return response
+    def stream(self):
+        yield from self._stdout
+        yield from iter(partial(self.process.stdout.read, 1024), b"")
+
+    def get_response(self):
+        # Iniciate select()
+        self.selector.register(self.process.stdout, EVENT_READ, True)
+        self.selector.register(self.process.stderr, EVENT_READ, False)
+
+        # Send request body
+        self.send_body()
+
+        # Read initial chunk of response to parse headers
+        # This assumes that git-http-backend will fail here if it will fail
+        self.fetch_headers()
+
+        self.selector.unregister(self.process.stdout)
+        self.selector.unregister(self.process.stderr)
+        self.selector.close()
+
+        retcode = self.process.poll()
+
+        output_err = b"".join(self._stderr).decode()
+
+        # Log error
+        if output_err:
+            report_error(
+                cause="Git backend failure",
+                extra_log=output_err,
+                project=self.obj.project,
+                level="error",
+                message=True,
+            )
+
+        # Handle failure
+        if retcode:
+            return HttpResponseServerError(output_err)
+
+        message = message_from_string(self._headers.decode())
+
+        # Handle status in response
+        if "status" in message:
+            return HttpResponse(status=int(message["status"].split()[0]))
+
+        # Send streaming content as reponse
+        return StreamingHttpResponse(
+            streaming_content=self.stream(), content_type=message["content-type"]
+        )
+
+
+def run_git_http(request, obj, git_request):
+    """Git HTTP backend execution wrapper."""
+    # Invoke Git HTTP backend
+    wrapper = GitHTTPBackendWrapper(obj, request, git_request)
+    return wrapper.get_response()
