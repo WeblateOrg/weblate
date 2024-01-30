WeblateOrg/weblate

commit e3ff25a8ab62c590ac983ad04aac9f1fcb0686ec

add-ons: Refactor signals handling
Add generic handle_addon_event which includes all the logic and all the
signal handlers just call this. This heavily reduces code duplication
and makes the implementation consistent in all invocations. The new
code is also more robust and provides a better logging.

Fixes #10172

weblate/addons/models.py
@@ -2,10 +2,12 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
+
 import sentry_sdk
 from appconf import AppConf
 from django.db import Error as DjangoDatabaseError
-from django.db import models
+from django.db import models, transaction
 from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -13,7 +15,7 @@ from django.urls import reverse
 from django.utils.functional import cached_property
 
 from weblate.addons.events import AddonEvent
-from weblate.trans.models import Change, Component, Unit
+from weblate.trans.models import Change, Component, Translation, Unit
 from weblate.trans.signals import (
     component_post_update,
     store_post_load,
@@ -187,41 +189,86 @@ class AddonsConf(AppConf):
         prefix = ""
 
 
-def handle_addon_error(addon, component):
-    report_error(cause=f"add-on {addon.name} failed", project=component.project)
-    # Uninstall no longer compatible add-ons
-    if not addon.addon.can_install(component, None):
-        addon.disable()
+def handle_addon_event(
+    event: AddonEvent,
+    method: str | callable,
+    args: tuple | None = None,
+    *,
+    component: Component | None = None,
+    translation: Translation | None = None,
+    addon_queryset: AddonQuerySet | None = None,
+    auto_scope: bool = False,
+):
+    # Scope is used for logging
+    scope = translation or component
+
+    # Shortcuts for frequently used variables
+    if component is None and translation is not None:
+        component = translation.component
+
+    # EVENT_DAILY uses custom queryset because it is not triggered from the
+    # object scope
+    if addon_queryset is None:
+        addon_queryset = Addon.objects.filter_event(component, event)
+
+    # Filter matching events
+    for addon in addon_queryset:
+        with transaction.atomic():
+            # Extract scope from the add-on model, used for EVENT_DAILY
+            if auto_scope:
+                component = scope = addon.component
+
+            scope.log_debug("running %s add-on: %s", event.label, addon.name)
+            try:
+                # Execute event in senty span to track performance
+                with sentry_sdk.start_span(
+                    op=f"addon.{event.name}", description=addon.name
+                ):
+                    if isinstance(method, str):
+                        getattr(addon.addon, method)(*args)
+                    else:
+                        # Callback is used in tasks
+                        method(addon)
+            except DjangoDatabaseError:
+                raise
+            except Exception as error:
+                # Log failure
+                scope.log_error(
+                    "failed %s add-on: %s: %s", event.label, addon.name, error
+                )
+                report_error(
+                    cause=f"add-on {addon.name} failed", project=component.project
+                )
+                # Uninstall no longer compatible add-ons
+                if not addon.addon.can_install(component, None):
+                    scope.log_warning(
+                        "uninstalling incompatible %s add-on: %s",
+                        event.label,
+                        addon.name,
+                    )
+                    addon.disable()
+            else:
+                scope.log_debug("completed %s add-on: %s", event.label, addon.name)
 
 
 @receiver(vcs_pre_push)
 def pre_push(sender, component, **kwargs):
-    for addon in Addon.objects.filter_event(component, AddonEvent.EVENT_PRE_PUSH):
-        component.log_debug("running pre_push add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.pre_push", description=addon.name):
-                addon.addon.pre_push(component)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed pre_push add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_PRE_PUSH,
+        "pre_push",
+        (component,),
+        component=component,
+    )
 
 
 @receiver(vcs_post_push)
 def post_push(sender, component, **kwargs):
-    for addon in Addon.objects.filter_event(component, AddonEvent.EVENT_POST_PUSH):
-        component.log_debug("running post_push add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.post_push", description=addon.name):
-                addon.addon.post_push(component)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed post_push add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_POST_PUSH,
+        "post_push",
+        (component,),
+        component=component,
+    )
 
 
 @receiver(vcs_post_update)
@@ -233,160 +280,90 @@ def post_update(
     skip_push: bool = False,
     **kwargs,
 ):
-    for addon in Addon.objects.filter_event(component, AddonEvent.EVENT_POST_UPDATE):
-        if child and addon.repo_scope:
-            continue
-        component.log_debug("running post_update add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.post_update", description=addon.name):
-                addon.addon.post_update(component, previous_head, skip_push)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed post_update add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_POST_UPDATE,
+        "post_update",
+        (component, previous_head, skip_push),
+        component=component,
+    )
 
 
 @receiver(component_post_update)
 def component_update(sender, component, **kwargs):
-    for addon in Addon.objects.filter_event(
-        component, AddonEvent.EVENT_COMPONENT_UPDATE
-    ):
-        component.log_debug("running component_update add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(
-                op="addon.component_update", description=addon.name
-            ):
-                addon.addon.component_update(component)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed component_update add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_COMPONENT_UPDATE,
+        "component_update",
+        (component,),
+        component=component,
+    )
 
 
 @receiver(vcs_pre_update)
 def pre_update(sender, component, **kwargs):
-    for addon in Addon.objects.filter_event(component, AddonEvent.EVENT_PRE_UPDATE):
-        component.log_debug("running pre_update add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.pre_update", description=addon.name):
-                addon.addon.pre_update(component)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed pre_update add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_PRE_UPDATE,
+        "pre_update",
+        (component,),
+        component=component,
+    )
 
 
 @receiver(vcs_pre_commit)
 def pre_commit(sender, translation, author, **kwargs):
-    component = translation.component
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_PRE_COMMIT)
-    for addon in addons:
-        translation.log_debug("running pre_commit add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.pre_commit", description=addon.name):
-                addon.addon.pre_commit(translation, author)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            translation.log_debug("completed pre_commit add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_PRE_COMMIT,
+        "pre_commit",
+        (translation, author),
+        translation=translation,
+    )
 
 
 @receiver(vcs_post_commit)
 def post_commit(sender, component, **kwargs):
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_POST_COMMIT)
-    for addon in addons:
-        component.log_debug("running post_commit add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.post_commit", description=addon.name):
-                addon.addon.post_commit(component)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            component.log_debug("completed post_commit add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_POST_COMMIT,
+        "post_commit",
+        (component,),
+        component=component,
+    )
 
 
 @receiver(translation_post_add)
 def post_add(sender, translation, **kwargs):
-    component = translation.component
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_POST_ADD)
-    for addon in addons:
-        translation.log_debug("running post_add add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(op="addon.post_add", description=addon.name):
-                addon.addon.post_add(translation)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            translation.log_debug("completed post_add add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_POST_ADD,
+        "post_add",
+        (translation,),
+        translation=translation,
+    )
 
 
 @receiver(unit_pre_create)
 def unit_pre_create_handler(sender, unit, **kwargs):
-    translation = unit.translation
-    component = translation.component
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_UNIT_PRE_CREATE)
-    for addon in addons:
-        translation.log_debug("running unit_pre_create add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(
-                op="addon.unit_pre_create", description=addon.name
-            ):
-                addon.addon.unit_pre_create(unit)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            translation.log_debug("completed unit_pre_create add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_UNIT_PRE_CREATE,
+        "unit_pre_create",
+        (unit,),
+        translation=unit.translation,
+    )
 
 
 @receiver(post_save, sender=Unit)
 @disable_for_loaddata
 def unit_post_save_handler(sender, instance, created, **kwargs):
-    translation = instance.translation
-    component = translation.component
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_UNIT_POST_SAVE)
-    for addon in addons:
-        translation.log_debug("running unit_post_save add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(
-                op="addon.unit_post_save", description=addon.name
-            ):
-                addon.addon.unit_post_save(instance, created)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            translation.log_debug("completed unit_post_save add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_UNIT_POST_SAVE,
+        "unit_post_save",
+        (instance, created),
+        translation=instance.translation,
+    )
 
 
 @receiver(store_post_load)
 def store_post_load_handler(sender, translation, store, **kwargs):
-    component = translation.component
-    addons = Addon.objects.filter_event(component, AddonEvent.EVENT_STORE_POST_LOAD)
-    for addon in addons:
-        translation.log_debug("running store_post_load add-on: %s", addon.name)
-        try:
-            with sentry_sdk.start_span(
-                op="addon.store_post_load", description=addon.name
-            ):
-                addon.addon.store_post_load(translation, store)
-        except DjangoDatabaseError:
-            raise
-        except Exception:
-            handle_addon_error(addon, component)
-        else:
-            translation.log_debug("completed store_post_load add-on: %s", addon.name)
+    handle_addon_event(
+        AddonEvent.EVENT_STORE_POST_LOAD,
+        "store_post_load",
+        (translation, store),
+        translation=translation,
+    )

weblate/addons/tasks.py
@@ -7,15 +7,13 @@ from __future__ import annotations
 import os
 
 from celery.schedules import crontab
-from django.db import Error as DjangoDatabaseError
-from django.db import transaction
 from django.db.models import F, Q
 from django.http import HttpRequest
 from django.utils import timezone
 from lxml import html
 
 from weblate.addons.events import AddonEvent
-from weblate.addons.models import Addon, handle_addon_error
+from weblate.addons.models import Addon, handle_addon_event
 from weblate.lang.models import Language
 from weblate.trans.models import Component
 from weblate.utils.celery import app
@@ -104,19 +102,18 @@ def language_consistency(addon_id: int, language_ids: list[int]):
 
 @app.task(trail=False)
 def daily_addons():
+    def daily_callback(addon):
+        addon.addon.daily(addon.component)
+
     today = timezone.now()
-    addons = Addon.objects.annotate(hourmod=F("component_id") % 24).filter(
-        hourmod=today.hour, event__event=AddonEvent.EVENT_DAILY
+    handle_addon_event(
+        AddonEvent.EVENT_DAILY,
+        daily_callback,
+        addon_queryset=Addon.objects.annotate(hourmod=F("component_id") % 24).filter(
+            hourmod=today.hour, event__event=AddonEvent.EVENT_DAILY
+        ),
+        auto_scope=True,
     )
-    for addon in addons.prefetch_related("component"):
-        with transaction.atomic():
-            addon.component.log_debug("running daily add-on: %s", addon.name)
-            try:
-                addon.addon.daily(addon.component)
-            except DjangoDatabaseError:
-                raise
-            except Exception:
-                handle_addon_error(addon, addon.component)
 
 
 @app.task(trail=False)

commit 0b282971fb0acf225ee2377d3390a663d22c83f0

add-ons: trigger discovery on the main repo only
This is a regression introduced in
e3ff25a8ab62c590ac983ad04aac9f1fcb0686ec - it removed child condition
from the repository scoped add-ons causing discovery add-on being
triggered on all shared repos instead of only main one.

See #10892

weblate/addons/base.py
@@ -135,7 +135,7 @@ class BaseAddon:
         if AddonEvent.EVENT_POST_UPDATE in self.events:
             component.log_debug("running post_update add-on: %s", self.name)
             component.commit_pending("add-on", None)
-            self.post_update(component, "", False)
+            self.post_update(component, "", False, False)
         if AddonEvent.EVENT_COMPONENT_UPDATE in self.events:
             component.log_debug("running component_update add-on: %s", self.name)
             self.component_update(component)
@@ -179,7 +179,7 @@ class BaseAddon:
         """Hook triggered before repository is updated from upstream."""
         # To be implemented in a subclass
 
-    def post_update(self, component, previous_head: str, skip_push: bool):
+    def post_update(self, component, previous_head: str, skip_push: bool, child: bool):
         """
         Hook triggered after repository is updated from upstream.
 
@@ -373,7 +373,7 @@ class UpdateBaseAddon(BaseAddon):
     def update_translations(self, component, previous_head):
         raise NotImplementedError
 
-    def post_update(self, component, previous_head: str, skip_push: bool):
+    def post_update(self, component, previous_head: str, skip_push: bool, child: bool):
         # Ignore file parse error, it will be properly tracked as an alert
         with suppress(FileParseError):
             self.update_translations(component, previous_head)

weblate/addons/cdn.py
@@ -145,5 +145,5 @@ class CDNJSAddon(BaseAddon):
             component.id,
         )
 
-    def post_update(self, component, previous_head: str, skip_push: bool):
+    def post_update(self, component, previous_head: str, skip_push: bool, child: bool):
         self.daily(component)

weblate/addons/cleanup.py
@@ -68,4 +68,4 @@ class RemoveBlankAddon(BaseCleanupAddon):
                 translation.store_hash()
 
     def post_commit(self, component):
-        self.post_update(component, "weblate:post-commit", skip_push=True)
+        self.post_update(component, "weblate:post-commit", skip_push=True, child=False)

weblate/addons/discovery.py
@@ -25,7 +25,9 @@ class DiscoveryAddon(BaseAddon):
     repo_scope = True
     trigger_update = True
 
-    def post_update(self, component, previous_head: str, skip_push: bool):
+    def post_update(self, component, previous_head: str, skip_push: bool, child: bool):
+        if child:
+            return
         self.discovery.perform(
             remove=self.instance.configuration.get("remove"), background=True
         )

weblate/addons/models.py
@@ -283,7 +283,7 @@ def post_update(
     handle_addon_event(
         AddonEvent.EVENT_POST_UPDATE,
         "post_update",
-        (component, previous_head, skip_push),
+        (component, previous_head, skip_push, child),
         component=component,
     )
 

weblate/addons/scripts.py
@@ -49,7 +49,7 @@ class BaseScriptAddon(BaseAddon):
     def post_push(self, component):
         self.run_script(component)
 
-    def post_update(self, component, previous_head: str, skip_push: bool):
+    def post_update(self, component, previous_head: str, skip_push: bool, child: bool):
         self.run_script(component, env={"WL_PREVIOUS_HEAD": previous_head})
 
     def post_commit(self, component):

weblate/addons/tests.py
@@ -158,7 +158,7 @@ class IntegrationTest(TestAddonMixin, ViewTestCase):
         ADDONS[TestCrashAddon.get_identifier()] = TestCrashAddon
 
         with self.assertRaises(TestError):
-            addon.post_update(self.component, "head", False)
+            addon.post_update(self.component, "head", False, False)
 
         # The crash should be handled here and addon uninstalled
         self.component.trigger_post_update(
@@ -254,9 +254,9 @@ class GettextAddonTest(ViewTestCase):
         addon = MsgmergeAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, rev, False)
+        addon.post_update(self.component, rev, False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("po/cs.po", commit)
@@ -385,9 +385,9 @@ class AppStoreAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("cs/changelogs/100000.txt", commit)
 
@@ -402,9 +402,9 @@ class AndroidAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("android-not-synced/values-cs/strings.xml", commit)
         self.assertIn('\n-    <string name="hello">Ahoj svete</string>', commit)
@@ -420,9 +420,9 @@ class WindowsRCAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("winrc/cs-CZ.rc", commit)
         self.assertIn("\n-IDS_MSG5", commit)
@@ -438,9 +438,9 @@ class IntermediateAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         commit = self.component.repository.show(self.component.repository.last_revision)
         # It should remove string not present in the English file
         self.assertIn("intermediate/cs.json", commit)
@@ -459,7 +459,7 @@ class ResxAddonTest(ViewTestCase):
         with self.component.repository.lock:
             self.component.repository.execute(["fetch", "--unshallow", "origin"])
         addon.post_update(
-            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False
+            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False, False
         )
         self.assertNotEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
@@ -473,7 +473,7 @@ class ResxAddonTest(ViewTestCase):
         with self.component.repository.lock:
             self.component.repository.execute(["fetch", "--unshallow", "origin"])
         addon.post_update(
-            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False
+            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False, False
         )
         self.assertNotEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
@@ -489,7 +489,7 @@ class CSVAddonTest(ViewTestCase):
         rev = self.component.repository.last_revision
         addon = CleanupAddon.create(self.component)
         addon.post_update(
-            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False
+            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False, False
         )
         self.assertNotEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
@@ -500,7 +500,7 @@ class CSVAddonTest(ViewTestCase):
         rev = self.component.repository.last_revision
         addon = RemoveBlankAddon.create(self.component)
         addon.post_update(
-            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False
+            self.component, "da07dc0dc7052dc44eadfa8f3a2f2609ec634303", False, False
         )
         self.assertNotEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
@@ -517,7 +517,7 @@ class JsonAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("json-mono-sync/cs.json", commit)
@@ -528,7 +528,7 @@ class JsonAddonTest(ViewTestCase):
         addon = RemoveBlankAddon.create(self.component)
         self.assertNotEqual(rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("json-mono-sync/cs.json", commit)
@@ -708,9 +708,9 @@ class PropertiesAddonTest(ViewTestCase):
         addon = CleanupAddon.create(self.component)
         self.assertNotEqual(init_rev, self.component.repository.last_revision)
         rev = self.component.repository.last_revision
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(rev, self.component.repository.last_revision)
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         commit = self.component.repository.show(self.component.repository.last_revision)
         self.assertIn("java/swing_messages_cs.properties", commit)
         self.component.do_reset()
@@ -888,7 +888,7 @@ class DiscoveryTest(ViewTestCase):
             )
         self.assertEqual(Component.objects.filter(repo=link).count(), 3)
         with override_settings(CREATE_GLOSSARIES=self.CREATE_GLOSSARIES):
-            addon.post_update(self.component, "", False)
+            addon.post_update(self.component, "", False, False)
         self.assertEqual(Component.objects.filter(repo=link).count(), 3)
 
     def test_form(self):
@@ -1008,7 +1008,7 @@ class LanguageConsistencyTest(ViewTestCase):
         )
 
         # Trigger post update signal, should do nothing
-        addon.post_update(self.component, "", False)
+        addon.post_update(self.component, "", False, False)
         self.assertEqual(Translation.objects.count(), 15)
 
 