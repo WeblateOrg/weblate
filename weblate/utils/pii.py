576190397a8f643685bb2f9ff2aefa7c36e626d2
Michal Čihař <michal@cihar.com>
2024-01-30 11:27:46 +0100

    utils: factor out string masking
    This makes the code more readable and avoids ambiguous string unpacking.

--- weblate/utils/pii.py
+++ weblate/utils/pii.py
@@ -3,21 +3,31 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-def mask_email(email: str):
+def mask_string(string: str, include_first: bool = False, limit: int = 3) -> str:
+    length = len(string)
+    if length <= limit:
+        return length * "*"
+
+    parts = []
+    if include_first:
+        parts.append(string[0])
+        string = string[1:]
+        length -= 1
+
+    parts.append("*" * (length - 1))
+    parts.append(string[-1])
+    return "".join(parts)
+
+
+def mask_email(email: str) -> str:
     name, domain = email.rsplit("@", maxsplit=1)
-    if len(name) <= 3:
-        masked_name = len(name) * "*"
-    else:
-        first, *hidden, last = name
-        masked_name = f"{first}{'*' * len(hidden)}{last}"
+    masked_name = mask_string(name, include_first=True)
 
     if len(domain) <= 4 or "." not in domain:
-        *hidden, last = domain
-        masked_domain = f"{len(hidden) * '*'}{last}"
+        masked_domain = mask_string(domain, limit=0)
     else:
         part, tld = domain.rsplit(".", maxsplit=1)
-        *hidden, last = tld
-        masked_tld = f"{len(hidden) * '*'}{last}"
+        masked_tld = mask_string(tld, limit=0)
         masked_domain = f"{len(part) * '*'}.{masked_tld}"
 
     return f"{masked_name}@{masked_domain}"
