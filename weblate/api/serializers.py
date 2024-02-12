WeblateOrg/weblate

commit a616e0bbe75e3315253eb43f704def6198fe4a65

api: project endpoint to get and create labels (#10453)
* feat: project endpoint to get and create labels

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* feat: merge label serializers

* tests: label API

* fix: ordered label QuerySet for pagination

* chore: format

* docs: API labels read and create

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

weblate/api/serializers.py
@@ -377,6 +377,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     languages_url = serializers.HyperlinkedIdentityField(
         view_name="api:project-languages", lookup_field="slug"
     )
+    labels_url = serializers.HyperlinkedIdentityField(
+        view_name="api:project-labels", lookup_field="slug"
+    )
 
     class Meta:
         model = Project
@@ -393,6 +396,7 @@ class ProjectSerializer(serializers.ModelSerializer):
             "categories_url",
             "changes_list_url",
             "languages_url",
+            "labels_url",
             "translation_review",
             "source_review",
             "set_language_team",
@@ -988,10 +992,14 @@ class MemorySerializer(serializers.ModelSerializer):
         )
 
 
-class LabelsSerializer(serializers.RelatedField):
+class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
+        fields = ("id", "name", "color")
+        read_only_fields = ("project",)
 
+
+class UnitLabelsSerializer(serializers.RelatedField, LabelSerializer):
     def get_queryset(self):
         """
         List of available labels for an unit.
@@ -1008,15 +1016,12 @@ class LabelsSerializer(serializers.RelatedField):
         project = unit.translation.component.project
         return project.label_set.all()
 
-    def to_representation(self, value):
-        return value.name
-
     def to_internal_value(self, data):
         try:
-            label = self.get_queryset().get(name=data)
+            label = self.get_queryset().get(id=data)
         except Label.DoesNotExist as err:
             raise serializers.ValidationError(
-                "Label with this name was not found."
+                "Label with this ID was not found in this project."
             ) from err
         return label
 
@@ -1039,7 +1044,7 @@ class UnitSerializer(serializers.ModelSerializer):
     target = PluralField()
     timestamp = serializers.DateTimeField(read_only=True)
     pending = serializers.BooleanField(read_only=True)
-    labels = LabelsSerializer(many=True, read_only=True)
+    labels = UnitLabelsSerializer(many=True)
 
     class Meta:
         model = Unit
@@ -1081,7 +1086,7 @@ class UnitWriteSerializer(serializers.ModelSerializer):
     """Serializer for updating source unit."""
 
     target = PluralField()
-    labels = LabelsSerializer(many=True)
+    labels = UnitLabelsSerializer(many=True)
 
     class Meta:
         model = Unit

weblate/api/tests.py
@@ -3235,26 +3235,26 @@ class UnitAPITest(APIBaseTest):
             method="patch",
             code=403,
             superuser=True,
-            request={"labels": "test"},
+            request={"labels": [label1.id]},
         )
 
         # Edit on source will fail when label doesn't exist
         # or is not in the same project
         self.do_request(
             "api:unit-detail",
-            kwargs={"pk": unit.pk},
+            kwargs={"pk": unit.source_unit.pk},
             method="patch",
             code=400,
             superuser=True,
-            request={"labels": "foo"},
+            request={"labels": [4000]},
         )
         self.do_request(
             "api:unit-detail",
-            kwargs={"pk": unit.pk},
+            kwargs={"pk": unit.source_unit.pk},
             method="patch",
             code=400,
             superuser=True,
-            request={"labels": "test_2"},
+            request={"labels": [label2.id]},
         )
 
         # Edit on source will work when label exists
@@ -3264,7 +3264,7 @@ class UnitAPITest(APIBaseTest):
             method="patch",
             code=200,
             superuser=True,
-            request={"labels": "test"},
+            request={"labels": [label1.id]},
         )
 
         # Label should be now updated
@@ -4014,3 +4014,48 @@ class CategoryAPITest(APIBaseTest):
 
         for translation in response.data["results"]:
             self.do_request(translation["url"])
+
+
+class LabelAPITest(APIBaseTest):
+    def test_get_label(self):
+        label = self.component.project.label_set.create(name="test", color="navy")
+
+        response = self.do_request(
+            "api:project-labels",
+            kwargs={"slug": self.component.project.slug},
+            method="get",
+            code=200,
+        )
+
+        self.assertEqual(len(response.data["results"]), 1)
+
+        response_label = response.data["results"][0]
+
+        self.assertEqual(response_label["id"], label.id)
+        self.assertEqual(response_label["name"], label.name)
+        self.assertEqual(response_label["color"], label.color)
+
+    def test_create_label(self):
+        self.do_request(
+            "api:project-labels",
+            kwargs={"slug": Project.objects.first().slug},
+            method="post",
+            superuser=True,
+            request={
+                "name": "Test Label",
+                "color": "green",
+            },
+            code=201,
+        )
+
+        self.do_request(
+            "api:project-labels",
+            kwargs={"slug": Project.objects.first().slug},
+            method="post",
+            superuser=False,
+            request={
+                "name": "Test Label 2",
+                "color": "red",
+            },
+            code=403,
+        )

weblate/api/views.py
@@ -51,6 +51,7 @@ from weblate.api.serializers import (
     ComponentSerializer,
     FullUserSerializer,
     GroupSerializer,
+    LabelSerializer,
     LanguageSerializer,
     LockRequestSerializer,
     LockSerializer,
@@ -754,6 +755,31 @@ class ProjectViewSet(
         page = self.paginate_queryset(queryset)
 
         serializer = ChangeSerializer(page, many=True, context={"request": request})
+
+        return self.get_paginated_response(serializer.data)
+
+    @action(detail=True, methods=["get", "post"])
+    def labels(self, request, **kwargs):
+        obj = self.get_object()
+
+        if request.method == "POST":
+            if not request.user.has_perm("project.edit", obj):
+                self.permission_denied(request, "Can not create labels")
+            with transaction.atomic():
+                serializer = LabelSerializer(
+                    data=request.data, context={"request": request, "project": obj}
+                )
+                serializer.is_valid(raise_exception=True)
+                serializer.save(project=obj)
+                return Response(
+                    serializer.data,
+                    status=HTTP_201_CREATED,
+                )
+
+        queryset = obj.label_set.all().order_by("id")
+        page = self.paginate_queryset(queryset)
+
+        serializer = LabelSerializer(page, many=True, context={"request": request})
 
         return self.get_paginated_response(serializer.data)
 

commit 313208b7ec2b43f6124b1419df0d335b0e067f25

api: Fix rendering HTML view for unit
This is a regression introduced by a616e0bbe75e3315253eb43f704def6198fe4a65 (#10453).

Fixes #10978
Fixes WEBLATE-6ZB

weblate/api/serializers.py
@@ -1013,6 +1013,11 @@ class UnitLabelsSerializer(serializers.RelatedField, LabelSerializer):
         return label
 
 
+class UnitFlatLabelsSerializer(UnitLabelsSerializer):
+    def to_representation(self, value):
+        return value.id
+
+
 class UnitSerializer(serializers.ModelSerializer):
     web_url = AbsoluteURLField(source="get_absolute_url", read_only=True)
     translation = MultiFieldHyperlinkedIdentityField(
@@ -1073,7 +1078,7 @@ class UnitWriteSerializer(serializers.ModelSerializer):
     """Serializer for updating source unit."""
 
     target = PluralField()
-    labels = UnitLabelsSerializer(many=True)
+    labels = UnitFlatLabelsSerializer(many=True)
 
     class Meta:
         model = Unit