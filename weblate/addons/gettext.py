WeblateOrg/weblate

commit fb6486b3b9cb6d0f1c4a64c97248f45b9759235f

addons: avoid changes when only whitespace differs

weblate/addons/gettext.py
@@ -196,7 +196,7 @@ class UpdateConfigureAddon(GettextBaseAddon):
                     continue
                 if not stripped.startswith('ALL_LINGUAS="'):
                     continue
-                if lines[i] != expected:
+                if stripped != expected:
                     lines[i] = expected
                     added = True
 

commit 2d77bc2065e2a5ea360da0d895bbce2002f1aa0e

add-ons: fix ALL_LINGUAS unnecessary updates
This was a regression in fb6486b3b9cb6d0f1c4a64c97248f45b9759235f.

weblate/addons/gettext.py
@@ -196,7 +196,7 @@ class UpdateConfigureAddon(GettextBaseAddon):
                     continue
                 if not stripped.startswith('ALL_LINGUAS="'):
                     continue
-                if stripped != expected:
+                if line != expected:
                     lines[i] = expected
                     added = True
 