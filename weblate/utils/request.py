d1de2116ad16253f27178ed35e820c9f139ef270
Michal Čihař <michal@cihar.com>
2024-04-09 15:25:20 +0200

    utils: annotate return types

--- weblate/utils/request.py
+++ weblate/utils/request.py
@@ -5,19 +5,19 @@
 import user_agents
 
 
-def get_request_meta(request, name: str):
+def get_request_meta(request, name: str) -> str:
     """Return request meta if request is set and meta available."""
     if not request:
         return ""
     return request.META.get(name, "")
 
 
-def get_ip_address(request):
+def get_ip_address(request) -> str:
     """Return IP address for request."""
     return get_request_meta(request, "REMOTE_ADDR")
 
 
-def get_user_agent_raw(request):
+def get_user_agent_raw(request) -> str:
     """Return raw user agent string."""
     return get_request_meta(request, "HTTP_USER_AGENT")
 
