eebafdd68ff951b6348ee4992e8bb8e30213e939
Michal Čihař <michal@cihar.com>
2024-05-03 15:17:26 +0200

    Setting version to 5.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ project_copyright = "Michal Čihař"
 author = "Michal Čihař"
 
 # The full version, including alpha/beta/rc tags
-release = "5.5.3"
+release = "5.5.4"
 
 
 # -- General configuration ---------------------------------------------------
