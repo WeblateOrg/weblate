359a8419c9f3689b2c0cf2ebbc92a2848446b34a
Michal Čihař <michal@cihar.com>
2024-05-21 09:17:31 +0200

    feat(machinery): include GPT-4o model to OpenAI
    Fixes #11681

--- weblate/machinery/forms.py
+++ weblate/machinery/forms.py
@@ -283,6 +283,7 @@ class OpenAIMachineryForm(KeyMachineryForm):
     # Ordering choices here defines priority for automatic selection
     MODEL_CHOICES = (
         ("auto", pgettext_lazy("OpenAI model selection", "Automatic selection")),
+        ("gpt-4o", "GPT-4o"),
         ("gpt-4-1106-preview", "GPT-4 Turbo"),
         ("gpt-4", "GPT-4"),
         ("gpt-3.5-turbo-1106", "Updated GPT 3.5 Turbo"),
