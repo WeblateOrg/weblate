8fe9496a9f5f0cbe31a6fda110f8e77d93ebec7e
Maciej Olko <maciej.olko@affirm.com>
2024-06-04 01:26:58 +0200

    Add stub for multiple repositories repository

---
+++ weblate/vcs/multiple.py
@@ -0,0 +1,20 @@
+from django.utils.translation import gettext_lazy
+
+from weblate.vcs.base import Repository
+
+
+class MultipleRepositories(Repository):
+    name = "Many repositories"  # limit length to 20
+    push_label = gettext_lazy("This will push changes to the upstream repositories.")
+
+    @classmethod
+    def is_supported(cls):
+        return True  # cannot check internal repos without instantiating the class, assuming True
+
+    @classmethod
+    def is_configured(cls):
+        return True  # cannot check internal repos without instantiating the class, assuming True
+
+    @classmethod
+    def get_version(cls):
+        return 1  # cannot check internal repos without instantiating the class, assuming True
