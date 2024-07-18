d07f109875332bd8448cfad5d1df9320e15a20ce
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-07-18 17:05:31 +0100

    Add left and right arrows for browsing project strings

---
+++ weblate/static/editor/browse.js
@@ -0,0 +1,29 @@
+(() => {
+    function ProjectStringsBrowser() {
+        Mousetrap.bindGlobal("right", (e) => {
+            const nextButton = $('#button-next');
+            const nextLocation = nextButton.attr('href');
+            if (nextButton.length && !nextButton.hasClass('disabled')) {
+                if (nextLocation != undefined) {
+                    window.location.href = nextLocation;
+                }
+            }
+            return false;
+        });
+        Mousetrap.bindGlobal("left", (e) => {
+            const prevButton = $('#button-prev');
+            const prevLocation = prevButton.attr('href')
+            if (prevButton.length && !prevButton.hasClass('disabled')) {
+                if (prevLocation != undefined) {
+                    window.location.href = prevLocation;
+                }
+            }
+            return false;
+        });
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        new ProjectStringsBrowser();
+    });
+})();
+
