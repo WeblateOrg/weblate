8cf05c52891099b44908ff3024705f9909f6ba8e
Mehdi Eloualy <92476321+meel-hd@users.noreply.github.com>
2024-07-19 11:46:25 +0000

    Resolve pre-commit checks

--- weblate/static/editor/browse.js
+++ weblate/static/editor/browse.js
@@ -1,10 +1,14 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 (() => {
   function ProjectStringsBrowser() {
     Mousetrap.bindGlobal("right", (e) => {
       const nextButton = $("#button-next");
       const nextLocation = nextButton.attr("href");
       if (nextButton.length && !nextButton.hasClass("disabled")) {
-        if (nextLocation != undefined) {
+        if (nextLocation !== undefined) {
           window.location.href = nextLocation;
         }
       }
@@ -14,7 +18,7 @@
       const prevButton = $("#button-prev");
       const prevLocation = prevButton.attr("href");
       if (prevButton.length && !prevButton.hasClass("disabled")) {
-        if (prevLocation != undefined) {
+        if (prevLocation !== undefined) {
           window.location.href = prevLocation;
         }
       }
