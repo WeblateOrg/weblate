e31e0ffd93d25c7c873d15fcacc3e92587c27cfe
Mehdi Eloualy <92476321+meel-hd@users.noreply.github.com>
2024-08-07 16:09:04 +0100

    feat(screenshots): Add clipboard paste functionality for screenshots (#12181)
    * Feat: Add clipboard paste functionality for screenshots
    
    The user clicks paste. Then the clipboard is read if the permesion is given and the context is secure (as required by the clipboard API). Then the image is pasted into the select file input. Otherwise if no image is available we only inform the  user about that.
    
    NOTE: Implemented only in the add screenshot form in /screenshots page and not in the modal.
    
    Related: #6710
    
    * Add screenshot clipboard paste in the modal box.
    
    The previous commit added pase screenshot in screenshot/ pages. This adds it in the modal box that opens when Add Screenshot button clicked while checking individual strings.
    
    Related: #6710
    
    * Move the paste button closer to image input
    
    The paste button now is rendered  by django screenshot form isntead of being in the template.
    
    * Remove the paste button if clipboard API  isn't supported by the browser
    
    * Remove part of the forgotten paste button
    
    * Use custom input instead of using dummy one
    
    * Add paste button to screenshot editing
    
    * Add changelog entry
    
    Co-authored-by: Michal Čihař <michal@cihar.com>

---
+++ weblate/static/js/screenshots/clipboard-paste.js
@@ -0,0 +1,75 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+$(document).ready(() => {
+  // The paste trigger button
+  const $pasteScreenshotBtn = $("#paste-screenshot-btn");
+  // The file input to store the screenshot file
+  const $screenshotFileInput = $("#screenshot-form-container input#id_image");
+
+  // Check if the browser supports the Clipboard API
+  if (!navigator.clipboard || !navigator.clipboard.read) {
+    $pasteScreenshotBtn.remove();
+  }
+
+  $pasteScreenshotBtn.on("click", async (e) => {
+    e.preventDefault();
+    try {
+      // Read clipboard content
+      const clipboardItems = await navigator.clipboard.read();
+      let imageFound = false;
+      for (const clipboardItem of clipboardItems) {
+        // Find the image in the clipboard
+        for (const type of clipboardItem.types) {
+          if (type.startsWith("image/")) {
+            // Convert the image data to a file data
+            const blob = await clipboardItem.getType(type);
+            const reader = new FileReader();
+            reader.onload = (_event) => {
+              if ($screenshotFileInput.length) {
+                // Load the file data into the form input
+                const fileName = `screenshot_${Date.now()}.${type.split("/")[1]}`;
+                const imageFile = new File([blob], fileName, { type: type });
+                const dataTransfer = new DataTransfer();
+                dataTransfer.items.add(imageFile);
+                $screenshotFileInput[0].files = dataTransfer.files;
+                // Inform paste success
+                showInfo("success", gettext("Image Pasted!"));
+              } else {
+                showInfo("danger", gettext("Something went wrong!"));
+              }
+            };
+            reader.readAsDataURL(blob);
+            imageFound = true;
+            break;
+          }
+        }
+        if (imageFound) {
+          break;
+        }
+      }
+      if (!imageFound) {
+        showInfo("warning", gettext("No image found in clipboard"));
+      }
+    } catch (err) {
+      showInfo("danger", gettext("Something went wrong!"));
+      console.error("Failed to read clipboard contents: ", err);
+    }
+  });
+});
+
+/**
+ * Displays an information message on the screenshot form.
+ *
+ * @param {string} type - The type of the message (e.g., "success", "error", "warning").
+ * @param {string} message - The content of the message.
+ */
+function showInfo(type, message) {
+  const $pasteScreenshotInfo = $("#paste-screenshot-info-label");
+  const span = `<span class="text-${type}">${message}</span>`;
+  $pasteScreenshotInfo.html(span);
+  $pasteScreenshotInfo
+    .css("transform", "scale(1)")
+    .removeClass("animate__animated animate__fadeIn");
+}
