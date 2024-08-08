471034e7237c3d0a9dcbc7a111c2fd0fe2666740
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-08-08 09:39:31 +0100

    Add webpack bundeling for client side libraries.
    Started with Sentry.

---
+++ client/src/main.js
@@ -0,0 +1,9 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/// Imports to project wide dependencies goes in here
+
+/// Example:
+// import $ from 'jquery';
+// window.$ = $;
