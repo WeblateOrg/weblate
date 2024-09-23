f6f345a0c7c279bc1c286a87d13b2a97d3dcd379
gersona <gersona@hotmail.fr>
2024-09-23 17:13:31 +0300

    Bitbucket Cloud PR  implementation

--- weblate/vcs/git.py
+++ weblate/vcs/git.py
@@ -15,7 +15,7 @@ import urllib.parse
 from configparser import NoOptionError, NoSectionError, RawConfigParser
 from json import JSONDecodeError, dumps
 from time import sleep, time
-from typing import TYPE_CHECKING, NoReturn
+from typing import TYPE_CHECKING, Any, NoReturn
 from zipfile import ZipFile
 
 import requests
@@ -1121,6 +1121,7 @@ class GitMergeRequestBase(GitForcePushRepository):
         data: dict | None = None,
         params: dict | None = None,
         json: dict | None = None,
+        auth: tuple[str, str] | None = None,
         retry: int = 0,
     ) -> tuple[dict, requests.Response, str]:
         do_retry = False
@@ -1149,7 +1150,7 @@ class GitMergeRequestBase(GitForcePushRepository):
                         data=data,
                         params=params,
                         json=json,
-                        auth=self.get_auth(credentials),
+                        auth=auth or self.get_auth(credentials),
                         timeout=5,
                     )
                 except (OSError, HTTPError) as error:
@@ -2153,3 +2154,151 @@ class BitbucketServerRepository(GitMergeRequestBase):
             if pr_exist_message in error_message:
                 return
             self.failed_pull_request(error_message, pr_url, response, response_data)
+
+
+class BitbucketCloudRepository(GitMergeRequestBase):
+    name = gettext_lazy("Bitbucket Cloud merge request")
+    identifier = "bitbucketcloud"
+    _version = None
+    API_TEMPLATE = "{scheme}://api.{host}/2.0/repositories/{owner}/{slug}"
+    REQUIRED_CONFIG = {"username", "token", "workspace"}
+
+    def get_credentials(self) -> dict[str, str]:
+        super_credentials = super().get_credentials()
+        credentials = self.get_credentials_by_hostname(super_credentials["hostname"])
+        super_credentials["workspace"] = credentials["workspace"]
+        return super_credentials
+
+    def get_auth(self, credentials: dict[str, str]) -> tuple[str, str]:
+        return credentials["username"], credentials["token"]
+
+    def get_headers(self, credentials: dict[str, str]) -> dict[str, str]:
+        return {
+            "Accept": "application/json",
+        }
+
+    def get_default_reviewers_uuids(self, credentials: dict[str, str]) -> list[str]:
+        list_reviewers_url = "{}/default-reviewers".format(credentials["url"])
+        try:
+            reviewers = self.build_full_paginated_result(
+                credentials, list_reviewers_url, "Reviewers listing error: "
+            )
+        except RepositoryError:
+            return []
+
+        return [reviewer["uuid"] for reviewer in reviewers]
+
+    def build_full_paginated_result(
+        self,
+        credentials: dict[str, str],
+        url: str,
+        error_message: str,
+    ) -> list[Any]:
+        result: list[Any] = []
+        next_url = url
+
+        while next_url:
+            response_data, _response, error = self.request("get", credentials, next_url)
+
+            if error:
+                raise RepositoryError(
+                    0,
+                    f"{error_message} {error}",
+                )
+
+            result.extend(response_data["values"])
+            next_url = response_data.get("next")
+        return result
+
+    def create_pull_request(
+        self,
+        credentials: dict[str, str],
+        origin_branch: str,
+        fork_remote: str,
+        fork_branch: str,
+    ) -> None:
+        pr_url = "{}/pullrequests".format(credentials["url"])
+        title, description = self.get_merge_message()
+
+        payload = {
+            "title": title,
+            "description": {"raw": description},
+            "source": {
+                "branch": {"name": fork_branch},
+                "repository": {"type": "repository", "name": fork_remote},
+            },
+            "destination": {
+                "branch": {"name": origin_branch},
+            },
+            "reviewers": [
+                {"uuid": reviewer}
+                for reviewer in self.get_default_reviewers_uuids(credentials)
+            ],
+        }
+
+        response_data, response, error = self.request(
+            "post", credentials, pr_url, json=payload
+        )
+        # Bitbucket Cloud handles Pull request already exists
+        # and just returns its data
+
+        if response_data["type"] == "error":
+            # gracefully handle nothing to merge case
+            if "There are no changes to be pulled" in error:
+                return
+
+            self.failed_pull_request(error, pr_url, response, response_data)
+
+    def create_fork(self, credentials: dict[str, str]) -> None:
+        fork_url = "{}/forks".format(credentials["url"])
+        payload = {"workspace": {"slug": credentials["workspace"]}}
+
+        forked_repo: dict | None = None
+
+        # list forks of users, if fork already exist, set remote_fork as fork
+        for fork in self.list_repo_forks(credentials):
+            if fork["owner"]["username"] == credentials["workspace"]:
+                forked_repo = fork
+
+        if forked_repo is None:
+            response_data, response, error = self.request(
+                "post", credentials, fork_url, json=payload
+            )
+
+            # if name is already taken, append an numeric value at the end
+            if "already has a repository with this name" in error:
+                fork_name = "{}-{}".format(
+                    credentials["slug"],
+                    random.randint(1000, 9999),  # noqa: S311
+                )
+                payload["name"] = fork_name
+
+                response_data, response, error = self.request(
+                    "post", credentials, fork_url, json=payload
+                )
+
+            if response_data["type"] == "error":
+                raise RepositoryError(
+                    0, self.get_fork_failed_message(error, credentials, response)
+                )
+
+            forked_repo = response_data
+
+        ssh_url_to_repo = next(
+            link["href"]
+            for link in forked_repo["links"]["clone"]
+            if link["name"] == "ssh"
+        )
+        http_url_to_repo = next(
+            link["href"]
+            for link in forked_repo["links"]["clone"]
+            if link["name"] == "https"
+        )
+
+        self.configure_fork_remote(ssh_url_to_repo, http_url_to_repo, credentials)
+
+    def list_repo_forks(self, credentials: dict[str, str]) -> list[dict[str, Any]]:
+        list_forks_url = "{}/forks?role=owner".format(credentials["url"])
+        return self.build_full_paginated_result(
+            credentials, list_forks_url, "Forks listing error: "
+        )
