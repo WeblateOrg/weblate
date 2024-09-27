ba3cb4c5c06b1e919131edcd664d76c29108b814
Michal Čihař <michal@cihar.com>
2024-09-27 12:58:51 +0200

    feat(api): add OpenAPI schema using drf-spectacular
    Issue #12584

--- weblate/middleware.py
+++ weblate/middleware.py
@@ -64,7 +64,12 @@ CSP_DIRECTIVES: CSP_TYPE = {
 }
 
 # URLs requiring inline javascript
-INLINE_PATHS = {"social:begin", "djangosaml2idp:saml_login_process"}
+INLINE_PATHS = {
+    "social:begin",
+    "djangosaml2idp:saml_login_process",
+    "swagger-ui",
+    "redoc",
+}
 
 
 class ProxyMiddleware:
