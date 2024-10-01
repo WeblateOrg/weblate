408086be4e74977630b77c44a32e3e40f65876e8
Michal Čihař <michal@cihar.com>
2024-10-01 07:53:24 +0200

    fix(permissions): raise error if user is missing in context
    Silently ignoring this makes it easy to miss user in some inclusion
    template tags and the permission check then silently returns false.

--- weblate/auth/templatetags/permissions.py
+++ weblate/auth/templatetags/permissions.py
@@ -11,6 +11,8 @@ register = template.Library()
 def perm(context, permission, obj=None):
     try:
         user = context["user"]
-    except KeyError:
-        return False
+    except KeyError as error:
+        raise ValueError(
+            "Missing user in context, could not perform permission check"
+        ) from error
     return user.has_perm(permission, obj)
