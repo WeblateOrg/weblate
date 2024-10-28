0554b44991c7a743195ccdfa1daf8cdb0605da9d
gers <gersona@users.noreply.github.com>
2024-10-28 09:48:25 +0300

    fix: Strip probited initial character in Glossary CSV upload  (#12806)
    * problematic character cleaned out
    
    * changelog update
    
    * Code coverage and DeepSource fix
    
    * move cleanup prohibited characters to glossary.models

--- weblate/machinery/dummy.py
+++ weblate/machinery/dummy.py
@@ -2,7 +2,11 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from .base import DownloadTranslations, MachineTranslation
+from .base import (
+    DownloadTranslations,
+    GlossaryMachineTranslationMixin,
+    MachineTranslation,
+)
 
 
 class DummyTranslation(MachineTranslation):
@@ -52,3 +56,36 @@ class DummyTranslation(MachineTranslation):
                 "service": "Dummy",
                 "source": text,
             }
+
+
+class DummyGlossaryTranslation(DummyTranslation, GlossaryMachineTranslationMixin):
+    """Dummy glossary translation for testing purposes."""
+
+    glossary_count_limit = 1
+
+    def download_translations(
+        self, source, language, text: str, unit, user, threshold: int = 75
+    ) -> DownloadTranslations:
+        """Translate with glossary."""
+        self.get_glossary_id(source, language, unit)
+        return super().download_translations(
+            source, language, text, unit, user, threshold
+        )
+
+    def list_glossaries(self):
+        """List glossaries."""
+        return {}
+
+    def delete_glossary(self, glossary_id: str) -> None:
+        """Delete glossary."""
+        return
+
+    def delete_oldest_glossary(self) -> None:
+        """Delete oldest glossary."""
+        return self.delete_glossary("")
+
+    def create_glossary(
+        self, source_language: str, target_language: str, name: str, tsv: str
+    ) -> None:
+        """Create glossary."""
+        return
