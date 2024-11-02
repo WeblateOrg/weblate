92bb99ce21a85460e6300ff43ecf25fb9821c5a9
Michal Čihař <michal@cihar.com>
2024-11-02 12:25:58 +0100

    chore: setting version to 5.9

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ project_copyright = "Michal Čihař"
 author = "Michal Čihař"
 
 # The full version, including alpha/beta/rc tags
-release = "5.8.2"
+release = "5.9"
 
 # -- General configuration ---------------------------------------------------
 
