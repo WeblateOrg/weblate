1e80c48348cd9d5bcd53d95560cd0f3871b8fe47
Michal Čihař <michal@cihar.com>
2024-11-07 09:46:40 +0100

    chore(rundev): pin base image and rely on renovate bot to update it
    This is better approach than pulling it on each build.

--- rundev.sh
+++ rundev.sh
@@ -24,8 +24,6 @@ cd dev-docker/
 
 build() {
     mkdir -p data
-    # Fetch up-to-date base docker image
-    docker pull weblate/weblate:bleeding
     # Build the container
     docker compose build --build-arg USER_ID="$USER_ID" --build-arg GROUP_ID="$GROUP_ID"
     cat > .env << EOT
