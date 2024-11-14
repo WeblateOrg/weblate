28a520e428bf70254e0350a15ec2906608b8096f
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-11-14 13:49:54 +0100

    chore(licenses): move multi.js lib to client build workflow

---
+++ client/src/multi.js
@@ -0,0 +1,8 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import multi from "multi.js/src/multi";
+import "multi.js/dist/multi.min.css";
+
+window.multi = multi;
