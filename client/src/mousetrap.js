3296649c03e6754582a6c27bf95f5064a6f11160
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-11-15 13:07:38 +0100

    chore(deps): move Mousetrap to new client build workflow
    Changes include:
    - Moved moustrap and moustrap-global-bind to  be managed in client/.
    - Removed old bundles.
    - Build the new bundle with moustrap and mousetrap-bind-global.
    - Updated base.html to include the new bundles.
    - Removed old license files and generated a new one.
    - Added LLVM-exception.txt to licesne to stay reuse compliant with the new changes.

---
+++ client/src/mousetrap.js
@@ -0,0 +1,8 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import Mousetrap from "mousetrap";
+import "mousetrap-global-bind";
+
+window.Mousetrap = Mousetrap;
