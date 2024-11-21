1d1dd2087d85c3f45c5af48c6e3b2bbf1b0ce906
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-11-21 11:01:10 +0100

    Move source-code fonts to new build workflow
    Changes Include:
    - Removed the old scripts/yarn/** dir, as it is no longer needed.
    - Moved source-code-pro and source-sans to client/
    - Built and bundled the fonts.
    - Updated templates to use the new built fonts and styles.
    - Added new built bundles license info in REUSE.toml.
    
    Note:
    - The old bundles did not get removed to keep this PR simple for review.
    
    TODO:
    - Remove old fonts bundles.
    - Update any dependents on them to use the new bundles.
    - if FONTS_CDN_URL is configured it should be seted up with static/js/vendor/fonts instead of the old font-source/
    - Remove the old yarn-update workflow and its configs and workflows
    
    Related: #12172

---
+++ client/src/fonts.js
@@ -0,0 +1,8 @@
+// Copyright © Michal Čihař <michal@weblate.org>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Importing font styles, when built will include also
+// the font assets they depend on, automatically
+import "source-code-pro/source-code-pro.css";
+import "source-sans/source-sans-3.css";
