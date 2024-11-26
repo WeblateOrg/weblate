0e419825dd7a6964684e43af55d73c827758c7e3
Michal Čihař <michal@cihar.com>
2024-11-26 13:09:15 +0100

    doc: remove secondary index for latex
    We are not building PDF documentation and it's TOC has become out of
    sync with HTML one years ago. So apparently nobody needs this and nobody
    noticed it is not complete.

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,6 @@ exclude_patterns = [
     "_build",
     "Thumbs.db",
     ".DS_Store",
-    "admin/install/steps/*.rst",
     "devel/reporting-example.rst",
 ]
 
@@ -131,7 +130,6 @@ if os.environ.get("READTHEDOCS", "") == "True":
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["../weblate/static/"]
-
 
 html_logo = "../weblate/static/logo-128.png"
 
@@ -189,9 +187,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    ("latexindex", "Weblate.tex", "The Weblate Manual", author, "manual")
-]
+latex_documents = [("index", "Weblate.tex", "The Weblate Manual", author, "manual")]
 
 # Include logo on title page
 latex_logo = "../weblate/static/logo-1024.png"
