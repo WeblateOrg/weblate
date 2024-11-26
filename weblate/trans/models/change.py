2a4aca44e34413651e1c451378497d87b273ff08
Michal Čihař <michal@cihar.com>
2024-11-26 09:18:35 +0100

    chore(changes): fix signal import location

--- weblate/trans/models/change.py
+++ weblate/trans/models/change.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.db import models, transaction
 from django.db.models import Count, Q
-from django.db.models.base import post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from django.utils.html import escape, format_html
