c3b268c25dabe4dbd89d0467881f8505b12d7bbb
Michal Čihař <michal@cihar.com>
2024-12-09 10:37:54 +0100

    feat: improve libre checklist rendering
    - visually highlight problematic bits
    - do not use links without a valid URL
    - add type annotations to related methods

--- weblate/utils/licenses.py
+++ weblate/utils/licenses.py
@@ -14,19 +14,19 @@ LICENSE_URLS = {name: url for name, _verbose, url, _is_libre in ALL_LICENSES}
 LICENSE_NAMES = {name: verbose for name, verbose, _url, _is_libre in ALL_LICENSES}
 
 
-def is_libre(name):
+def is_libre(name: str) -> bool:
     return name in LIBRE_IDS
 
 
-def get_license_url(name):
+def get_license_url(name: str) -> str:
     return LICENSE_URLS.get(name)
 
 
-def get_license_name(name):
+def get_license_name(name: str) -> str:
     return LICENSE_NAMES.get(name, name)
 
 
-def get_license_choices():
+def get_license_choices() -> list[tuple[str, str]]:
     license_filter = settings.LICENSE_FILTER
     if license_filter is None or "proprietary" in license_filter:
         result = [("proprietary", "Proprietary")]
