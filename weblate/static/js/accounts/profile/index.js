2e88565a0487d3e5d1bbc78aa2daa5c0de4b20b1
Mehdi El Oualy <mehdieloualy@gmail.com>
2024-12-17 14:20:55 +0100

    Open project in new tab and add info label

--- weblate/static/js/accounts/profile/index.js
+++ weblate/static/js/accounts/profile/index.js
@@ -10,7 +10,7 @@ $(document).ready(() => {
     if ($(e.target).closest(".multi-wrapper a").length > 0) {
       const slug = $(e.target).closest(".multi-wrapper a").data("value");
       if (slug) {
-        window.location.href = `/projects/${slug}`;
+        window.open(`/projects/${slug}`, "_blank");
       }
       // Prevent context menu from displaying
       e.preventDefault();
