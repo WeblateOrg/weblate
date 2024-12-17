97f1b24e119c7355c95e1b482caabfced9aa29c2
Michal Čihař <michal@cihar.com>
2024-12-17 13:18:57 +0100

    fix(autofix): correctly apply autofix in various conditions
    - Apply autofixes after adjusting plurals. This way the fixer gets a
      correct number of plurals.
    - Gracefully handle different number of plurals as these might not be
      plurals, but multi value strings.
    
    Fixes WEBLATE-12CM
    Fixes WEBLATE-1CZ9

--- weblate/trans/autofixes/base.py
+++ weblate/trans/autofixes/base.py
@@ -45,15 +45,19 @@ class AutoFix:
                 for number, examples in source_plural.examples.items()
             }
             target_examples = target_plural.examples
-            plurals_map = [
-                source_examples.get(tuple(target_examples.get(target_index, [])), -1)
+            plurals_map: dict[int, int] = {
+                target_index: source_examples.get(
+                    tuple(target_examples.get(target_index, [])), -1
+                )
                 for target_index in range(target_plural.number)
-            ]
+            }
             # Ensure we have a source strings for each map
             while len(source_strings) <= max(plurals_map):
                 source_strings.append(source_strings[0])
             results = [
-                self.fix_single_target(text, source_strings[plurals_map[i]], unit)
+                self.fix_single_target(
+                    text, source_strings[plurals_map.get(i, -1)], unit
+                )
                 for i, text in enumerate(target)
             ]
         return [r[0] for r in results], max(r[1] for r in results)
