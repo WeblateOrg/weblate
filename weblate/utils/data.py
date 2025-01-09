46e6bacb64b5866a1068a14e7f5986021929b09b
Michal Čihař <michal@cihar.com>
2025-01-09 14:21:58 +0100

    chore: use pathlib for some path manipulations
    This makes the code easier to read.

--- weblate/utils/data.py
+++ weblate/utils/data.py
@@ -4,13 +4,26 @@
 
 """Data files helpers."""
 
+from __future__ import annotations
+
 import os
+from pathlib import Path
 
 from django.conf import settings
 
 
 def data_dir(component, *args):
     """Return path to data dir for given component."""
+    # TODO: remove once all users are migrated to data_path
     if component == "cache" and settings.CACHE_DIR:
         return os.path.join(settings.CACHE_DIR, *args)
     return os.path.join(settings.DATA_DIR, component, *args)
+
+
+def data_path(component: str) -> Path:
+    """Return path to data dir for given component."""
+    if component == "cache" and settings.CACHE_DIR:
+        # Honor cache directory if configured, legacy setups have it
+        # as a subdirectory of the data directory.
+        return Path(settings.CACHE_DIR)
+    return Path(settings.DATA_DIR) / component
