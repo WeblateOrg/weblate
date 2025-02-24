97152efe7bd1772fcb5a8348b8f1ab410475791a
Michal Čihař <michal@cihar.com>
2025-02-24 07:58:19 +0100

    chore: setting version to 5.10.2

--- weblate/utils/version.py
+++ weblate/utils/version.py
@@ -25,7 +25,7 @@ def get_root_dir():
 
 
 # Weblate version
-VERSION = "5.10.1"
+VERSION = "5.10.2-dev"
 
 # Version string without suffix
 VERSION_BASE = VERSION.replace("-dev", "").replace("-rc", "")
