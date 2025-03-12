707795e14f5967d0ca9b4f97f2e368f9919f2c62
Michal Čihař <michal@cihar.com>
2025-03-12 16:31:59 +0100

    fix(metrics): lazily calculate only todays metrics
    Since the database is now sparse, it does not contain past entries for
    newly created objects.
    
    Fixes WEBLATE-23QS
    Fixes WEBLATE-9N8

--- weblate/metrics/models.py
+++ weblate/metrics/models.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import datetime
 from itertools import zip_longest
+from typing import cast
 
 from django.core.cache import cache
 from django.db import models, transaction
@@ -21,12 +22,12 @@ from weblate.memory.models import Memory
 from weblate.screenshots.models import Screenshot
 from weblate.trans.models import (
     Category,
-    Change,
     Component,
     ComponentList,
     Project,
     Translation,
 )
+from weblate.trans.models.change import Change, ChangeQuerySet
 from weblate.utils.decorators import disable_for_loaddata
 from weblate.utils.stats import (
     BaseStats,
@@ -189,6 +190,7 @@ class MetricManager(models.Manager["Metric"]):
 
         This is used to fill in blanks in a history.
         """
+        changes: ChangeQuerySet
         if obj is None:
             changes = Change.objects.all()
         elif isinstance(
@@ -201,7 +203,7 @@ class MetricManager(models.Manager["Metric"]):
             | ProjectLanguage
             | CategoryLanguage,
         ):
-            changes = obj.change_set.all()
+            changes = cast("ChangeQuerySet", obj.change_set.all())  # type: ignore[misc]
         elif isinstance(obj, ComponentList):
             changes = Change.objects.filter(component__in=obj.components.all())
         elif isinstance(obj, Category):
