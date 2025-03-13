debd9d094110493f2f967bc7b2a517ab4632bdb1
Michal Čihař <michal@cihar.com>
2025-03-13 10:00:39 +0100

    fix(unit): exclude blank target plurals from same target units
    We really do not want to process blank strings at this point.
    
    Might be related to #14189

--- weblate/trans/models/unit.py
+++ weblate/trans/models/unit.py
@@ -208,7 +208,7 @@ class UnitQuerySet(models.QuerySet):
     def same_target(self, unit: Unit, target: str | None = None) -> UnitQuerySet:
         if target is None:
             target = unit.target
-        if not target:
+        if not target or not any(split_plural(target)):
             return self.none()
         translation = unit.translation
         component = translation.component
