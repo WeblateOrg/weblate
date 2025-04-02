e5b2dd0902725ce22cb8b519adee4d8b3d58b451
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-04-02 13:16:55 +0530

    use list_to_tuples with additional comprehension for better understanding

--- weblate/billing/models.py
+++ weblate/billing/models.py
@@ -29,7 +29,7 @@ from weblate.auth.models import User
 from weblate.trans.models import Alert, Component, Project, Translation
 from weblate.trans.util import list_to_tuples
 from weblate.utils.decorators import disable_for_loaddata
-from weblate.utils.html import format_html_join_comma
+from weblate.utils.html import format_html_join_comma, list_to_tuples
 from weblate.utils.stats import prefetch_stats
 
 
@@ -205,14 +205,11 @@ class Billing(models.Model):
     def __str__(self) -> str:
         projects = self.projects_display
         owners = self.owners.order()
-        owners_visible_names_tuples = list_to_tuples(
-            [x.get_visible_name() for x in owners]
-        )
         if projects:
             base = projects
         elif owners:
             base = format_html_join_comma(
-                "{}", (x for x in owners_visible_names_tuples)
+                "{}", list_to_tuples(x.get_visible_name() for x in owners)
             )
         else:
             base = "Unassigned"
@@ -256,9 +253,7 @@ class Billing(models.Model):
 
     @cached_property
     def projects_display(self):
-        return format_html_join_comma(
-            "{}", (x for x in list_to_tuples(self.all_projects))
-        )
+        return format_html_join_comma("{}", list_to_tuples(x for x in self.all_projects))
 
     @property
     def is_trial(self):
@@ -633,7 +628,7 @@ class Invoice(models.Model):
 
         if overlapping.exists():
             msg = "Overlapping invoices exist: {}".format(
-                format_html_join_comma("{}", (x for x in list_to_tuples(overlapping)))
+                format_html_join_comma("{}", list_to_tuples(x for x in overlapping))
             )
             raise ValidationError(msg)
 
