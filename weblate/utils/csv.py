b1db7da23efb43eea3c1979a0d8420c5b6f79c79
Gersona <gersona@hotmail.fr>
2025-04-30 19:54:44 +0300

    remove duplicated code

--- weblate/utils/csv.py
+++ weblate/utils/csv.py
@@ -16,6 +16,6 @@ unicode_whitespaces = {
 PROHIBITED_INITIAL_CHARS = csv_formula_triggers | unicode_whitespaces
 
 # escape the whitespaces so they are rendered as their string representation instead of an actual character
-PROHIBITED_INITIAL_CHARS_FOR_DISPLAY = {"=", "+", "-", "@", "|", "%"} | {
+PROHIBITED_INITIAL_CHARS_FOR_DISPLAY = csv_formula_triggers | {
     (f"\\u{ord(char):04x}",) for char in unicode_whitespaces
 }
