8b1915a24f1350a27aba7c3c77330effc6487657
Michal Čihař <michal@cihar.com>
2025-06-16 11:48:32 +0200

    feat: include reproducible serial in SBOM
    Without that some tools reject the format even though the serial is only
    recommended in the specification.

---
+++ scripts/reproducible-sbom.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import sys
+import uuid
+
+
+class NullNamespace:
+    bytes = b""
+
+
+if len(sys.argv) != 2:
+    print("Usage: reproducible-sbom.py sbom.json")
+    sys.exit(1)
+
+filename = sys.argv[1]
+with open(filename) as handle:
+    data = json.load(handle)
+
+# Remove varying fields
+data["metadata"].pop("timestamp", None)
+
+# Generate UUID based on the content (with serialNumber excluded)
+checksum_data = data.copy()
+checksum_data.pop("serialNumber")
+reproducible_uuid = uuid.uuid5(NullNamespace, json.dumps(checksum_data))
+
+# Update serial number
+data["serialNumber"] = f"urn:uuid:{reproducible_uuid}"
+
+with open(filename, "w") as handle:
+    json.dump(data, handle, indent=2)
+    handle.write("\n")
