32d958a0d4b1e3a1c4451a426d506d0d37485601
Michal Čihař <michal@cihar.com>
2025-08-05 21:50:00 +0200

    fix: add offline context for bootstrap 5

--- weblate/utils/compress.py
+++ weblate/utils/compress.py
@@ -10,9 +10,11 @@ from weblate.accounts.models import Profile
 def offline_context():
     for theme, _name in Profile.theme.field.choices:
         for bidi in (True, False):
-            yield {
-                "fonts_cdn_url": settings.FONTS_CDN_URL,
-                "STATIC_URL": settings.STATIC_URL,
-                "LANGUAGE_BIDI": bidi,
-                "theme": theme,
-            }
+            for bootstrap_5 in (True, False):
+                yield {
+                    "fonts_cdn_url": settings.FONTS_CDN_URL,
+                    "STATIC_URL": settings.STATIC_URL,
+                    "LANGUAGE_BIDI": bidi,
+                    "theme": theme,
+                    "bootstrap_5": bootstrap_5,
+                }
