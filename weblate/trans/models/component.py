221e385f7ecd6edd1d0a24a80e89e821f1a9bc2c
Michal Čihař <michal@cihar.com>
2025-08-07 12:26:47 +0200

    feat(changes): add forced sync and scan operations
    These were currently performed without being tracked.

--- weblate/trans/models/component.py
+++ weblate/trans/models/component.py
@@ -2091,7 +2091,7 @@ class Component(
 
     @perform_on_link
     @transaction.atomic
-    def do_file_sync(self, request=None) -> None:
+    def do_file_sync(self, request: AuthenticatedHttpRequest | None = None) -> None:
         from weblate.trans.models import Unit
         from weblate.trans.tasks import perform_commit
 
@@ -2101,14 +2101,24 @@ class Component(
         ).exclude(translation__language_id=self.source_language_id):
             PendingUnitChange.store_unit_change(unit)
 
+        self.change_set.create(
+            action=ActionEvents.FORCE_SYNC,
+            user=self.acting_user or (request.user if request else None),
+        )
+
         perform_commit.delay_on_commit(
             self.pk, "file-sync", user_id=request.user.id if request else None
         )
 
     @perform_on_link
     @transaction.atomic
-    def do_file_scan(self, request=None) -> bool:
+    def do_file_scan(self, request: AuthenticatedHttpRequest | None = None) -> bool:
         self.commit_pending("file-scan", request.user if request else None)
+        self.change_set.create(
+            action=ActionEvents.FORCE_SCAN,
+            user=self.acting_user or (request.user if request else None),
+        )
+
         self.create_translations(request=request, force=True)
         return True
 
