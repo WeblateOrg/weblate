4e4c26b370a6635a0a8453742431a413e37f8f9f
Michal Čihař <michal@cihar.com>
2025-08-13 20:41:39 +0200

    fix(translation): avoid collecting save errors
    Fixes WEBLATE-31K3

--- weblate/trans/models/translation.py
+++ weblate/trans/models/translation.py
@@ -982,11 +982,6 @@ class Translation(
                         pending_change.source_unit_explanation
                     )
                 except Exception as error:
-                    report_error(
-                        "Could not update unit", project=self.component.project
-                    )
-                    # TODO: once we have a deeper stack of pending changes,
-                    # this should be kept as pending, so that the changes are not lost
                     unit.state = STATE_FUZZY
                     # Use update instead of hitting expensive save()
                     Unit.objects.filter(pk=unit.pk).update(state=STATE_FUZZY)
@@ -994,6 +989,7 @@ class Translation(
                         action=ActionEvents.SAVE_FAILED,
                         target=self.component.get_parse_error_message(error),
                     )
+                    # this should be kept as pending, so that the changes are not lost
                     changes_status[pending_change.pk] = False
                     continue
 
