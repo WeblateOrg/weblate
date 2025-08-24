fb05d7e81a2f5f82e273fe390b63f87f263844bd
Gersona <gersona@hotmail.fr>
2025-08-24 18:11:15 +0300

    add 'file_format_params' to ComponentSerializer

--- weblate/trans/validators.py
+++ weblate/trans/validators.py
@@ -60,3 +60,12 @@ def validate_language_code(code: str | None, filename: str, required: bool = Fal
         raise ValidationError({"filemask": message})
 
     return Language.objects.auto_get_or_create(code=code, create=False)
+
+
+def validate_file_format_parameters(value: dict) -> None:
+    from weblate.trans.file_format_params import FILE_FORMATS_PARAMS
+
+    name_to_file_format_parmas = {param.name: param for param in FILE_FORMATS_PARAMS}
+    for param_name, param_value in value.items():
+        param = name_to_file_format_parmas[param_name]
+        param().get_field().clean(param_value)
