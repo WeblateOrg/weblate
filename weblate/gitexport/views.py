7a238e017943dc5d3d1a13ced0e06404fad4cb73
Michal Čihař <michal@weblate.org>
2025-09-24 18:04:35 +0200

    fix(gitexport): close pipes when done (#16302)
    Make sure the communication pipes are closed when no longer needed.

--- weblate/gitexport/views.py
+++ weblate/gitexport/views.py
@@ -135,7 +135,7 @@ class GitStreamingHttpResponse(StreamingHttpResponse):
     def close(self) -> None:
         if self.wrapper.process.poll() is None:
             self.wrapper.process.kill()
-        self.wrapper.process.wait()
+        self.wrapper.wait()
         super().close()
 
 
@@ -258,10 +258,17 @@ class GitHTTPBackendWrapper:
 
         # Handle status in response
         if "status" in message:
-            self.process.wait()
+            self.wait()
             return HttpResponse(status=int(message["status"].split()[0]))
 
         # Send streaming content as response
         return GitStreamingHttpResponse(
             streaming_content=self, content_type=message["content-type"]
         )
+
+    def wait(self):
+        self.process.wait()
+        if self.process.stdout is not None:
+            self.process.stdout.close()
+        if self.process.stderr is not None:
+            self.process.stderr.close()
