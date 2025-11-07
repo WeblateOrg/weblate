7df83b3acac035dd9944581c6aeda23e742170be
Michal Čihař <michal@cihar.com>
2025-11-07 09:47:52 +0100

    chore: setting version to 5.14.3

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ project_copyright = "Michal Čihař"
 author = "Michal Čihař"
 
 # The full version, including alpha/beta/rc tags
-release = "5.14.2"
+release = "5.14.3"
 
 # -- General configuration ---------------------------------------------------
 
