ca636f6426f6f3c5c035d8bd52014497160599fa
Michal Čihař <michal@cihar.com>
2025-11-11 10:54:35 +0100

    chore: test bilingual update availability by a class
    This consolidates the checks across the codebase.

--- weblate/auth/permissions.py
+++ weblate/auth/permissions.py
@@ -9,6 +9,7 @@ from typing import TYPE_CHECKING, cast
 from django.conf import settings
 from django.utils.translation import gettext
 
+from weblate.formats.base import BilingualUpdateMixin
 from weblate.lang.models import Language
 from weblate.trans.models import (
     Category,
@@ -478,7 +479,7 @@ def check_upload(
     if (
         translation.is_source
         and not translation.is_template
-        and not hasattr(translation.component.file_format_cls, "update_bilingual")
+        and not issubclass(translation.component.file_format_cls, BilingualUpdateMixin)
     ):
         return Denied(
             gettext("The file format does not support updating source strings.")
