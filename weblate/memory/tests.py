a96558b2053d7a3247590ca0cd387099088b06b1
Gersona <gersona@hotmail.fr>
2025-12-04 12:42:40 +0300

    implement Memory context support for TMX file

--- weblate/memory/tests.py
+++ weblate/memory/tests.py
@@ -141,6 +141,12 @@ class MemoryModelTest(TransactionsTestMixin, FixtureTestCase):
 
     def test_import_tmx_command(self) -> None:
         self.do_import_file_command_test(get_test_file("memory.tmx"), 2)
+        self.assertEqual(
+            Memory.objects.filter(status=Memory.STATUS_ACTIVE, from_file=True)
+            .first()
+            .context,
+            "Context",
+        )
 
     def test_import_tmx2_command(self) -> None:
         self.do_import_file_command_test(get_test_file("memory2.tmx"), 1)
