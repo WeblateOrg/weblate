4b979f6aacc52e8b1a0e78793b3552c7d0f77b4a
Michal Čihař <michal@cihar.com>
2026-01-15 11:09:04 +0100

    fix(machinery): remove unsued code

--- weblate/machinery/views.py
+++ weblate/machinery/views.py
@@ -28,7 +28,6 @@ from weblate.machinery.base import (
 from weblate.machinery.models import MACHINERY
 from weblate.trans.models import Project, Unit
 from weblate.trans.templatetags.translations import format_language_string
-from weblate.utils.diff import Differ
 from weblate.utils.errors import report_error
 from weblate.utils.views import parse_path
 from weblate.wladmin.views import MENU as MANAGE_MENU
@@ -401,7 +400,6 @@ def handle_machinery(request: AuthenticatedHttpRequest, service, unit, search=No
     }
 
     machinery_settings = component.project.get_machinery_settings()
-    Differ()
     targets = unit.get_target_plurals()
 
     try:
