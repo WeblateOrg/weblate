2b949b66144237a59a828b7b276ffe716da98e73
Michal Čihař <michal@cihar.com>
2026-01-20 15:03:50 +0100

    fix selenium tests

--- weblate/fonts/tests/utils.py
+++ weblate/fonts/tests/utils.py
@@ -11,10 +11,19 @@ from weblate.fonts.models import FONT_STORAGE, Font
 from weblate.trans.tests.test_views import FixtureTestCase
 
 PACKAGE_PATH = resources.files(weblate_fonts)
-FONT_DIR = PACKAGE_PATH / "static/weblate_fonts/kurinto/ttf"
+FONT_DIR = PACKAGE_PATH / "static" / "weblate_fonts" / "kurinto" / "ttf"
 FONT_NAME = "KurintoSans-Rg.ttf"
 FONT = FONT_DIR / FONT_NAME
 FONT_BOLD = FONT_DIR / "KurintoSans-Bd.ttf"
+
+FONT_SOURCE = (
+    PACKAGE_PATH
+    / "static"
+    / "weblate_fonts"
+    / "source-sans"
+    / "ttf"
+    / "SourceSans3-Bold.ttf"
+)
 
 
 class FontTestCase(FixtureTestCase):
