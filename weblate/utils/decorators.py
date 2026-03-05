d950891123254561383d40173d285f4269ce579a
Michal Čihař <michal@cihar.com>
2026-03-05 14:35:40 +0100

    feat: allow to make engage page public with login required
    This might be useful for some environments.

--- weblate/utils/decorators.py
+++ weblate/utils/decorators.py
@@ -4,6 +4,9 @@
 
 from functools import wraps
 
+from django.conf import settings
+from django.contrib.auth.decorators import login_not_required
+
 
 def disable_for_loaddata(signal_handler):
     """Turn off signal handlers when loading fixture data."""
@@ -15,3 +18,13 @@ def disable_for_loaddata(signal_handler):
         signal_handler(*args, **kwargs)
 
     return wrapper
+
+
+def engage_login_not_required(view_func):
+    """Apply @login_not_required only if engage page should be public."""
+    # 1. Evaluate your complex condition
+    if settings.REQUIRE_LOGIN and settings.PUBLIC_ENGAGE:
+        return login_not_required(view_func)
+
+    # 3. Otherwise, return the original function as-is
+    return view_func
