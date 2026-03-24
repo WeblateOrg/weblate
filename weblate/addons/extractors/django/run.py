0c21c4483219b16dcd2cbd309c5c926ee4037fb6
Michal Čihař <michal@cihar.com>
2026-03-24 18:47:03 +0100

    feat(add-ons): add pot update support
    - currently limited to xgettext, Django and Sphinx
    - we have not intention for generic solution here
    
    Fixes #10708
    Fixes #18428

---
+++ weblate/addons/extractors/django/run.py
@@ -0,0 +1,32 @@
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+
+import os
+import sys
+
+import django
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "weblate.addons.extractors.django.settings"
+)
+
+
+def main() -> None:
+    django.setup()
+
+    from weblate.addons.extractors.django.command import Command
+
+    Command().run_from_argv(
+        [
+            sys.executable,
+            "weblate-extract-makemessages",
+            *sys.argv[1:],
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
