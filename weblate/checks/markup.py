e2cff525dad36a72bbc7692e7d84a6a6735a5799
Michal Čihař <michal@weblate.org>
2026-03-24 08:37:06 +0100

    Apply suggestion from @nijel

--- weblate/checks/markup.py
+++ weblate/checks/markup.py
@@ -356,7 +356,7 @@ class XMLCharsAroundTagsCheck(BaseXMLCheck):
             return False
         # Arabic letter Waw ("و") is a conjunction that commonly attaches directly
         # to the adjacent word without a space, so don't flag space vs Waw
-        if ARABIC_WAW in (src_char, tgt_char):
+        if ARABIC_WAW in {src_char, tgt_char}:
             return False
         return True
 
