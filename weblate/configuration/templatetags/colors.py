6c1653228106d8a1dfb73703c69f4b5c3190fd79
Karen Konou <konoukaren@gmail.com>
2026-08-07 12:35:30 +0200

    feat(ui): expand appearance customization options

--- weblate/configuration/templatetags/colors.py
+++ weblate/configuration/templatetags/colors.py
@@ -14,3 +14,24 @@ def hex_to_rgb(value):
     return ", ".join(
         map(str, tuple(int(value.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4)))
     )
+
+
+@register.filter
+def darken(value, amount=0.6):
+    """Return a darker shade of the given hex color."""
+    channels = (int(value.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
+    return "#{:02x}{:02x}{:02x}".format(
+        *(min(255, max(0, round(channel * amount))) for channel in channels)
+    )
+
+
+@register.filter
+def lighten(value, amount=0.4):
+    """Return a lighter shade of the given hex color."""
+    channels = (int(value.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
+    return "#{:02x}{:02x}{:02x}".format(
+        *(
+            min(255, max(0, round(channel + (255 - channel) * amount)))
+            for channel in channels
+        )
+    )
