92d22d65dd11f8837eca4abc5e9f76f105981791
Michal Čihař <michal@cihar.com>
2026-09-03 07:27:06 +0200

    fix(scripts): whitespace cleanup for contributors

--- scripts/list-contributors.py
+++ scripts/list-contributors.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 
 import argparse
+import re
 from collections import defaultdict
 from pathlib import Path
 from typing import TYPE_CHECKING, Literal
@@ -20,6 +21,7 @@ if TYPE_CHECKING:
 
 VERSION = weblate.utils.version.VERSION_BASE
 ROOT_DIR = Path(__file__).parent.parent
+WHITESPACE_RE = re.compile(r"\s\s+")
 
 CategoryType = Literal["code", "translations", "docs"]
 
@@ -28,6 +30,7 @@ IGNORE_AUTHORS: tuple[str, ...] = (
     "GitHub",
     "Anonymous",
     "Hosted Weblate",
+    "Weblate CI",
     "Copilot",
     "root",
 )
@@ -90,6 +93,8 @@ def get_commit_authors(commit: Commit) -> set[str]:
         for line in str(commit.message).splitlines()
         if line.lower().startswith("co-authored-by:")
     )
+    # Remove extra whitespace
+    authors = [WHITESPACE_RE.sub(" ", author) for author in authors]
     return {
         MAP_AUTHORS.get(author, author) for author in authors if is_valid_author(author)
     }
